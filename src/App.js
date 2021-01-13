ce970fbcf634b05809b5741b8ba55989bcdea621
John Deyrup <jdeyrup@hubspot.com>
2021-01-12 20:05:35 -0500

    update data source

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import React from 'react';
+import { useState } from 'react';
 import Amplify from "@aws-amplify/core";
 import { DataStore } from "@aws-amplify/datastore";
 
@@ -8,35 +8,21 @@ import { Bill } from "./models";
 import awsConfig from "./aws-exports";
 Amplify.configure(awsConfig);
 
-class Page extends React.Component {
-  constructor(props) {
-    super(props);
-    queryDatabase().then(x => console.log(x));
-    this.state = {
-      queryResults: []
-    };
-  }
-
-  render() {
-    return (
-      <div>
-        <p>Page script</p>
-      </div>
-    )
-  }
-}
 
 async function queryDatabase() {
-  return await DataStore.query(Bill);
+  // setBil
 }
 
 function App() {
+  const [bills, setBills] = useState([]);
+
   
   return (
     <div className="App">
       <header className="App-header">
-        <Page/>
+      <p>Front end goes here</p>
       </header>
+      
     </div>
   );
 }
