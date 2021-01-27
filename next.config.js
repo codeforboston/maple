d7fbda3982746e41b11b724555a4ae7faf58540f
d.ondrich <dbo1093@gmail.com>
2021-01-26 20:40:37 -0500

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -2,8 +2,5 @@ const isProduction = process.env.NODE_ENV === "production";
 
 module.exports = {
   assetPrefix: isProduction ? "/advocacy-maps" : "",
-  publicRuntimeConfig: {
-    // used in '/components/Link.js/', for more details go to the component itself
-    linkPrefix: isProduction ? "/advocacy-maps" : "",
-  },
+  basePath: isProduction ? "/advocacy-maps" : "",
 };
