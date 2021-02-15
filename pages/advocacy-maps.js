62811360e79456dde5178d72d3129fd1582a8725
nonstiky <NONstiky@gmail.com>
2021-02-15 17:13:17 -0500

    remove google form component

--- pages/advocacy-maps.js
+++ pages/advocacy-maps.js
@@ -1,6 +1,5 @@
 import dynamic from "next/dynamic";
 import { Layout } from "../components";
-import GoogleForm from "../components/GoogleForm/GoogleForm";
 
 const MapWithNoSSR = dynamic(() => import("../components/Map/Map.jsx"), {
   ssr: false,
@@ -10,12 +9,6 @@ function AdvocacyMap() {
   return (
     <Layout pageTitle="Advocacy Maps">
       <MapWithNoSSR />
-      <GoogleForm 
-        header="Sample Form"
-        blurb="sample blurb"
-        url={"https://docs.google.com/forms/d/e/1FAIpQLSdqd_mLfxBMJ6-rtzNddxhrL4W0Hoa5_ZDaREMyFwmo8C9lKg/viewform?embedded=true"} 
-        width="500px" 
-        height="500px"/>
     </Layout>
   );
 }
