bc1e2683de1faac44440c529b300e91d2d83bad0
Scott Solmonson <ssolmonson63@gmail.com>
2021-02-27 12:30:34 -0500

    Insert persuasive content on priority pages
    Inserted the paragraph above the map, defined within the word document
    attached to issue #64, on each priority page.

--- pages/political-advertisements.js
+++ pages/political-advertisements.js
@@ -9,12 +9,20 @@ function PoliticalAdvertisements() {
           advertisements?
         </b>
         <br />
-        The unregulated state of our campaign finance system and the
-        proliferation of similarly unregulated digital platforms is a dangerous
-        combination, especially considering the well-documented existence of
-        foreign interference in our elections. This bill requires organizations
-        to disclose the details of their purchase. This will increase
-        transparency in our electoral processes, and dissuade foreign actors.
+        Representative democracy is stronger when more constituents participate in
+        elections, and turnout is highest when participation is made easy.
+        Massachusetts participation rates in the most recent elections indicate it was
+        a success – which stems largely from the emergency acts smartly passed by the
+        Legislature in response to COVID-19 (see
+        <a href="https://malegislature.gov/Laws/SessionLaws/Acts/2020/Chapter115"> St.2020 c.115 </a>
+        and <a href="https://malegislature.gov/Laws/SessionLaws/Acts/2020/Chapter255"> St.2020 c.255</a>).
+        Among the temporary changes made to our election systems was a relaxing of
+        vote-by-mail (absentee ballot) restrictions, allowing individuals from across
+        the state to vote early and by absentee ballot.
+        <a href="https://www.bostonherald.com/2020/11/20/mail-in-ballots-made-up-42-of-massachusetts-votes-cast-in-november-election/"> 42% of registered voters in the
+        Commonwealth took advantage of the relatively easy means to vote </a> and would likely
+        do so again if given the opportunity. Massachusetts should retain these improved
+        voting mechanisms.
         <br />
         You can read our full letter on this issue, <a href="/early-mail-voting-letter">here</a>.
         <br />
