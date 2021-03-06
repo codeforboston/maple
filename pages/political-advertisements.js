ababc478eface0e60be8ecd971bed63b77ab52bf
nonstiky <NONstiky@gmail.com>
2021-03-06 10:19:11 -0500

    Add paragraph to EMV and delete political ad page

--- pages/political-advertisements.js
+++ pages/political-advertisements.js
@@ -8,24 +8,6 @@ function PoliticalAdvertisements() {
           Why should organizations be required to disclose their political
           advertisements?
         </b>
-        <br />
-        Representative democracy is stronger when more constituents participate in
-        elections, and turnout is highest when participation is made easy.
-        Massachusetts participation rates in the most recent elections indicate it was
-        a success – which stems largely from the emergency acts smartly passed by the
-        Legislature in response to COVID-19 (see
-        <a href="https://malegislature.gov/Laws/SessionLaws/Acts/2020/Chapter115"> St.2020 c.115 </a>
-        and <a href="https://malegislature.gov/Laws/SessionLaws/Acts/2020/Chapter255"> St.2020 c.255</a>).
-        Among the temporary changes made to our election systems was a relaxing of
-        vote-by-mail (absentee ballot) restrictions, allowing individuals from across
-        the state to vote early and by absentee ballot.
-        <a href="https://www.bostonherald.com/2020/11/20/mail-in-ballots-made-up-42-of-massachusetts-votes-cast-in-november-election/"> 42% of registered voters in the
-        Commonwealth took advantage of the relatively easy means to vote </a> and would likely
-        do so again if given the opportunity. Massachusetts should retain these improved
-        voting mechanisms.
-        <br />
-        You can read our full letter on this issue, <a href="/early-mail-voting-letter">here</a>.
-        <br />
         <b>Advocacy Map</b>
       </p>
     </Layout>
