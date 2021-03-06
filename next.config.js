0b12d602fa8fe65b6c1898dec168750f9d43bc05
dbo1093 <dbo1093@gmail.com>
2021-03-06 12:45:36 -0500

    next js config

--- next.config.js
+++ next.config.js
@@ -1,9 +1,10 @@
 const isProduction = process.env.NODE_ENV === "production";
-const withImages = require('next-images');
+const withImages = require("next-images");
 
 // module.exports = withImage();
 
 module.exports = withImages({
+  target: "serverless",
   assetPrefix: isProduction ? "/advocacy-maps" : "",
   basePath: isProduction ? "/advocacy-maps" : "",
 });
