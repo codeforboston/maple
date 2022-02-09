b6bdcb9e0f06dd5f31d7adb11a32e9589e8132fb
jkoren <j.korenstein@gmail.com>
2022-02-08 22:02:39 -0500

    changed action of "View BIll" button to dynamically route to bill URL

--- components/ViewBills/ViewBills.js
+++ components/ViewBills/ViewBills.js
@@ -1,35 +1,38 @@
 import React from "react";
+import { useRouter } from "next/router"
 import { bills } from "../MockAPIResponse"
-import { Table, Container } from 'react-bootstrap'
+import { Table, Container, NavLink, Button } from 'react-bootstrap'
 import ViewBill from "../ViewBill/ViewBill"
 
-const legislature = "192"
-
-const billsComponent = !bills ? "" :
-  bills.map((bill, index) => {
-    const billNumForURL = bill.billNumber.replace('.', '')
-    const url = `https://malegislature.gov/Bills/${legislature}/${billNumForURL}`
-    return (
-    <tr key={index}>
-      <td><a href={url} rel="noreferrer" target="_blank">{bill.billNumber}</a></td>
-      <td>{bill.title}</td>
-      <td>{bill.primarySponsor.name}</td>
-      <td>0</td>
-      <td>
-        <ViewBill
-          bill={bill}
-        />
-      </td>
-    </tr>
-    )
-  }
-)
 const ViewBills = (props) => {
+  const router = useRouter()
+  
+  const billsComponent = !bills ? "" :
+    bills.map((bill, index) => {
+      const billNumForURL = bill.billNumber.replace('.', '')
+      const url = `/bill/${billNumForURL}`
+      return (
+      <tr key={index}>
+        <NavLink href={url}>{bill.billNumber}</NavLink>
+        <td>{bill.title}</td>
+        <td>{bill.primarySponsor.name}</td>
+        <td>0</td>
+        <td>
+          <Button variant="primary" onClick={() => router.push(`/bill/${billNumForURL}`)}>
+            View Bill
+          </Button>
+          {/* <ViewBill
+            bill={bill}
+          /> */}
+        </td>
+      </tr>
+      )
+    }
+  )
+  
   return (
     <Container>
-      
       <h1>Most Active Bills </h1>
-
       <Table striped bordered hover>
         <thead>
           <tr>
