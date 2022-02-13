c8f9b9ac209bbe384d42952a4830d9acbc5b04b7
arutfield <36383013+arutfield@users.noreply.github.com>
2022-02-13 18:58:01 -0500

    most stuff added

--- components/AddTestimony/AddTestimony.js
+++ components/AddTestimony/AddTestimony.js
@@ -1,5 +1,5 @@
 import React, {useState} from "react";
-import { Button, Modal } from 'react-bootstrap'
+import { Form, Button, Row, Modal } from 'react-bootstrap'
 
 const AddTestimony = (props) => {
   const bill = props.bill
@@ -18,20 +18,64 @@ const AddTestimony = (props) => {
       
       <Modal show={showAddComment} onHide={handleCloseAddComment} size="lg">
         <Modal.Header closeButton onClick={handleCloseAddComment}>
-            <Modal.Title>{bill ? bill.billNumber + " - " + bill.title : ""}</Modal.Title>
+            <Modal.Title>{"Add Your Testimony"}</Modal.Title>
         </Modal.Header>
         <Modal.Body>
-          <>
-            <div className="text-center">
-              <textarea className="form-control" rows="20" placeholder="My comments on this bill" required></textarea>
-            </div>
-          </>
+            <table class="center" border="0" width="100%">
+                <tbody>
+                    <tr>
+                        <td>
+                            <table>
+                                <tbody>
+                                    <tr><td>
+                                        </td>
+                                    </tr>
+                                    <tr>
+                                        <select id = "OpinionList" onchange = "opinionSelection()" >
+                                            <option> Endorse/Neutral/Oppose </option>
+                                            <option>Endorse</option>
+                                            <option>Neutral</option>
+                                            <option>Oppose</option>
+                                        </select>
+                                    </tr>
+                                    <tr>
+                                        <td><div class="form-check">
+                                            <input class="form-check-input" type="checkbox" value="" id="flexCheckChecked" checked/>
+                                            <label class="form-check-label" for="flexCheckChecked">
+                                                Anonymous
+                                            </label>
+                                        </div></td>
+                                    </tr>
+                                    <tr>
+                                        <td><div class="form-check">
+                                            <input class="form-check-input" type="checkbox" value="" id="flexCheckChecked" checked/>
+                                            <label class="form-check-label" for="flexCheckChecked">
+                                                Send copy to your legislatures
+                                            </label>
+                                        </div></td>
+                                    </tr>
+                                    <tr>
+                                        <td><div class="form-check">
+                                            <input class="form-check-input" type="checkbox" value="" id="flexCheckChecked" checked/>
+                                            <label class="form-check-label" for="flexCheckChecked">
+                                                Send copy to relevant committee:
+                                            </label>
+                                        </div></td>
+                                    </tr>
+                                </tbody>
+                            </table>
+                            </td>
+                        <td>
+                            <textarea className="form-control" resize="none" rows="20" placeholder="My comments on this bill" required></textarea>
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
         </Modal.Body>
-
         <Modal.Footer>
-          <Button variant="primary" onClick={handleShowAddComment}>
-            Save
-          </Button>
+            <Button variant="primary" onClick={handleShowAddComment}>
+                Publish
+            </Button>
         </Modal.Footer>
       </Modal>
     </>
