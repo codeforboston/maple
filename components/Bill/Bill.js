97b31daa1b31238549b12a72d5e89f31f2e533a7
Alex Ball <me@alexjball.com>
2022-02-14 12:57:10 -0500

    use useBill in Bill page

--- components/Bill/Bill.js
+++ components/Bill/Bill.js
@@ -1,39 +1,47 @@
-import React from "react";
-import { Row } from 'react-bootstrap'
-import BillTestimonies from '../BillTestimonies/BillTestimonies'
-import AddTestimony from '../AddTestimony/AddTestimony'
-import BillName from '../BillName/BillName'
-import BillHistory from '../BillHistory/BillHistory'
-import BillCosponsors from '../BillCosponsors/BillCosponsors'
-import BillStatus from '../BillStatus/BillStatus'
+import React from "react"
+import { Row, Spinner } from "react-bootstrap"
+import BillTestimonies from "../BillTestimonies/BillTestimonies"
+import AddTestimony from "../AddTestimony/AddTestimony"
+import BillName from "../BillName/BillName"
+import BillHistory from "../BillHistory/BillHistory"
+import BillCosponsors from "../BillCosponsors/BillCosponsors"
+import BillStatus from "../BillStatus/BillStatus"
+import { useBill } from "../db"
 
-const ViewBillPage = (props) => {
-  const bill = props.bill
-  return (
+const ViewBillPage = props => {
+  const { bill, loading } = useBill(props.billId)
+  console.log(bill)
+  return loading ? (
+    <Row>
+      <Spinner animation="border" className="mx-auto" />
+    </Row>
+  ) : (
     <>
       <Row>
         <div className=" d-flex justify-content-center">
-          <BillName bill={bill}/>
-          <BillHistory bill={bill}/>
-          <BillCosponsors bill={bill}/>
-          <BillStatus bill={bill}/>
+          <BillName bill={bill} />
+          <BillHistory bill={bill} />
+          <BillCosponsors bill={bill} />
+          <BillStatus bill={bill} />
         </div>
       </Row>
       <div className="text-center">
-        <h4>{bill ? bill.BillNumber + "  General Court: "+ bill.GeneralCourtNumber : ""} </h4>
+        <h4>
+          {bill
+            ? bill.BillNumber + "  General Court: " + bill.GeneralCourtNumber
+            : ""}
+        </h4>
         <h4>{bill ? bill.Title : ""}</h4>
       </div>
       <div>
-        {(bill && bill.DocumentText != null) ? bill.DocumentText.substring(1,700)+"..." : ""}
+        {bill && bill.DocumentText != null
+          ? bill.DocumentText.substring(1, 700) + "..."
+          : ""}
       </div>
-      <BillTestimonies
-        bill={bill}
-      /> 
-      <AddTestimony
-        bill={bill}
-      /> 
+      <BillTestimonies bill={bill} />
+      <AddTestimony bill={bill} />
     </>
-  );
-};
+  )
+}
 
-export default ViewBillPage;
+export default ViewBillPage
