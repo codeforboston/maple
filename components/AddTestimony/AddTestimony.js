af182205d98614ee72af56156f503493dbe87aa2
jkoren <j.korenstein@gmail.com>
2022-03-06 07:11:54 -0500

    work on EditTestimony, changed AddComment to Testimony, for example showAddComment becomes showTestimony

--- components/AddTestimony/AddTestimony.js
+++ components/AddTestimony/AddTestimony.js
@@ -1,130 +1,18 @@
 import React, {useState} from "react";
 import { Button, Modal } from 'react-bootstrap'
 import { useAuth } from "../../components/auth"
-import { useProfile, useMember } from "../db"
-import * as links from "../../components/links"
-
-const publishTestimony = (props) => {
-    console.log("publishing..")
-    // save to Firebase
-}
-
-const CommentModal = (props) => {
-    const bill=props.bill
-    const showAddComment=props.showAddComment
-    const setShowAddComment=props.setShowAddComment
-    const handleCloseAddComment = () => setShowAddComment(false);
-    
-    const { profile } = useProfile()
-    const representativeId = profile ? profile.representative.id : null
-    const senatorId = profile ? profile.senator.id : null
-    const { member } = useMember(senatorId)
-    const senatorEmail =  member ? member.EmailAddress : null
-    // console.log(senatorEmail)
-    const { member2 } = useMember(representativeId)
-    const [testimony, setTestimony] = useState("")
-    // console.log(member2 ? member2.EmailAddress : null)
-    const url = `mailto:${senatorEmail}?subject=My testimony on Bill ${bill ? bill.BillNumber : ""}&body=${testimony}`
-    
-    return (
-     <Modal show={showAddComment} onHide={handleCloseAddComment} size="lg">
-        <Modal.Header closeButton onClick={handleCloseAddComment}>
-            <Modal.Title>{"Add Your Testimony" + (bill ? " for " + bill.BillNumber + " - " + bill.Title : "")}</Modal.Title>
-        </Modal.Header>
-        <Modal.Body>
-            <table className="center" border="0" width="100%">
-                <tbody>
-                    <tr>
-                        <td>
-                            <table>
-                                <tbody>
-                                    <tr>
-                                        <td>
-                                            <div className="text-center">
-                                                <select className="form-control">
-                                                    <option value="DEFAULT">Select my support..</option>
-                                                    <option value="Endorse">Endorse</option>
-                                                    <option value="Oppose">Oppose</option>
-                                                    <option value="Neutral">Neutral</option>
-                                                </select>
-                                            </div>
-                                        </td>
-                                    </tr>
-                                    <tr>
-                                        <td><div className="form-check">
-                                            <input className="form-check-input" type="checkbox" value="" id="flexCheckChecked"/>
-                                            <label className="form-check-label" htmlFor="flexCheckChecked">
-                                                Anonymous
-                                            </label>
-                                        </div></td>
-                                    </tr>
-                                    {/* <tr>
-                                        <td><div className="form-check">
-                                            <input className="form-check-input" type="checkbox" value="" id="flexCheckChecked"/>
-                                            <label className="form-check-label" htmlFor="flexCheckChecked">
-                                                Send copy to your legislatures
-                                            </label>
-                                        </div></td>
-                                    </tr> */}
-                                    {/* <td><div className="form-check">
-                                        <input className="form-check-input" type="checkbox" value="" id="flexCheckChecked"/>
-                                        <label className="form-check-label" htmlFor="flexCheckChecked">
-                                            Send copy to relevant committee
-                                        </label>
-                                    </div></td> */}
-                                </tbody>
-                            </table>
-                                    <div>
-                                        <links.External href={url}>
-                                            Send copy to your legislators
-                                        </links.External>
-                                    </div>
-                                    <div>
-                                        <links.External href={url}>
-                                            Send copy to relevant committee
-                                        </links.External>
-                                    </div>
-                            </td>
-                        <td width="400px">
-                            <textarea 
-                                className="form-control" 
-                                resize="none" 
-                                rows="20" 
-                                placeholder="My comments on this bill" required
-                                onChange={e => {
-                                    const someText = e.target.value
-                                    setTestimony(someText)
-                                }}    
-                            />
-
-                           
-                            <Button className="mt-2">Upload a document</Button>
-                        </td>
-                        <td width="50px"/>
-
-                    </tr>
-                </tbody>
-            </table>
-        </Modal.Body>
-        <Modal.Footer>
-            <Button variant="primary" onClick={publishTestimony}>
-                Publish
-            </Button>
-        </Modal.Footer>
-    </Modal>
-    )
-} 
+import CommentModal from "../CommentModal/CommentModal"
    
 const AddTestimony = (props) => {
   const bill = props.bill
-  const [showAddComment, setShowAddComment] = useState(false);
+  const [showTestimony, setShowTestimony] = useState(false);
 
-  const handleShowAddComment = () => setShowAddComment(true);
+  const handleShowTestimony = () => setShowTestimony(true);
   const { authenticated, user } = useAuth()
     return (
   <>
         <div className="d-flex justify-content-center">
-            <Button variant="primary" onClick={handleShowAddComment}>
+            <Button variant="primary" onClick={handleShowTestimony}>
                 {authenticated ? "Add your voice" : "Login to add your voice"}
             </Button>
         </div>
@@ -132,9 +20,9 @@ const AddTestimony = (props) => {
         {/* to force authentication to add comments, add "authenticated &&" in front of modal call below */}
         <CommentModal
             bill={bill}
-            showAddComment={showAddComment}
-            setShowAddComment={setShowAddComment}
-            handleShowAddComment={handleShowAddComment}
+            showTestimony={showTestimony}
+            setShowTestimony={setShowTestimony}
+            handleShowTestimony={handleShowTestimony}
         />
     </>
     )
--- components/EditTestimony/EditTestimony.js
+++ components/EditTestimony/EditTestimony.js
@@ -1,5 +1,6 @@
 import React, {useState} from "react";
 import { Button, Modal } from 'react-bootstrap'
+import CommentModal from "../CommentModal/CommentModal"
 
 const EditTestimony = (props) => {
   const bill = props.bill
@@ -14,21 +15,12 @@ const EditTestimony = (props) => {
       Edit
     </Button>
     <Modal show={showTestimony} onHide={handleCloseTestimony} size="lg">
-      <Modal.Header closeButton onClick={handleCloseTestimony}>
-          <Modal.Title>{bill ? bill.BillNumber + " - " + bill.Title : ""}</Modal.Title>
-      </Modal.Header>
-      <Modal.Body>
-          <h4>{testimony ? testimony.authorUid + " - " + testimony.publishedAt.toDate().toLocaleString() + " - " + testimony.position : ""}</h4>
-          <p>{testimony ? testimony.content : ""}</p>
-          <h4>
-            {testimony && testimony.attachment != null ? 
-            <Button variant="primary">
-              See attachment
-            </Button> 
-            : 
-            ""}
-          </h4>
-      </Modal.Body>
+        <CommentModal
+            bill={bill}
+            showAddComment={showTestimony}
+            setShowTestimony={setShowTestimony}
+            handleShowTestimony={handleShowTestimony}
+        />
     </Modal>
   </>
   )
