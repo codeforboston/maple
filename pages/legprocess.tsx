4783a1280f825c8b4d6bed0b03eccdb4df6d06a9
Nathan <sandersn@gmail.com>
2022-03-26 15:22:04 -0400

    leg process

--- pages/legprocess.tsx
+++ pages/legprocess.tsx
@@ -17,7 +17,7 @@ export default createPage({
         <ul>
           <li><b>Who</b>: <em>Anyone can submit testimony</em> to the MA legislature. Legislators tend to value testimony most when it comes from their own constituents, so testimony from MA residents is typically directed to both the committee that is substantively responsible for the bill as well as the legislators (House member and Senator) representing your district.</li>
           <li><b>What</b>: Your testimony will be most impactful when it feels <em>distinctive and relevant</em>, so be sure to write your own text and explain why you are interested in an issue.</li>
-          <li><b>When</b>: Committees generally accept testimony up until the <em>hearing date designated for a bill</em>. Although some committees will accept testimony after this date, for the greatest impact you should submit your testimony before the hearing.</li>
+          <li><b>When</b>: Committees generally accept testimony up until the <em>hearing date designated for a bill</em>. You can use the bill pages on this website to identify relevant committee dates. Although some committees will accept testimony after this date, for the greatest impact you should submit your testimony before the hearing.</li>
           <li><b>Where</b>: Testimony is generally accepted by <em>committees of the legislature</em> by sending an email to their Chairs.  This website, MATT, will help you to do this by making it easy to find a bill you want to testify in and then generate an email, which you fully control, which you can then send to the relevant personnel.</li>
           <li><b>Why</b>: The key role of testimony is to let your legislators know how you feel about an issue. If you don't share your perspective, it may not be taken into account when policymakers make decisions about the laws that govern all our lives.</li>
         </ul>
@@ -36,6 +36,19 @@ export default createPage({
         <h1>Understanding the Massachusetts Legislative Process</h1>
         
         <h2>How a bill becomes a law</h2>
+        
+        Some of the key steps in the legislative process for most laws in MA are,
+        
+        <ol>
+          <li>Filing. A Representative, Senator, or the Governor must file a bill.  Constituents can write to their legislators (typically their own district's House or Senate member) to propose a bill to be filed. This can be done at any time, but is typically done before the start of the legislative session (i.e. before the third Friday in January of odd-numbered years).  Bills filed later, during the legislative session, require special approval.</li>
+          <li>Testimony! All stakeholders have the chance to share their thoughts with the legislature by submitting written testimony. Typically, testimony is submitted to the legislative Committee responsible for the bill in advance of their public hearing. You can also deliver oral testimony by attending a hearing or you can reach out to your legislators and speak to them directly.</li>
+          <li>Public hearing. All bills are formally heard by committees during public hearings which are open to the public and recorded and posted as videos on the legislature's website.  While the amount of time available to speak during a public hearing is limited, more detailed comments can be submitted to the committee in written testimony.</li>
+          <li>Committee reports. Committees must file reports on each bill under their consideration after discussing them in Executive Session following the public hearing. This typically occurs before February of the second year of the legislative session (even-numbered years). The goal for proponents of a bill is that the Committee will recommend they "ought to pass" and thereby promote them out of the Committee. Most bills, however, are "sent to study," meaning that they will not be passed out of committee in that session. A successful bill may be redrafted or amended by the committee based on testimony received and other deliberations.  Many of those bills will be refiled in the next session and can be considered again.</li>
+          <li>Three readings. Each bill passed out of Committee will be "read" three times by each branch, the House and Senate. This typically entails floor debate of the full chamber and a vote of the Committee on Ways and Means or Steering and Policy.</li>
+          <li>Engrossment and Enactment. After the three readings, the bill will be voted on by each of the full chambers (House and Senate), resulting in (if successful) "engrossment" and then "enactment."</li>
+          <li>Conference Committee. If necessary, differences between the House and Senate versions of a bill will be reconciled by a temporary Conference Committee appointed by the House Speaker and Senate President.</li>
+          <li>Executive branch. Lastly, the Governor is responsible for signing the enacted and reconciled bill into law. The governor can also veto the bill, return it to the Legislature for changes, or take a number of other less-common actions.</li>
+        </ol>
         
         <h2>The role of testimony</h2>
         
