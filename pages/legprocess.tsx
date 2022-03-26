9faafbef072362b77272ac7ab30d8f98cb326cd2
Nathan <sandersn@gmail.com>
2022-03-26 14:58:58 -0400

    adding detail - basics

--- pages/legprocess.tsx
+++ pages/legprocess.tsx
@@ -10,7 +10,19 @@ export default createPage({
         
         All laws passed by state legislatures should be informed by feedback from residents and community stakeholders.  In Massachusetts, among other ways to have your voice heard, you can submit written testimony to the legislature regarding specific bills.  
         
-        This website, the MATT platform, can help facilitate your submission of written testimony to the MA Legislature.
+        This website, the MATT platform, can help facilitate your submission of written testimony to the MA Legislature.  However, please note that this is not an official government website and you do not need to use this platform to submit your testimony.
+        
+        Here are the essential things to know before submitting testimony,
+        
+        <ul>
+          <li><b>Who</b>: <em>Anyone can submit testimony</em> to the MA legislature. Legislators tend to value testimony most when it comes from their own constituents, so testimony from MA residents is typically directed to both the committee that is substantively responsible for the bill as well as the legislators (House member and Senator) representing your district.</li>
+          <li><b>What</b>: Your testimony will be most impactful when it feels <em>distinctive and relevant</em>, so be sure to write your own text and explain why you are interested in an issue.</li>
+          <li><b>When</b>: Committees generally accept testimony up until the <em>hearing date designated for a bill</em>. Although some committees will accept testimony after this date, for the greatest impact you should submit your testimony before the hearing.</li>
+          <li><b>Where</b>: Testimony is generally accepted by <em>committees of the legislature</em> by sending an email to their Chairs.  This website, MATT, will help you to do this by making it easy to find a bill you want to testify in and then generate an email, which you fully control, which you can then send to the relevant personnel.</li>
+          <li><b>Why</b>: The key role of testimony is to let your legislators know how you feel about an issue. If you don't share your perspective, it may not be taken into account when policymakers make decisions about the laws that govern all our lives.</li>
+        </ul>
+        
+        For more detail, see the sections below.
         
         <h2>Other Resources</h2>
         
@@ -30,6 +42,10 @@ export default createPage({
         <h1>Communicating with Legislators</h1>
         
         <h2>Writing Effective Testimony</h2>
+        
+        <h1>Why transparency matters</h1>
+        
+        A core goal of the MATT platform (this website) is to increase the transparency of the legislative process in MA.
         
       </>
     )
