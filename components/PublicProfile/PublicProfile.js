5d222dec372581c79b08214e424bca5728fb728d
jkoren <j.korenstein@gmail.com>
2022-03-27 18:50:56 -0400

    create a public profile page and link from TestimoniesTable

---
+++ components/PublicProfile/PublicProfile.js
@@ -0,0 +1,14 @@
+import React from "react"
+
+const PublicProfile = ({ id }) => {
+  // find the profile from the id
+
+  return (
+    <>
+      <h1>Bob Testimonygiver</h1>
+      <h3>{id}</h3>
+    </>
+  )
+}
+
+export default PublicProfile
