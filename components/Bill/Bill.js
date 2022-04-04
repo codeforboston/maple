4c2bcb2628532ba54ee4fb5147b00ad8ca9e5b63
sashamaryl <sashamaryl@gmail.com>
2022-04-03 21:33:21 -0400

    update formatting top section of bill detail Change bill status button: “Status: XYZ” (colon) Cut bill text # of characters by 50% Delete “General Court: 192”

--- components/Bill/Bill.js
+++ components/Bill/Bill.js
@@ -6,6 +6,7 @@ import BillCosponsors from "../BillCosponsors/BillCosponsors"
 import BillStatus from "../BillStatus/BillStatus"
 import BillReadMore from "../BillReadMore/BillReadMore"
 import { useBill } from "../db"
+import { formatBillId } from "../formatting.ts"
 
 const ViewBillPage = ({ billId }) => {
   const { loading, result: fullBill } = useBill(billId)
@@ -15,35 +16,39 @@ const ViewBillPage = ({ billId }) => {
   const committeeName = fullBill?.currentCommittee?.name
   const houseChairEmail = fullBill?.currentCommittee?.houseChair?.email
   const senateChairEmail = fullBill?.currentCommittee?.senateChair?.email
+
   return loading ? (
     <Row>
       <Spinner animation="border" className="mx-auto" />
     </Row>
   ) : (
     <>
+      <div className="text-center">
+        <h4>{bill ? formatBillId(bill.BillNumber) : ""}</h4>
+        <div>{bill ? bill.Title : ""}</div>
+        <div>
+          <b>Lead Sponsor: </b>
+          {bill.PrimarySponsor.Name}
+        </div>
+      </div>
       <Row>
-        <div className=" d-flex justify-content-center">
+        <div className=" d-flex justify-content-center mt-1">
           <BillCosponsors bill={bill} />
           <BillStatus bill={bill} billHistory={billHistory} />
         </div>
       </Row>
       <div className="text-center">
-        <h4>
-          {bill
-            ? bill.BillNumber + "  General Court: " + bill.GeneralCourtNumber
-            : ""}
-        </h4>
-        <h4>{committeeName ? "Current Committee: " + committeeName : ""}</h4>
-        <h4>{bill ? bill.Title : ""}</h4>
-        <h5>{bill ? bill.Pinslip : ""}</h5>
+        <b>Current Committee: </b>
+        {committeeName ? committeeName : "Not currently in committee"}{" "}
+        {/*TODO: this will be a link*/}
       </div>
-      <div>
+      <div className="m-3">
         {bill && bill.DocumentText != null ? (
           <>
             <span style={{ whiteSpace: "pre-wrap" }}>
-              {bill.DocumentText.substring(0, 700) + "..."}
+              <i>{bill.DocumentText.substring(0, 350)}&#8288;&#8230;</i>
             </span>
-            {bill.DocumentText.length > 700 ? (
+            {bill.DocumentText.length > 350 ? (
               <BillReadMore bill={bill} />
             ) : null}
           </>
