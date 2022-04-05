dafd18924b67c87f412fc4cad4b7153eee23e4f3
d.ondrich <dbo1093@gmail.com>
2022-04-05 15:24:28 -0400

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -4,6 +4,6 @@ const withImages = require("next-images");
 // module.exports = withImage();
 
 module.exports = withImages({
-  assetPrefix: isProduction ? "https://d-ondrich.github.io/advocacy-maps/" : "",
-  basePath: isProduction ? "/advocacy-maps" : "",
+  assetPrefix: "",
+  basePath: "",
 });
