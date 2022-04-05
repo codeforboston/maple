e7e82b4d4ac4aef67ef600a9987a5981e0ef077d
jkoren <j.korenstein@gmail.com>
2022-04-05 16:19:15 -0400

    Added preview profile button

--- pages/profile.tsx
+++ pages/profile.tsx
@@ -5,7 +5,9 @@ import * as links from "../components/links"
 import { createPage } from "../components/page"
 import SelectLegislators from "../components/SelectLegislators"
 import MyTestimonies from "../components/MyTestimonies/MyTestimonies"
-import { Row, Col, FormControl, Form, Spinner } from "react-bootstrap"
+import { Row, Col, FormControl, Form, Spinner, Button } from "react-bootstrap"
+import { useRouter } from "next/router"
+import { useAuth } from "../components/auth"
 
 const showLegislators = (
   <>
@@ -51,6 +53,10 @@ export default createPage({
   title: "Profile",
   Page: requireAuth(({ user: { displayName } }) => {
     const profile = useProfile()
+
+    const { user } = useAuth()
+    const uid = user?.uid
+
     const [entity, setEntity] = useState("individual")
     const makeOrganization = () => {
       setEntity("organization")
@@ -59,6 +65,7 @@ export default createPage({
       setEntity("individual")
     }
     const individual = entity == "individual"
+    const router = useRouter()
 
     return (
       <>
@@ -101,11 +108,19 @@ export default createPage({
               }}
             />
             <label className="form-check-label" htmlFor="flexCheckChecked">
-              Allow others to see my profile&nbsp;
+              Allow others to see my profile
             </label>
             {profile.updatingIsPublic ? (
               <Spinner animation="border" className="mx-auto" size="sm" />
             ) : null}
+            <div className="mt-2">
+              <Button
+                variant="primary"
+                onClick={() => router.push(`/publicprofile?id=${uid}`)}
+              >
+                Preview Profile
+              </Button>
+            </div>
           </div>
         )}
         <MyTestimonies />
