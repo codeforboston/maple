4f3a1421d2a569c8432cb0cfd09ee1801a9e6546
arutfield <36383013+arutfield@users.noreply.github.com>
2022-04-12 20:32:56 -0400

    reads from user query to get information on profile

--- components/PublicProfile/PublicProfile.js
+++ components/PublicProfile/PublicProfile.js
@@ -1,22 +1,32 @@
 import React from "react"
 import { usePublicProfile } from "../db"
 import UserTestimonies from "../UserTestimonies/UserTestimonies"
+import * as links from "../../components/links.tsx"
 
 const PublicProfile = ({ id }) => {
   const profile = usePublicProfile(id)
   const user = profile.result
-
-  const bio = `Ipsum eu proident qui occaecat et nisi non nostrud Lorem quis. Minim ex proident non tempor veniam minim non elit ipsum non incididunt laborum pariatur excepteur. Quis excepteur Lorem id nisi quis nostrud veniam enim id officia ullamco adipisicing consectetur. Duis eiusmod aliqua proident in sunt deserunt non. Non veniam cillum mollit non ex enim ut deserunt in veniam tempor ex mollit. Veniam est aliquip cillum ad dolore sint. Sint magna est eu laborum occaecat tempor minim nulla velit.`
-  const twit = "fakename"
-  const linkedIn = "https://www.linkedin.com/in/fakename/"
+  const bio = user?.about
+  const twit = user?.social?.twitter
+  const linkedIn = user?.social?.linkedIn
 
   return (
     <>
       <h1>{user?.displayName ? user?.displayName : "Name placeholder"}</h1>
       <h4>Senator: {user?.senator?.name}</h4>
       <h4>Representative: {user?.representative?.name}</h4>
-      <h4>Twitter: @{twit}</h4>
-      <h4>LinkedIn:{linkedIn}</h4>
+      <h4>
+        Twitter:{" "}
+        <links.External href={`https://www.twitter.com/${twit}`}>
+          @{twit}
+        </links.External>
+      </h4>
+      <h4>
+        LinkedIn:{" "}
+        <links.External href={`https://www.linkedin.com/in/${linkedIn}`}>
+          {linkedIn}
+        </links.External>
+      </h4>
       <p>{bio}</p>
       <UserTestimonies authorId={id} />
     </>
