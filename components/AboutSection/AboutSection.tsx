3ea8954d3197777a7bb186b3dd9b296eb4f93eb6
sashamaryl <sashamaryl@gmail.com>
2022-05-20 19:16:10 -0400

    tweaks adds bg logo (temp asset from sketch - not full logo) adds futura font stack

--- components/AboutSection/AboutSection.tsx
+++ components/AboutSection/AboutSection.tsx
@@ -1,38 +1,45 @@
-import { Container, Row } from "react-bootstrap"
+import { Container, Row, Col, Image } from "react-bootstrap"
 import AboutInfoCard from "../AboutSectionInfoCard/AboutInfoCard"
+import BackgroundLogo from "../LogoPlacements/BackgroundLogo"
 import styles from "./AboutSection.module.css"
 
 export default function AboutSection() {
   return (
     <Container fluid>
-      <Row
-        xs={1}
-        md={3}
-        className={`${styles.angledTop} g-4 justify-content-center`}
-      >
+      <Row className={`${styles.angledTop}`}>
         <Row>
-          <h2 className="text-white">About MAPLE</h2>
+          <Col xs={{ span: 10, offset: 1 }} md={{ span: 4, offset: 1 }}>
+            <h2 className="text-white text-nowrap text-center">About MAPLE</h2>
+          </Col>
         </Row>
-        <AboutInfoCard
-          title="Who"
-          bodytext="Anyone can submit testimony to the MA legislature. Legislators tend to value testimony most when it comes from their own constituents, so testimony from MA residents is typically directed to both the committee that is substantively responsible for the bill as well as the legislators (House member and Senator) representing your district."
-        ></AboutInfoCard>
-        <AboutInfoCard
-          title="When"
-          bodytext="Committees generally accept testimony up until the hearing date designated for a bill. You can use the bill pages on this website to identify relevant committee dates. Although some committees will accept testimony after this date, for the greatest impact you should submit your testimony before the hearing."
-        ></AboutInfoCard>
-        <AboutInfoCard
-          title="Why"
-          bodytext="The key role of testimony is to let your legislators know how you feel about an issue. If you don't share your perspective, it may not be taken into account when policymakers make decisions about the laws that govern all our lives."
-        ></AboutInfoCard>
-        <AboutInfoCard
-          title="Where"
-          bodytext="Testimony is generally accepted by committees of the legislature by sending an email to their Chairs. This website, MAPLE, will help you to do this by making it easy to find a bill you want to testify in and then generate an email, which you fully control, which you can then send to the relevant personnel."
-        ></AboutInfoCard>
-        <AboutInfoCard
-          title="What"
-          bodytext="Your testimony will be most impactful when it feels distinctive and relevant, so be sure to write your own text and explain why you are interested in an issue.            "
-        ></AboutInfoCard>
+        <Row
+          xs={1}
+          md={2}
+          lg={3}
+          className={`g-4 justify-content-center w-75 m-auto py-5`}
+        >
+          <BackgroundLogo />
+          <AboutInfoCard
+            title="Who"
+            bodytext="Anyone can submit testimony to the MA legislature. Legislators tend to value testimony most when it comes from their own constituents, so testimony from MA residents is typically directed to both the committee that is substantively responsible for the bill as well as the legislators (House member and Senator) representing your district."
+          ></AboutInfoCard>
+          <AboutInfoCard
+            title="When"
+            bodytext="Committees generally accept testimony up until the hearing date designated for a bill. You can use the bill pages on this website to identify relevant committee dates. Although some committees will accept testimony after this date, for the greatest impact you should submit your testimony before the hearing."
+          ></AboutInfoCard>
+          <AboutInfoCard
+            title="Why"
+            bodytext="The key role of testimony is to let your legislators know how you feel about an issue. If you don't share your perspective, it may not be taken into account when policymakers make decisions about the laws that govern all our lives."
+          ></AboutInfoCard>
+          <AboutInfoCard
+            title="Where"
+            bodytext="Testimony is generally accepted by committees of the legislature by sending an email to their Chairs. This website, MAPLE, will help you to do this by making it easy to find a bill you want to testify in and then generate an email, which you fully control, which you can then send to the relevant personnel."
+          ></AboutInfoCard>
+          <AboutInfoCard
+            title="What"
+            bodytext="Your testimony will be most impactful when it feels distinctive and relevant, so be sure to write your own text and explain why you are interested in an issue.            "
+          ></AboutInfoCard>
+        </Row>
       </Row>
     </Container>
   )
--- components/AboutSectionInfoCard/AboutInfoCard.tsx
+++ components/AboutSectionInfoCard/AboutInfoCard.tsx
@@ -1,17 +1,26 @@
-import { Card, Col, Container } from "react-bootstrap"
-
+import { Button, Card, Col, Container } from "react-bootstrap"
+import styles from "./AboutInfoCard.module.css"
 
 export type AboutInfoCardProps = {
-  title: string, 
-  bodytext: string, 
+  title: string
+  bodytext: string
   options?: {}
 }
 
-export default function AboutInfoCard({title, bodytext}: AboutInfoCardProps){
-  return <Col>
-    <Card className="h-100">
-      <Card.Header as="h2" className="text-center w-50 align-self-center">{title}</Card.Header>
-      <Card.Body>{bodytext}</Card.Body>
-    </Card>
-  </Col>
+export default function AboutInfoCard({ title, bodytext }: AboutInfoCardProps) {
+  return (
+    <Col className="my-3">
+      <Card className="h-100">
+        <Card.Header
+          as="h3"
+          className={`text-center align-self-center text-white rounded-0 ${styles.cardHeader}`}
+        >
+          {title}
+        </Card.Header>
+        <Card.Body className={`${styles.cardBody}`}>
+          <p className={`${styles.cardBodyParagraph}`}>{bodytext}</p>
+        </Card.Body>
+      </Card>
+    </Col>
+  )
 }
