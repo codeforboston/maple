4a6a87d8edf3de1fd218f295429573f60bfd26f0
pololeningcelaya <pololc2000@gmail.com>
2022-05-31 01:20:14 -0300

    Add profile link to header and adjustments to match the mockup

--- components/V2Layout.tsx
+++ components/V2Layout.tsx
@@ -1,10 +1,12 @@
 import { useRouter } from "next/router"
 import React from "react"
-import { SignOut, useAuth } from "./auth"
+import { SignInWithModal, SignOut, useAuth } from "./auth"
 import { Container, Nav, Navbar } from "./bootstrap"
 import { Wrap } from "./links"
 import Head from "next/head"
 import Image from "react-bootstrap/Image"
+import ProfileLink from "./ProfileLink/ProfileLink"
+import { auth } from "./firebase"
 
 const V2Layout: React.FC<{ title?: string }> = ({ children, title }) => {
   return (
@@ -27,44 +29,42 @@ const V2Layout: React.FC<{ title?: string }> = ({ children, title }) => {
 
 const TopNav: React.FC = () => {
   const { authenticated } = useAuth()
+  const displayName = useAuth().user?.displayName
+  const signOut = () => auth.signOut()
   return (
-    <Navbar bg="secondary" variant="dark" expand="xl">
-      <Container>
-        <Navbar.Toggle aria-controls="topnav" />
-        <Navbar.Collapse id="topnav">
-          <Nav className="me-auto">
-            <NavLink href="/">Home</NavLink>
-            <NavLink href="/bills">Bills</NavLink>
-            <NavLink href="/testimonies">Testimony</NavLink>
-            <NavLink href="/about">About</NavLink>
-            <NavLink href="/legprocess">Learn</NavLink>
-          </Nav>
-        </Navbar.Collapse>
-        <Wrap href="/">
-          <Navbar.Brand>
-            <Image fluid src="nav-logo.png" alt="logo"></Image>
-          </Navbar.Brand>
-        </Wrap>
-        <AccountNav authenticated={authenticated} />
-      </Container>
-    </Navbar>
-  )
-}
-
-const AccountNav: React.FC<{ authenticated: boolean }> = ({
-  authenticated
-}) => {
-  return authenticated ? (
     <>
-      <Nav className="me-auto">
-        <NavLink href="/profile">Profile</NavLink>
-      </Nav>
-      <SignOut />
+      <Navbar bg="secondary" variant="dark" expand={false}>
+        <Container>
+          <Navbar.Toggle aria-controls="topnav" />
+          <Navbar.Brand>
+              <Nav.Link href="/">
+                  <Image fluid src="nav-logo.png" alt="logo"></Image>
+              </Nav.Link>
+          </Navbar.Brand>
+          <Nav>
+            {!authenticated?(
+                <SignInWithModal/>
+                ):(
+                <ProfileLink displayName={displayName}></ProfileLink>
+            )}
+          </Nav>
+          <Navbar.Collapse id="topnav">
+            <Nav className="me-auto">
+              <NavLink href="/">Home</NavLink>
+              <NavLink href="/bills">Bills</NavLink>
+              <NavLink href="/testimonies">Testimony</NavLink>
+              <NavLink href="/about">About</NavLink>
+              <NavLink href="/legprocess">Learn</NavLink>
+              {authenticated && (
+                <div>
+                  <SignOut variant="secondary" size="sm"/>
+                </div>
+              )}
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
     </>
-  ) : (
-    <Nav className="me-auto">
-      <NavLink href="/login">Sign In</NavLink>
-    </Nav>
   )
 }
 
