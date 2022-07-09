cbd270b38a86accdf5508b599344efc9bdf0a194
sashamaryl <sashamaryl@tgmail.com>
2022-07-09 00:07:45 +0000

    created tabbed panes for profile edit page

--- components/ProfilePage/EditProfilePage.tsx
+++ components/ProfilePage/EditProfilePage.tsx
@@ -1,8 +1,27 @@
-import { ChangeEvent, FormEvent, useState } from "react"
+import {
+  ChangeEvent,
+  FormEvent,
+  SetStateAction,
+  useEffect,
+  useState
+} from "react"
+import styled from "styled-components"
 import { useAuth } from "../auth"
-import { Col, Container, Form, Row, Spinner, Tab, Tabs } from "../bootstrap"
+import {
+  Col,
+  Container,
+  Form,
+  Nav,
+  Row,
+  Spinner,
+  Tab,
+  Tabs
+} from "../bootstrap"
 import { Profile, ProfileHook, useProfile } from "../db"
 import { AboutMeEditForm } from "./AboutMeEditForm"
+import TabContainer from "react-bootstrap/TabContainer"
+import { TabContent, TabPane } from "react-bootstrap"
+import ViewTestimony from "../UserTestimonies/ViewTestimony"
 
 export function EditProfile() {
   const { user } = useAuth()
@@ -18,6 +37,45 @@ export function EditProfile() {
   )
 }
 
+const StyledTabNav = styled(Nav).attrs(props => ({
+  className: props.className
+}))`
+  text-align: center;
+  margin: 0 1rem;
+  font-family: Nunito;
+  font-size: 1.25rem;
+  color: var(--bs-dark);
+
+
+  .nav-item {
+    background-color: white;
+    width: 13rem;
+    overflow: visible;
+    height: 2.25em;
+    margin-left: 1rem;
+    z-index: 1;
+  }
+
+  .nav-item:first-child {
+    margin-left: 0;
+  }
+
+  .nav-item .active {
+    background-color: white;
+    height: 5rem;
+  }
+`
+
+const StyledTabContent = styled(TabContent)`
+  margin-top: -0.5rem;
+  z-index: -1;
+`
+
+const Header = styled.div`
+  font-size: 3.75rem;
+  font-family: Nunito;
+  font-weight: 500;
+`
 export function EditProfileForm({
   profile,
   actions
@@ -29,23 +87,31 @@ export function EditProfileForm({
 
   return (
     <Container>
-      <Row>
-        <Col>Edit Profile</Col>
-      </Row>
-      <Tabs activeKey={key}>
-        <Tab
-          title="About You"
-          eventKey="AboutYou"
-          onSelect={() => setKey("AboutYou")}
-        >
-          <AboutMeEditForm {...{ profile, actions }} />
-        </Tab>
-        <Tab
-          title="Testimonies"
-          eventKey="Testimonies"
-          onSelect={() => setKey("Testimonies")}
-        ></Tab>
-      </Tabs>
+      <Header>Edit Profile</Header>
+      <TabContainer activeKey={key} onSelect={(k: any) => setKey(k)}>
+        <StyledTabNav>
+          <Nav.Item>
+            <Nav.Link eventKey="AboutYou">About You</Nav.Link>
+          </Nav.Item>
+          <Nav.Item>
+            <Nav.Link eventKey="Testimonies">Testimonies</Nav.Link>
+          </Nav.Item>
+          <Nav.Item>
+            <Nav.Link eventKey="Following">Following</Nav.Link>
+          </Nav.Item>
+        </StyledTabNav>
+        <StyledTabContent>
+          <TabPane title="About You" eventKey="AboutYou">
+            <AboutMeEditForm {...{ profile, actions }} />
+          </TabPane>
+          <TabPane title="Testimonies" eventKey="Testimonies">
+            <ViewTestimony />
+          </TabPane>
+          <TabPane title="Following" eventKey="Following">
+            <ViewTestimony />
+          </TabPane>
+        </StyledTabContent>
+      </TabContainer>
     </Container>
   )
 }
