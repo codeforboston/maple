c2a956b520b6efdc127e828d07c295568e8709d2
Alex Ball <me@alexjball.com>
2022-07-23 16:54:59 -0400

    fix types

--- functions/src/profile/createProfile.ts
+++ functions/src/profile/createProfile.ts
@@ -3,7 +3,7 @@ import { db } from "../firebase"
 import { Profile } from "./types"
 
 export const createProfile = functions.auth.user().onCreate(async user => {
-  const profile: Profile = { auth: { role: "user" }, public: false }
+  const profile: Profile = { role: "user", public: false }
   if (user.displayName) profile.displayName = user.displayName
   await db.doc(`/profiles/${user.uid}`).set(profile, { merge: true })
 })
