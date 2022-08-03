2a498dbe9d603c8c651d5cd5a80f67cc50cc8296
jkoren <j.korenstein@gmail.com>
2022-08-02 20:17:30 -0400

    remove supporting code for tables

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,10 @@
 import React from "react"
 import AboutSection from "../components/AboutSection/AboutSection"
 import { useAuth } from "../components/auth"
-import { Button, Col, Container, Row } from "../components/bootstrap"
 import HeroHeader from "../components/HeroHeader/HeroHeader"
 import Leaf from "../components/Leaf/Leaf"
-import { Wrap } from "../components/links"
 import { createPage } from "../components/page"
 import TestimonyCalloutSection from "../components/TestimonyCallout/TestimonyCalloutSection"
-import ViewBillsOnHomePage from "../components/ViewBillsOnHomePage/ViewBillsOnHomePage"
-import Testimonies from "../components/TestimoniesOnHomePage/TestimoniesOnHomePage"
 
 export default createPage({
   Page: () => {
