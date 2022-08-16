d62e2acb68dfc574b815c9451d27db08aa95712b
Kep <KepCreations@gmail.com>
2022-08-16 16:00:30 -0400

    Basic layout for full screen done

--- components/HearingsScheduled/HearingsScheduled.tsx
+++ components/HearingsScheduled/HearingsScheduled.tsx
@@ -1,32 +1,105 @@
 import React, { useState } from "react"
 import { Container, Carousel } from "react-bootstrap"
-import { Button, Col, Image, Row } from "../bootstrap"
+import { Button, Col, Image, Row, Card } from "../bootstrap"
 import styles from "./HearingsScheduled.module.css"
+import { useUpcomingEvents } from "../db/events"
+import { Event, Session, Hearing } from "../../functions/src/events/types"
+
+export const formatDate = (
+  dateString: string
+): { day: string; month: string; date: string; time: string } => {
+  const daysOfWeek = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday"
+  ]
+  const months = [
+    "Jan",
+    "Feb",
+    "Mar",
+    "Apr",
+    "May",
+    "Jun",
+    "Jul",
+    "Aug",
+    "Sep",
+    "Oct",
+    "Nov",
+    "Dec"
+  ]
+  const date = new Date(dateString)
+  const day = daysOfWeek[new Date(date).getDay()]
+  const month = months[date.getMonth()]
+  const num = date.getDate().toString()
+
+  const [hourString, minute] = dateString.split("T")[1].split(":")
+  let hour = parseInt(hourString)
+  const meridian = hour < 12 ? "PM" : "AM"
+  if (hour > 12) hour -= 12
+  const formattedTime = `${hour.toString()}:${minute} ${meridian}`
+
+  return { day, month, date: num, time: formattedTime }
+}
+
+type EventData = {
+  type: string
+  name: string
+  id: number
+  location: string
+  fullDate: Date
+  month: string
+  date: string
+  day: string
+  time: string
+}
+
+const EventCard = ({
+  type,
+  name,
+  id,
+  location,
+  fullDate,
+  month,
+  date,
+  day,
+  time
+}: EventData) => {
+  const hearingBaseURL = "https://malegislature.gov/Events/Hearings/Detail/"
+  const sessionBaseURL = "https://malegislature.gov/Events/Sessions/Detail/"
+  return (
+    <div className={styles.card}>
+      <div className={styles.cardHeader}>
+        <div>
+          <p className={styles.month}>{month}</p>
+          <p className={styles.date}>{date}</p>
+        </div>
+        <div className={styles.day}>{day}</div>
+      </div>
+
+      <div className={styles.cardBody}>
+        <div>
+          <p className={styles.time}>{time}</p>
+        </div>
+        <div>
+          <p className={styles.name}>
+            {type === "hearings" ? (
+              <a href={`${hearingBaseURL}${id}`}>{name}</a>
+            ) : (
+              <a href={`${sessionBaseURL}${id}`}>{name}</a>
+            )}
+          </p>
+          <p className={styles.location}>{location}</p>
+        </div>
+      </div>
+    </div>
+  )
+}
 
 export const HearingsScheduled = () => {
-  // const hearings = [
-  //   {
-  //     date: new Date(),
-  //     place1: "Senate Session",
-  //     place2: "Senate Chamber"
-  //   },
-  //   {
-  //     date: new Date(),
-  //     place1: "Senate Session",
-  //     place2: "Senate Chamber"
-  //   },
-  //   {
-  //     date: new Date(),
-  //     place1: "Senate Session",
-  //     place2: "Senate Chamber"
-  //   },
-  //   {
-  //     date: new Date(),
-  //     place1: "Senate Session",
-  //     place2: "Senate Chamber"
-  //   }
-  // ]
-
   const Leaf = () => {
     return (
       <div className={styles.container}>
@@ -43,6 +116,45 @@ export const HearingsScheduled = () => {
   ): void => {
     setIndex(selectedIndex)
   }
+
+  let events = useUpcomingEvents()
+
+  const eventList: EventData[] = []
+  if (events) {
+    for (let e of events) {
+      const date = formatDate(e.content.EventDate)
+      if (e.type === "session") {
+        eventList.push({
+          type: e.type,
+          name: e.content.Name,
+          id: e.content.EventId,
+          location: e.content.LocationName,
+          fullDate: new Date(e.content.EventDate),
+          month: date.month,
+          date: date.date,
+          day: date.day,
+          time: date.time
+        })
+      } else if (e.type === "hearing") {
+        eventList.push({
+          type: e.type,
+          name: e.content.Name,
+          id: e.content.EventId,
+          location: e.content.Location.LocationName,
+          fullDate: new Date(e.content.EventDate),
+          month: date.month,
+          date: date.date,
+          day: date.day,
+          time: date.time
+        })
+      }
+    }
+  }
+  eventList.map(e => {
+    console.log(
+      `Event: ${e.name} ${e.id} ${e.location} ${e.month} ${e.date} ${e.day}`
+    )
+  })
 
   return (
     <>
@@ -69,18 +181,37 @@ export const HearingsScheduled = () => {
                 indicators={false}
                 activeIndex={index}
                 onSelect={handleSelect}
-                bsPrefix={styles.carousel}
+                // bsPrefix={styles.carousel}
               >
                 <Carousel.Item>
-                  <h2 className="text-center">August 2022</h2>
+                  <h1 className="text-center">August 2022</h1>
                 </Carousel.Item>
                 <Carousel.Item>
-                  <h2 className="text-center">September 2022</h2>
+                  <h1 className="text-center">September 2022</h1>
                 </Carousel.Item>
                 <Carousel.Item>
-                  <h2 className="text-center">October 2022</h2>
+                  <h1 className="text-center">October 2022</h1>
                 </Carousel.Item>
               </Carousel>
+            </section>
+
+            <section className={styles.eventSection}>
+              {eventList?.map(e => {
+                return (
+                  <EventCard
+                    key={e.id}
+                    type={e.type}
+                    name={e.name}
+                    id={e.id}
+                    location={e.location}
+                    fullDate={e.fullDate}
+                    month={e.month}
+                    date={e.date}
+                    day={e.day}
+                    time={e.time}
+                  />
+                )
+              })}
             </section>
           </Col>
         </Row>
--- components/db/common.ts
+++ components/db/common.ts
@@ -24,3 +24,7 @@ export function now() {
 }
 
 export type Maybe<T> = T | null | undefined
+
+export function midnight() {
+  return now().set({ hour: 0, minute: 0, second: 0, millisecond: 0 }).toJSDate()
+}
