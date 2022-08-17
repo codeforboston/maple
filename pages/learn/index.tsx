d78c869426c2e6e23586c6526b01b3a3d7528521
Rodrigo Passos <rodrigopassos@mac.com>
2022-08-16 22:55:55 -0400

    run fix

--- pages/learn/index.tsx
+++ pages/learn/index.tsx
@@ -3,19 +3,14 @@ import { createPage } from "../../components/page"
 export default createPage({
   title: "Learn",
   Page: () => {
-
-    return (
-      <div>
-        Learn
-      </div>
-    )
+    return <div>Learn</div>
   }
 })
 
 export async function getStaticProps() {
   return {
     redirect: {
-      destination: "/learn/basics-of-testimony",
-    },
+      destination: "/learn/basics-of-testimony"
+    }
   }
 }
