fecc31be3422fbb0f5674c527549b3ebcf5f441e
Robert Mrowiec <rmrowiec@icloud.com>
2022-08-17 21:21:56 -0400

    update few styles + import needed packages

--- components/UserTestimonies/PositionBug.tsx
+++ components/UserTestimonies/PositionBug.tsx
@@ -5,7 +5,7 @@ export type Position = "endorse" | "neutral" | "oppose"
 export function PositionLabel({ position }: { position: Position }) {
   return (
     <div
-      className={`h5 bg-${position} px-5 text-white rounded-pill`}
+      className={`h5 bg-${position} px-5 py-2 text-white rounded-pill`}
       style={{ width: "max-content" }}
     >
       {capitalize(position)}
