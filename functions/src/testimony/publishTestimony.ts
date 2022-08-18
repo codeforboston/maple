85e65b5d31b0ae61cd3931b3c1bad27b2c352546
Robert Mrowiec <rmrowiec@icloud.com>
2022-08-18 00:42:55 -0400

    verify user email_verified on publishTestimony

--- functions/src/testimony/publishTestimony.ts
+++ functions/src/testimony/publishTestimony.ts
@@ -15,9 +15,11 @@ const PublishTestimonyRequest = Record({
 })
 
 export const publishTestimony = https.onCall(async (data, context) => {
-  const uid = checkAuth(context)
-  const { draftId } = checkRequest(PublishTestimonyRequest, data)
+  const checkEmailVerification = true
+  const uid = checkAuth(context, checkEmailVerification)
 
+  const { draftId } = checkRequest(PublishTestimonyRequest, data)
+  
   let output: TransactionOutput
   try {
     output = await db.runTransaction(t =>
