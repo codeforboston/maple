0e48e96f63494d3859ad7c881441de5576ab78a3
Kep <KepCreations@gmail.com>
2022-08-24 16:19:38 -0400

    Update image paths to absolute path

--- components/CommunicatingWithLegislators/CommunicatingWithLegislatorsContent.tsx
+++ components/CommunicatingWithLegislators/CommunicatingWithLegislatorsContent.tsx
@@ -16,7 +16,7 @@ const WritingContent = () => (
       <Image
         className={`float-md-end ${styles.image}`}
         fluid
-        src="computertextblob.png"
+        src="/computertextblob.png"
         alt="computer display with lines representing text"
       />
     </Col>
@@ -29,7 +29,7 @@ const OralContent = () => (
       <Image
         className={styles.image}
         fluid
-        src="micandpaper.png"
+        src="/micandpaper.png"
         alt="microphone next to a sheet of paper"
       />
     </Col>
@@ -58,7 +58,7 @@ const WriteOrCallContent = () => (
       <Image
         className={`float-md-end ${styles.image}`}
         fluid
-        src="envelopewithletter.png"
+        src="/envelopewithletter.png"
         alt="envelope with letter sticking out"
       />
     </Col>
