be01144a24900183c740fe9edbdd1b1e1607bd3d
Rodrigo Passos <rodrigopassos@mac.com>
2022-08-23 23:41:30 -0400

    add Storybook with Typography and button components

---
+++ stories/components/buttons/TableButton.stories.tsx
@@ -0,0 +1,11 @@
+/* eslint-disable import/no-anonymous-default-export */
+import {
+  TableButton
+} from "../../../components/buttons";
+
+export default {
+  title: "Components/Buttons/TableButton",
+  component: TableButton,
+};
+
+export const Primary = () => <TableButton><>Table Button</></TableButton>
