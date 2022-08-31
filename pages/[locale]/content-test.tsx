7ba5a1b605c4dcac3aab0c63278b52debebb5a78
Rodrigo Passos <rodrigopassos@mac.com>
2022-08-30 21:45:51 -0400

    fix

--- pages/[locale]/content-test.tsx
+++ pages/[locale]/content-test.tsx
@@ -12,7 +12,29 @@ export default createPage({
 
         <div>
           <Trans i18nKey="common:manifesto-intro">
-            <p>As aspiring Software Craftsmen we are raising the bar of professional software development by practicing it and helping others learn the craft. Through this work we have come to value:</p><ul><li>Not only working software, but also well-crafted software</li><li>Not only responding to change, but also steadily adding value</li><li>Not only individuals and interactions, but also a community of professionals</li><li>Not only customer collaboration, but also productive partnerships</li></ul><p>That is, in pursuit of the items on the left we have found the items on the right to be indispensable.</p>
+            <p>
+              As aspiring Software Craftsmen we are raising the bar of
+              professional software development by practicing it and helping
+              others learn the craft. Through this work we have come to value:
+            </p>
+            <ul>
+              <li>Not only working software, but also well-crafted software</li>
+              <li>
+                Not only responding to change, but also steadily adding value
+              </li>
+              <li>
+                Not only individuals and interactions, but also a community of
+                professionals
+              </li>
+              <li>
+                Not only customer collaboration, but also productive
+                partnerships
+              </li>
+            </ul>
+            <p>
+              That is, in pursuit of the items on the left we have found the
+              items on the right to be indispensable.
+            </p>
           </Trans>
         </div>
       </div>
--- pages/content-test.tsx
+++ pages/content-test.tsx
@@ -1,2 +1,2 @@
-import { Redirect } from '../lib/redirect'
+import { Redirect } from "../lib/redirect"
 export default Redirect
