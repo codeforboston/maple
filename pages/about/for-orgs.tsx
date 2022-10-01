91c0593502c217ab27e35704e36f5fccfbeeb2ad
Nathan <sandersn@gmail.com>
2022-09-30 22:05:47 -0400

    fix component reference

--- pages/about/for-orgs.tsx
+++ pages/about/for-orgs.tsx
@@ -1,4 +1,4 @@
-import { createPage } from "../components/page"
+import { createPage } from "../../components/page"
 import ForOrgs from "../components/ForOrgs/ForOrgs"
 
 export default createPage({
