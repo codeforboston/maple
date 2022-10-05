e4af3efff118798ca6c91e558b9f6d46d6c99fda
Alex Ball <me@alexjball.com>
2022-10-05 19:09:19 -0400

    Generate skeleton stories

---
+++ stories/components/cards/SeeMore.stories.tsx
@@ -0,0 +1,13 @@
+import { createMeta } from "stories/utils"
+
+// TODO: move into components directory
+const SeeMore = () => <div>TODO</div>
+
+export default createMeta({
+  title: "Components/Cards/SeeMore",
+  figmaUrl:
+    "https://www.figma.com/file/3ifz37EOwDfmnEG8320KlD/CS1---MAPLE?node-id=119%3A2856",
+  component: SeeMore
+})
+
+export const Primary = () => <SeeMore />
---
+++ stories/dashboard/missionCenter/DashboardTabs.stories.tsx
@@ -0,0 +1,13 @@
+import { createMeta } from "stories/utils"
+
+// TODO: move into components directory
+const DashboardTabs = () => <div>TODO</div>
+
+export default createMeta({
+  title: "Dashboard/Mission Center/DashboardTabs",
+  figmaUrl:
+    "https://www.figma.com/file/3ifz37EOwDfmnEG8320KlD/CS1---MAPLE?node-id=245%3A13880",
+  component: DashboardTabs
+})
+
+export const Primary = () => <DashboardTabs />
