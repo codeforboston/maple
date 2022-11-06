f24820920c52c0c8a1bc2e11c62fa17928a083f8
Mark Trepanier <marktrepanier.cajigas@gmail.com>
2022-11-06 01:59:05 -0400

    adds "thumbs" up down and neut svg to public folder. Inlcudes svgs in positions with numerical data

--- components/HotBillCard/Positions.tsx
+++ components/HotBillCard/Positions.tsx
@@ -1,4 +1,8 @@
 import React from "react"
+import { Container, CardImg } from "react-bootstrap"
+import Image from "react-bootstrap/Image"
+import styled from "styled-components"
+import ThumbsUp from "../../public/Thumbs Up.svg"
 
 export const Positions = (props: {
   endorseCount: number
@@ -6,8 +10,57 @@ export const Positions = (props: {
   neutralCount: number
 }) => {
   return (
-    <div>
-      {props.endorseCount} {props.neutralCount} {props.opposeCount}
-    </div>
+    <PositionsStyle>
+      <PositionStyle>
+        <p>Endorse</p>
+        <div>
+          <Image
+            className="svg"
+            alt=""
+            src="Thumbs Up.svg"
+            style={{ fill: "#AB7C94" }}
+          />
+          <p>{props.endorseCount}</p>
+        </div>
+      </PositionStyle>
+      <PositionStyle>
+        <p>Neutral</p>
+        <div>
+          <Image className="svg" alt="" src="Thumbs Neut.svg" />
+          <p>{props.neutralCount}</p>
+        </div>
+      </PositionStyle>
+      <PositionStyle>
+        <p>Oppose</p>
+        <div>
+          <Image className="svg" alt="" src="Thumbs Down.svg" />
+          <p>{props.opposeCount}</p>
+        </div>
+      </PositionStyle>
+    </PositionsStyle>
   )
 }
+
+const PositionsStyle = styled.div`
+  display: flex;
+  margin-right: 15%;
+`
+const PositionStyle = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin-left: 10%;
+  margin-bottom: 0;
+  p {
+    margin: 0;
+  }
+  div {
+    margin: 5%;
+    display: flex;
+    align-items: center;
+    text-align: center;
+    .svg {
+      margin-right: 5%;
+    }
+  }
+`
