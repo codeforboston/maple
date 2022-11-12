68380a8ec26b74b522a6515d65b8019e496bf2b5
Nathan <sandersn@gmail.com>
2022-11-11 23:12:50 -0500

    placeholder legislator content

--- components/ForLegislatorsCardContent/ForLegislatorsCardContent.js
+++ components/ForLegislatorsCardContent/ForLegislatorsCardContent.js
@@ -7,14 +7,15 @@ import Link from "next/link"
 const WhyMAPLECardContent = () => (
   <>
     <h3 className={`text-right fw-bold mt-3 ${styles.calltoaction}`}>
-      MAPLE is a powerful communication tool for organizations, offered
-      completely for free.
+      MAPLE is a free, nonpartisan, nonprofit platform that makes it easier for
+      legislative offices to see what constituents from around the Commonwealth
+      are saying about bills sitting in any Committee.
     </h3>
     <p>
-      It takes only a couple minutes to sign up for your organization&apos;s
-      account on MAPLE, giving you immediate access to publish and share your
-      testimony, designate your priority bills, connect with your members and
-      followers, and research legislation.
+      Even before you create your own account, you can search our lightning-fast
+      platform for all testimony submitted by MAPLE users about any bill. Our
+      advanced featurs let you filter to bills sponsored by your office, bills
+      concerning your city, or bills that you choose to follow.
     </p>
   </>
 )
@@ -22,121 +23,11 @@ const WhyMAPLECardContent = () => (
 const BenefitsCardContent = () => (
   <>
     <h3 className={`text-left fw-bold mb-4 ${styles.orgsheader}`}>
-      Help your priorities &amp; positions reach a wider audience
+      Find out what constituents are saying about your priority bills
     </h3>
     <Row className="mb-3">
       <Col>
-        <p>
-          Your organization&apos;s testimony will be highlighted to MAPLE
-          visitors when they look up bills, with easy tools for sharing on
-          social media. Testimony published on MAPLE will be seen by
-          legislators, residents, members of the media, and other stakeholders.
-          The more organizations share their testimony on the platform, the more
-          people will see it. You can even designate bills as your legislative
-          priorities to signal your focus for each session.{" "}
-        </p>
-      </Col>
-    </Row>
-
-    <h3 className={`text-left fw-bold mb-4 ${styles.orgsheader}`}>
-      Connect with your members, and grow your reach
-    </h3>
-    <Row className="mb-3">
-      <Col>
-        <p>
-          MAPLE users can discover your profile when searching for their policy
-          interests and follow your organization to get updates on your
-          priorities and positions. MAPLE helps alert your constituents to the
-          most important bills on issues you care about, and helps you explain
-          why you support or oppose them.{" "}
-        </p>
-      </Col>
-    </Row>
-
-    <h3 className={`text-left fw-bold mb-4 ${styles.orgsheader}`}>
-      Coordinate legislative communication campaigns with ease
-    </h3>
-    <Row className="mb-3">
-      <Col>
-        <p>
-          By linking to a bill page on MAPLE, your constituents can submit their
-          own testimony with just a few clicks. MAPLE makes it easy for users of
-          any experience level to draft, format, and submit their testimony to
-          the right legislators for any bill.{" "}
-        </p>
-      </Col>
-    </Row>
-
-    <h3 className={`text-left fw-bold mb-4 ${styles.orgsheader}`}>
-      See what everyone&apos;s saying
-    </h3>
-    <Row className="mb-3">
-      <Col>
-        <p>
-          Even if you don&apos;t sign in, you can use MAPLE to browse testimony
-          submitted on any bill. Get a lay of the land and track public opinion
-          at a glance using our endorse / oppose ratios. Read the full testimony
-          of any user to find other stakeholders you may want to connect with or
-          learn from.{" "}
-        </p>
-      </Col>
-    </Row>
-
-    <h3 className={`text-left fw-bold mb-4 ${styles.orgsheader}`}>
-      Curate your testimony history
-    </h3>
-    <Row className="mb-3">
-      <Col>
-        <p>
-          When you submit your testimony through MAPLE, it automatically appears
-          in an indexed &amp; organized listing on your profile page. Use your
-          profile to automatically generate a record of your lobbying efforts
-          for your website, for member communications, or for semi-annual
-          filings. You can also publish previously submitted tesitmony on MAPLE
-          at any time.{" "}
-        </p>
-      </Col>
-    </Row>
-
-    <h3 className={`text-left fw-bold mb-4 ${styles.orgsheader}`}>
-      Speed up your legislative research
-    </h3>
-    <Row className="mb-3">
-      <Col>
-        <p>
-          MAPLE is fast. (Really fast.) Try{" "}
-          <Link href="/bills">
-            <a>searching for a bill</a>
-          </Link>{" "}
-          to see just how much MAPLE can speed up your legislative research
-          process; no sign in required!{" "}
-        </p>
-      </Col>
-    </Row>
-
-    <h3 className={`text-left fw-bold mb-4 ${styles.orgsheader}`}>
-      Help change the norms in Massachusetts
-    </h3>
-    <Row className="mb-3">
-      <Col>
-        <p>
-          The Commonwealth can be a leader in legislative transparency and good
-          government. Help shift the norms in Massachusetts by making your
-          testimony available for anyone to read.{" "}
-        </p>
-      </Col>
-    </Row>
-
-    <Row className="text-center">
-      <Col>
-        <h3 className={`fw-bold mt-3 ${styles.calltoaction}`}>
-          Ready to sign up for MAPLE?
-        </h3>
-      </Col>
-    </Row>
-    <Row className="text-center mb-3">
-      <Col>
-        <SignInWithModal />
+        <p>Lorem Ipsum.</p>
       </Col>
     </Row>
   </>
@@ -145,30 +36,17 @@ const BenefitsCardContent = () => (
 const ChallengeCardContent = () => (
   <>
     <h3 className={`text-left fw-bold mb-4 ${styles.orgsheader}`} id="clo">
-      Join MAPLE&apos;s Coalition to make the 193rd General Court the most
-      transparent in Massachusetts history!
+      Start using MAPLE today to put your finger on the pulse of your
+      constituents!
     </h3>
     <p>
-      Organizations that join MAPLE&apos;s Coalition of Lead Organizations (CLO)
-      commit to publishing all of their public testimony submitted to the 193rd
-      General Court through the MAPLE platform. The 193rd General Court is the
-      session that runs from January of 2023 through 2024.{" "}
-    </p>
-    <p>
-      In addition to all the benefits outlined above, these organizations will
-      have their profiles featured prominently on the MAPLE website during the
-      two-year legislative session. Any organization can contact MAPLE to be
-      considered for membership in the CLO. Organizations will be prioritized to
-      join the CLO based on the size of the constituency they represent.
-    </p>
-    <p>
-      Maple is a nonpartisan project and does not take political positions;
-      membership in the CLO does not indicate endorsement of any political
-      position by MAPLE or its parent organization, members, or collaborators.
+      You can browse testimony submitted by constituents across the state on
+      MAPLE, with or without creating your own account.
     </p>
     <h3 className={`text-right fw-bold mt-3 ${styles.calltoaction}`}>
       <a href="mailto:mapletestimony@gmail.com">
-        Join the MAPLE coalition&mdash;reach out to mapletestimony@gmail.com
+        Any questions? We&apos;re here to help&mdash;reach out to
+        mapletestimony@gmail.com
       </a>
     </h3>
   </>
