f946f16d8fa7ef18e0ec211d702c1f8a1cbc2602
sashamaryl <sashamaryl@tgmail.com>
2022-11-28 15:09:52 +0000

    begins labeling

--- maple/classification.py
+++ maple/classification.py
@@ -28,16 +28,42 @@ def regex_classification(action: Action) -> ActionType:
 
 
 _action_rules = [
-    (re.compile(".*referred.*to the committee on(.*)", re.I), ActionType.referred),
+    (re.compile(".*referred.*to the committee on(.*)", re.I), ActionType.referred), 
+    # if going to a joint committee, this indicates first_committee
+    # the second_committee will be: 
+    #   - ways and means
+    #   - steering and policy
+    #   - steering policy and scheduling
+    #   - rules
+    #   - Health Care Financing
+    #   - can be none
+    # within the chamber stages there will/can be referals to: 
+    #   - Health Care Financing
+    #   - Bonding, Capital Expenditures and State Assets
+    #   - bills in the third reading
+    #   - ways and means
+    #   - steering and policy
+    #   - steering policy and scheduling
+    #   - rules
+    #   - can be none
+    
+    
     (re.compile(".*to be engrossed.*", re.I), ActionType.engrossement),
+    # should be or move into stage chamber 2
     (re.compile(".*hearing scheduled.* ", re.I), ActionType.hearing_scheduled),
     (re.compile(".*accompanied a study order.*", re.I), ActionType.study_order),
     (re.compile("house concurred.*", re.I), ActionType.concurred),
-    (re.compile("senate concurred.*", re.I), ActionType.concurred),
-    (re.compile("(see|accompanied) .*", re.I), ActionType.reference),
+    (re.compile("senate concurred.*", re.I), ActionType.concurred), 
+    (re.compile("(see|accompanied) .*", re.I), ActionType.reference), 
     (re.compile("reporting date extended.*", re.I), ActionType.date_change),
     (re.compile("read ^\\w*", re.I), ActionType.reading),
+    # 2nd and 3rd readings happen in the chambers. 1st read isn't always clearly listed.
     (re.compile("hearing canceled.*", re.I), ActionType.cancellation),
     (re.compile("rules suspended", re.I), ActionType.rules_note),
     (re.compile("signed by the governor.*", re.I), ActionType.signed_by_governor),
 ]
+
+# To discuss tuesday
+# -- how/when to label for stages, or translate action types into stages
+# -- compound statuses solution
+# -- who is adding/editing regexes?
--- maple/types.py
+++ maple/types.py
@@ -21,6 +21,16 @@ class Status(Enum):
     signed = "signed by governor"
 
 
+class Stage(Enum):
+    introduced = "introduced"
+    first_committee = "first_committee"
+    second_committee = "second_committee"
+    first_chamber = "first_chamber"
+    second_chamber = "second_chamber"
+    signed_by_governor = "signed_by_governor"
+    not_staged = "not_staged"
+
+
 class Branch(Enum):
     """The branch of government associated with a bill or action."""
 
@@ -45,6 +55,62 @@ class ActionType(Enum):
     rules_note = "rules_note"
 
 
+class JointComnittees(Enum):
+    advanced_information_technology_the_internet_and_cybersecurity = (
+        "advanced_information_technology_the_internet_and_cybersecurity"
+    )
+    bonding_capital_expenditures_and_state_assets = (
+        "bonding_capital_expenditures_and_state_assets"
+    )
+    cannabis_policy_ = "cannabis_policy"
+    children_families_and_persons_with_disabilities = (
+        "children_families_and_persons_with_disabilities"
+    )
+    community_development_and_small_businesses = (
+        "community_development_and_small_businesses"
+    )
+    consumer_protection_and_professional_licensure = (
+        "consumer_protection_and_professional_licensure"
+    )
+    covid_19_and_emergency_preparedness_and_management = (
+        "covid-19_and_emergency_preparedness_and_management"
+    )
+    economic_development_and_emerging_technologies = (
+        "economic_development_and_emerging_technologies"
+    )
+    education = "education"
+    elder_affairs = "elder_affairs"
+    election_laws = "election_laws"
+    environment_natural_resources_and_agriculture = (
+        "environment_natural_resources_and_agriculture"
+    )
+    export_development = "export_development"
+    financial_services = "financial_services"
+    health_care_financing = "health_care_financing"
+    higher_education = "higher_education"
+    housing = "housing"
+    the_judiciary = "the_judiciary"
+    labor_and_workforce_development = "labor_and_workforce_development"
+    mental_health_substance_use_and_recovery = (
+        "mental_health_substance_use_and_recovery"
+    )
+    municipalities_and_regional_government = "municipalities_and_regional_government"
+    public_health = "public_health"
+    public_safety_and_homeland_security = "public_safety_and_homeland_security"
+    public_service = "public_service"
+    racial_equity_civil_rights_and_inclusion = (
+        "racial_equity_civil_rights_and_inclusion "
+    )
+    revenue = "revenue"
+    state_administration_and_regulatory_oversight = (
+        "state_administration_and_regulatory_oversight"
+    )
+    telecommunications_utilities_and_energy = "telecommunications_utilities_and_energy"
+    tourism_arts_and_cultural_development = "tourism_arts_and_cultural_development"
+    transportation = "transportation"
+
+
+
 @dataclass(frozen=True)
 class Action:
     """An action taken on a Bill."""
@@ -62,5 +128,5 @@ class Bill:
     history: list[Action]
 
     def status(self, on: datetime) -> Status:
-        """The status of the bill, as of some date. """
+        """The status of the bill, as of some date."""
         raise NotImplementedError()
