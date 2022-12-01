d4d796499b225f24eaf4583be83698476d67a35a
jellyyams <53629007+jellyyams@users.noreply.github.com>
2022-12-01 15:20:10 -0500

    add header to alert card

---
+++ components/dashboard/AlertCard.tsx
@@ -0,0 +1,22 @@
+import { CardTitle} from "components/Card"
+import { Card as MapleCard } from "../Card/Card"
+import Styles from "./AlertCard.module.css"
+
+export const AlertCard = (props: {
+  header: string
+  subheader: string
+  timestamp: string
+  headerImgSrc: string
+  bodyImgSrc: string
+  bodyText: string
+}) => {
+
+  const header = <CardTitle 
+  header={props.header} 
+  subheader={props.subheader}
+  timestamp={props.timestamp} 
+  imgSrc={props.headerImgSrc}
+  />
+
+  return <MapleCard headerElement={header} />
+}
--- stories/dashboard/newsfeed/AlertCard.stories.tsx
+++ stories/dashboard/newsfeed/AlertCard.stories.tsx
@@ -1,13 +1,26 @@
+import { ComponentStory } from "@storybook/react"
+import React, { useState } from "react"
 import { createMeta } from "stories/utils"
+import { AlertCard } from "components/dashboard/AlertCard"
 
-// TODO: move into components directory
-const AlertCard = () => <div>TODO</div>
 
-export default createMeta({
+export default createMeta({ 
   title: "Dashboard/Newsfeed/AlertCard",
   figmaUrl:
     "https://www.figma.com/file/3ifz37EOwDfmnEG8320KlD/CS1---MAPLE?node-id=158%3A2873",
   component: AlertCard
 })
 
-export const Primary = () => <AlertCard />
+const Template: ComponentStory<typeof AlertCard> = props => {
+  return <AlertCard {...props} />
+}
+
+export const Primary = Template.bind({})
+Primary.args = {
+  header: "This is an alert title",
+  subheader: "this is the subeheader", 
+  timestamp: "This is a timestamp", 
+  headerImgSrc:"https://upload.wikimedia.org/wikipedia/commons/thumb/6/69/How_to_use_icon.svg/2214px-How_to_use_icon.svg.png", 
+  bodyImgSrc:"",
+  bodyText: "This is the bodyText"
+} 
