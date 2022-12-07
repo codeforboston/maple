34d484718e068b34661da9ab20ba674fb267ef97
Merritt Baggett <mertbagt@gmail.com>
2022-12-07 15:34:39 -0500

    gear icon added to Settings button

--- components/ProfilePage/EditProfilePage.tsx
+++ components/ProfilePage/EditProfilePage.tsx
@@ -1,5 +1,6 @@
 import { useCallback, useState } from "react"
 import { TabPane } from "react-bootstrap"
+import Image from "react-bootstrap/Image"
 import TabContainer from "react-bootstrap/TabContainer"
 import { useAuth } from "../auth"
 import { Button, Col, Container, Nav, Row, Spinner } from "../bootstrap"
@@ -92,6 +93,16 @@ export function EditProfileForm({
               className={`btn btn-lg btn-outline-secondary`}
               disabled={!!formUpdated}
             >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="24"
+                height="24"
+                fill="currentColor"
+                className={`bi bi-gear-fill px-1 pb-1`}
+                viewBox="0 0 16 16"
+              >
+                <path d="M9.405 1.05c-.413-1.4-2.397-1.4-2.81 0l-.1.34a1.464 1.464 0 0 1-2.105.872l-.31-.17c-1.283-.698-2.686.705-1.987 1.987l.169.311c.446.82.023 1.841-.872 2.105l-.34.1c-1.4.413-1.4 2.397 0 2.81l.34.1a1.464 1.464 0 0 1 .872 2.105l-.17.31c-.698 1.283.705 2.686 1.987 1.987l.311-.169a1.464 1.464 0 0 1 2.105.872l.1.34c.413 1.4 2.397 1.4 2.81 0l.1-.34a1.464 1.464 0 0 1 2.105-.872l.31.17c1.283.698 2.686-.705 1.987-1.987l-.169-.311a1.464 1.464 0 0 1 .872-2.105l.34-.1c1.4-.413 1.4-2.397 0-2.81l-.34-.1a1.464 1.464 0 0 1-.872-2.105l.17-.31c.698-1.283-.705-2.686-1.987-1.987l-.311.169a1.464 1.464 0 0 1-2.105-.872l-.1-.34zM8 10.93a2.929 2.929 0 1 1 0-5.86 2.929 2.929 0 0 1 0 5.858z" />
+              </svg>
               {"Settings"}
             </Button>
           </Internal>
