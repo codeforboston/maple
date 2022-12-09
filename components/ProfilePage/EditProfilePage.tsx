e23bb717b64db0922bb3febf7188ac1b0adf12ee
Merritt Baggett <mertbagt@gmail.com>
2022-12-09 16:12:03 -0500

    toggles set up for Notifications and Profile Settings buttons

--- components/ProfilePage/EditProfilePage.tsx
+++ components/ProfilePage/EditProfilePage.tsx
@@ -93,7 +93,7 @@ export function EditProfileForm({
         <Col className={`d-flex justify-content-end`}>
           <Internal className={`ml-2`} href={`javascript:void(0)`}>
             <Button
-              className={`btn btn-lg btn-outline-secondary mx-4`}
+              className={`btn btn-lg btn-outline-secondary me-4`}
               disabled={!!formUpdated}
               onClick={() => setSettingsModal("show")}
             >
@@ -107,6 +107,8 @@ export function EditProfileForm({
               >
                 <path d="M9.405 1.05c-.413-1.4-2.397-1.4-2.81 0l-.1.34a1.464 1.464 0 0 1-2.105.872l-.31-.17c-1.283-.698-2.686.705-1.987 1.987l.169.311c.446.82.023 1.841-.872 2.105l-.34.1c-1.4.413-1.4 2.397 0 2.81l.34.1a1.464 1.464 0 0 1 .872 2.105l-.17.31c-.698 1.283.705 2.686 1.987 1.987l.311-.169a1.464 1.464 0 0 1 2.105.872l.1.34c.413 1.4 2.397 1.4 2.81 0l.1-.34a1.464 1.464 0 0 1 2.105-.872l.31.17c1.283.698 2.686-.705 1.987-1.987l-.169-.311a1.464 1.464 0 0 1 .872-2.105l.34-.1c1.4-.413 1.4-2.397 0-2.81l-.34-.1a1.464 1.464 0 0 1-.872-2.105l.17-.31c.698-1.283-.705-2.686-1.987-1.987l-.311.169a1.464 1.464 0 0 1-2.105-.872l-.1-.34zM8 10.93a2.929 2.929 0 1 1 0-5.86 2.929 2.929 0 0 1 0 5.858z" />
               </svg>
+              {/*svg doesn't properly inherit "currentColor" if it's placed in
+               an Image component between the svg and the Button component */}
               {"Settings"}
             </Button>
           </Internal>
--- components/ProfilePage/NotificationSettingsModal.tsx
+++ components/ProfilePage/NotificationSettingsModal.tsx
@@ -1,5 +1,6 @@
+import { useState } from "react"
 import type { ModalProps } from "react-bootstrap"
-import { Button, Col, Container, Image, Modal, Row, Stack } from "../bootstrap"
+import { Button, Col, Image, Modal, Stack } from "../bootstrap"
 import styles from "./NotificationSettingsModal.module.css"
 
 export default function NotificationSettingsModal({
@@ -9,6 +10,13 @@ export default function NotificationSettingsModal({
 }: Pick<ModalProps, "show" | "onHide"> & {
   onCancelClick: () => void
 }) {
+  const [notifications, setNotifications] = useState<"enable" | "enabled">(
+    "enable"
+  ) //replace initial state with User data
+  const [profileSettings, setProfileSettings] = useState<"enable" | "enabled">(
+    "enable"
+  ) //replace initial state with User data
+
   return (
     <Modal
       show={show}
@@ -19,13 +27,13 @@ export default function NotificationSettingsModal({
       <Modal.Header closeButton>
         <Modal.Title id="notifications-modal">Settings</Modal.Title>
       </Modal.Header>
-      <Modal.Body className={styles.xthinContainer}>
+      <Modal.Body className={styles.thinContainer}>
         <Stack>
           &nbsp; Notifications
           <hr className={`mt-0`} />
         </Stack>
         <Stack
-          className={`${styles.NotificationSmallFontSize}`}
+          className={`${styles.ModalFontSize}`}
           direction={`horizontal`}
           gap={3}
         >
@@ -33,23 +41,42 @@ export default function NotificationSettingsModal({
             Would you like to receive updates about bills/organizations you
             follow through email?
           </Col>
-          <Button className={`btn btn-sm btn-outline-secondary ms-auto py-1`}>
-            <Image
-              className={`pe-1`}
-              src="/mail-2.svg"
-              alt="open envelope with letter"
-              width="22"
-              height="19"
-            />
-            {"Enable"}
-          </Button>
+          {notifications === "enable" ? (
+            <Button
+              className={`btn btn-sm btn-outline-secondary ms-auto py-1 ${styles.ModalButtonLength}`}
+              onClick={() => setNotifications("enabled")}
+            >
+              <Image
+                className={`pe-1`}
+                src="/mail-2.svg"
+                alt="open envelope with letter, toggles update frequency options"
+                width="22"
+                height="19"
+              />
+              {"Enable"}
+            </Button>
+          ) : (
+            <Button
+              className={`btn btn-sm btn-secondary ms-auto py-1 ${styles.ModalButtonLength}`}
+              onClick={() => setNotifications("enable")}
+            >
+              <Image
+                className={`pe-1`}
+                src="/mail-2.svg"
+                alt="open envelope with letter, toggles update frequency options"
+                width="22"
+                height="19"
+              />
+              {"Enabled"}
+            </Button>
+          )}
         </Stack>
         <Stack className={`pt-5`}>
           &nbsp; Profile Settings
           <hr className={`mt-0`} />
         </Stack>
         <Stack
-          className={`${styles.NotificationSmallFontSize}`}
+          className={`${styles.ModalFontSize}`}
           direction={`horizontal`}
           gap={3}
         >
@@ -57,11 +84,21 @@ export default function NotificationSettingsModal({
             Don't make my profile public. (Your name will still be associated
             with your testimony.)
           </Col>
-          <Button
-            className={`btn btn-sm btn-outline-secondary ms-auto py-1 ${styles.NotificationButtonLength}`}
-          >
-            {"Enable"}
-          </Button>
+          {profileSettings === "enable" ? (
+            <Button
+              className={`btn btn-sm btn-outline-secondary ms-auto py-1 ${styles.ModalButtonLength}`}
+              onClick={() => setProfileSettings("enabled")}
+            >
+              {"Enable"}
+            </Button>
+          ) : (
+            <Button
+              className={`btn btn-sm btn-secondary ms-auto py-1 ${styles.ModalButtonLength}`}
+              onClick={() => setProfileSettings("enable")}
+            >
+              {"Enabled"}
+            </Button>
+          )}
         </Stack>
         <Stack
           className={`d-flex justify-content-end pt-4`}
@@ -81,13 +118,12 @@ export default function NotificationSettingsModal({
 }
 
 /*
-  Notification button -> Enable, Enabled
-                         Enabled activates How Often Drop Down
-                              Set up Drop Down
-                         State -> Enable, Enabled (with How Often)
-  Profie button -> Enable, Enabled?
-                   State -> Enable, Enabled
-  Continue -> Update Backend with State and Close Modal
+  Notification button -> [x] Enable, Enabled
+                         [ ] Enabled activates How Often Drop Down
+                         [ ]     Set up Drop Down
+                         [ ] State -> Enable, Enabled (with How Often)
+  Continue -> [ ] Update Backend with Notifications & Profile Settings State 
+              [ ] then Close Modal
 
-  tweak :hover and :focus for all buttons?
+  tweak :hover and :focus for all/some buttons?
 */
