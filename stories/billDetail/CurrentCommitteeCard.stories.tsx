e1281af5d752137fc36f3b45ab9e9be6fa6b84d2
Tom Magnusson <tommagnuss@gmail.com>
2022-12-11 15:42:52 -0500

    adds CurrentCommitteeCard component and story

--- stories/billDetail/CurrentCommitteeCard.stories.tsx
+++ stories/billDetail/CurrentCommitteeCard.stories.tsx
@@ -1,7 +1,8 @@
+import { ComponentStory } from "@storybook/react"
 import { createMeta } from "stories/utils"
+import { CurrentCommitteeCard } from "../../components/CurrentCommitteeCard/CurrentCommitteeCard"
 
 // TODO: move into components directory
-const CurrentCommitteeCard = () => <div>TODO</div>
 
 export default createMeta({
   title: "Bill Detail/CurrentCommitteeCard",
@@ -10,4 +11,9 @@ export default createMeta({
   component: CurrentCommitteeCard
 })
 
-export const Primary = () => <CurrentCommitteeCard />
+const Template: ComponentStory<typeof CurrentCommitteeCard> = args => (
+  <CurrentCommitteeCard {...args} />
+)
+
+export const Primary = Template.bind({})
+Primary.args = { chamber: "House", committee: "Committee of Ways and Means" }
