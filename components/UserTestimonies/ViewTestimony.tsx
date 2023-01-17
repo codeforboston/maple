17d3d0e1db6a3fc7a6ea4ec34a82f8558c4e5dcd
Mark Trepanier <marktrepanier.cajigas@gmail.com>
2023-01-17 18:30:30 -0500

    allows dropdown to change title on click of menu item

--- components/UserTestimonies/ViewTestimony.tsx
+++ components/UserTestimonies/ViewTestimony.tsx
@@ -20,6 +20,7 @@ import { PositionLabel } from "./PositionBug"
 import { Card as MapleCard } from "components/Card"
 import { Tabs, Tab } from "./Tabs"
 import { current } from "@reduxjs/toolkit"
+import { RelevantSortConnector } from "instantsearch.js/es/connectors/relevant-sort/connectRelevantSort"
 
 const ViewTestimony = (
   props: UsePublishedTestimonyListing & {
@@ -43,13 +44,13 @@ const ViewTestimony = (
 
   const [activeTab, setActiveTab] = useState(1)
 
-  const handleClick = (e: Event, value: number) => {
+  const handleTabClick = (e: Event, value: number) => {
     setActiveTab(value)
   }
 
   const handleOrderClick = (e: React.MouseEvent<Element>) => {
     e.preventDefault()
-    console.log("hey")
+    setOrderBy((e.currentTarget as HTMLElement).innerText)
   }
 
   const tabs = [
@@ -58,19 +59,16 @@ const ViewTestimony = (
     <Tab key="oo" label="Organizations Only" active={false} value={3} />
   ]
   const body = (
-    <TitledSectionCard
-      className={className}
-      // bug={<SortTestimonyDropDown orderBy={orderBy} setOrderBy={setOrderBy} />}
-    >
+    <TitledSectionCard className={className}>
       {testimony.length > 0 && (
         <Tabs
           childTabs={tabs}
-          onChange={handleClick}
+          onChange={handleTabClick}
           selectedTab={activeTab}
         ></Tabs>
       )}
       <div>
-        <DropDownMenu cliik={handleOrderClick} currentOrder={orderBy} />
+        <DropDownMenu handleOrder={handleOrderClick} currentOrder={orderBy} />
       </div>
       {testimony.length > 0 ? (
         testimony.map(t => (
@@ -272,9 +270,9 @@ export const FormattedTestimonyContent = ({
 
 export const DropDownMenu = (props: {
   currentOrder: string
-  cliik?: ReactEventHandler
+  handleOrder?: ReactEventHandler
 }) => {
-  const { cliik, currentOrder } = props
+  const { handleOrder, currentOrder } = props
   return (
     <Dropdown>
       <Dropdown.Toggle variant="success" id="dropdown-basic">
@@ -282,8 +280,8 @@ export const DropDownMenu = (props: {
       </Dropdown.Toggle>
 
       <Dropdown.Menu>
-        <Dropdown.Item onClick={cliik}>Most Recent</Dropdown.Item>
-        <Dropdown.Item onClick={cliik}>Oldest</Dropdown.Item>
+        <Dropdown.Item onClick={handleOrder}>Most Recent</Dropdown.Item>
+        <Dropdown.Item onClick={handleOrder}>Oldest</Dropdown.Item>
       </Dropdown.Menu>
     </Dropdown>
   )
