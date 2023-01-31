5b90e57f58a3cbab6f4262fc5632513cbd7b44d3
Gerlin <68314210+gerlinp@users.noreply.github.com>
2023-01-31 02:26:47 -0500

    Lint error and tabs

--- components/Policies/PolicyPage.tsx
+++ components/Policies/PolicyPage.tsx
@@ -7,22 +7,27 @@ export default function PolicyPage() {
   const [policycontent, setpolicy] = useState<
     "terms-of-service" | "privacy-policy" | "code-of-conduct"
   >("privacy-policy")
-  const handleOnClick = (e: any) => {
+
+  const [toggleCurrentTab, setToggleTab] = useState(1);
+
+  const handleOnClick = (e: any, index: any) => {
+    setToggleTab(index)
     setpolicy(e.target.id)
   }
 
   return (
     <Container className={style.policyContent}>
       <h1>Policies</h1>
+      
       <Stack direction="horizontal">
-        <Button id="privacy-policy" onClick={handleOnClick}>
-          Privacy Policy
+        <Button className={`${style[toggleCurrentTab === 1 ? "currentTab" : "tab"]}`} id="privacy-policy" onClick={(e) => handleOnClick(e,1)}>
+          Privacy <br/> Policy
         </Button>
-        <Button id="terms-of-service" onClick={handleOnClick}>
-          Terms of Service
+        <Button className={`${style[toggleCurrentTab === 2 ? "currentTab" : "tab"]}`} id="terms-of-service" onClick={(e) => handleOnClick(e,2)}>
+          Terms <br/> of Service
         </Button>
-        <Button id="code-of-conduct" onClick={handleOnClick}>
-          Code of Conduct
+        <Button className={`${style[toggleCurrentTab === 3 ? "currentTab" : "tab"]}`} id="code-of-conduct" onClick={(e) => handleOnClick(e,3)}>
+          Code of <br/> Conduct
         </Button>
       </Stack>
 
