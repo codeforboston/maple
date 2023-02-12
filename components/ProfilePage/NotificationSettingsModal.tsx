a58748425283db19101d16a9e7d24a184bcb0fae
Alex Ball <me@alexjball.com>
2023-02-12 15:27:11 -0500

    run yarn fix

--- components/ProfilePage/NotificationSettingsModal.tsx
+++ components/ProfilePage/NotificationSettingsModal.tsx
@@ -62,72 +62,72 @@ export default function NotificationSettingsModal({
             &nbsp; Notifications
             <hr className={`mt-0`} />
           </Stack>
-          <Stack className={`${styles.modalFontSize}`} direction={`horizontal`}>     
-          <Col className={`col-8`}>
-            Would you like to receive updates about bills/organizations you
-            follow through email?
-          </Col>          
-          <Button
-            className={`
+          <Stack className={`${styles.modalFontSize}`} direction={`horizontal`}>
+            <Col className={`col-8`}>
+              Would you like to receive updates about bills/organizations you
+              follow through email?
+            </Col>
+            <Button
+              className={`
               btn btn-sm ms-auto py-1 ${styles.modalButtonLength} ${buttonSecondary}
             `}
-            onClick={() =>
-              setNotifications(notifications === "None" ? "Monthly" : "None")
-            }
-          >          
-            <Image
-              className={`pe-1`}
-              src="/mail-2.svg"
-              alt="open envelope with letter, toggles update frequency options"
-              width="22"
-              height="19"
-            />       
-            {notifications === "None" ? "Enable" : "Enabled"}          
-          </Button>
-        </Stack>        
-        <Stack
-          className={`
+              onClick={() =>
+                setNotifications(notifications === "None" ? "Monthly" : "None")
+              }
+            >
+              <Image
+                className={`pe-1`}
+                src="/mail-2.svg"
+                alt="open envelope with letter, toggles update frequency options"
+                width="22"
+                height="19"
+              />
+              {notifications === "None" ? "Enable" : "Enabled"}
+            </Button>
+          </Stack>
+          <Stack
+            className={`
           pt-3 ${styles.modalFontSize} 
           ${notifications === "None" ? "invisible" : ""} 
         `}
-          direction={`horizontal`}
-        >        
-          <Col className={`col-8`}>
-            How often would you like to receive emails?
-          </Col>        
-          <Dropdown className={`d-inline-block ms-auto`}>
-            <Dropdown.Toggle
-              className={`btn-sm py-1 ${styles.modalButtonLength}`}
-              variant="outline-secondary"
-              id="dropdown-basic"
-            >
-              {notifications}
-            </Dropdown.Toggle>
+            direction={`horizontal`}
+          >
+            <Col className={`col-8`}>
+              How often would you like to receive emails?
+            </Col>
+            <Dropdown className={`d-inline-block ms-auto`}>
+              <Dropdown.Toggle
+                className={`btn-sm py-1 ${styles.modalButtonLength}`}
+                variant="outline-secondary"
+                id="dropdown-basic"
+              >
+                {notifications}
+              </Dropdown.Toggle>
 
-            <Dropdown.Menu>
-              <Dropdown.Item onClick={() => setNotifications("Daily")}>
-                Daily
-              </Dropdown.Item>
-              <Dropdown.Item onClick={() => setNotifications("Weekly")}>
-                Weekly
-              </Dropdown.Item>
-              <Dropdown.Item onClick={() => setNotifications("Monthly")}>
-                Monthly
-              </Dropdown.Item>
-            </Dropdown.Menu>
-          </Dropdown>
-        </Stack>         
-        <Stack className={`pt-4`}>
-          &nbsp; Profile Settings
-          <hr className={`mt-0`} />
-        </Stack>    
-        <Stack className={`${styles.modalFontSize}`} direction={`horizontal`}>
-          <Col className={`col-8`}>
-            Don't make my profile public. (Your name will still be associated
-            with your testimony.)
-          </Col>
-          <Button
-            className={`
+              <Dropdown.Menu>
+                <Dropdown.Item onClick={() => setNotifications("Daily")}>
+                  Daily
+                </Dropdown.Item>
+                <Dropdown.Item onClick={() => setNotifications("Weekly")}>
+                  Weekly
+                </Dropdown.Item>
+                <Dropdown.Item onClick={() => setNotifications("Monthly")}>
+                  Monthly
+                </Dropdown.Item>
+              </Dropdown.Menu>
+            </Dropdown>
+          </Stack>
+          <Stack className={`pt-4`}>
+            &nbsp; Profile Settings
+            <hr className={`mt-0`} />
+          </Stack>
+          <Stack className={`${styles.modalFontSize}`} direction={`horizontal`}>
+            <Col className={`col-8`}>
+              Don't make my profile public. (Your name will still be associated
+              with your testimony.)
+            </Col>
+            <Button
+              className={`
               btn btn-sm ms-auto py-1 ${styles.modalButtonLength}
                 ${
                   isProfilePublic === true
