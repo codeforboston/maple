a7b00a707b416ee597d9e9525d1b0424e23e21f9
Merritt Baggett <mertbagt@gmail.com>
2023-02-14 09:59:53 -0500

    refactored out NotificationSettingsModal.module.css

--- components/ProfilePage/NotificationSettingsModal.tsx
+++ components/ProfilePage/NotificationSettingsModal.tsx
@@ -3,9 +3,8 @@ import type { ModalProps } from "react-bootstrap"
 import Dropdown from "react-bootstrap/Dropdown"
 import styled from "styled-components"
 import { Frequency } from "../auth"
-import { Button, Col, Form, Image, Modal, Stack } from "../bootstrap"
+import { Button, Col, Form, Image, Modal, Row, Stack } from "../bootstrap"
 import { ProfileHook } from "../db"
-import styles from "./NotificationSettingsModal.module.css"
 
 type Props = Pick<ModalProps, "show" | "onHide"> & {
   actions: ProfileHook
@@ -28,6 +27,10 @@ const StyledDropdownToggle = styled(Dropdown.Toggle)`
 
 const StyledModalBody = styled(Modal.Body)`
   padding: 0.8rem;
+`
+
+const StyledRow = styled(Row)`
+  font-size: 12px;
 `
 
 export default function NotificationSettingsModal({
@@ -75,85 +78,88 @@ export default function NotificationSettingsModal({
             &nbsp; Notifications
             <hr className={`mt-0`} />
           </Stack>
-          <Stack className={`${styles.modalFontSize}`} direction={`horizontal`}>
+          <StyledRow>
             <Col className={`col-8`}>
               Would you like to receive updates about bills/organizations you
               follow through email?
             </Col>
-            <StyledButton
-              className={`
-              btn btn-sm ms-auto py-1 ${buttonSecondary}
-            `}
-              onClick={() =>
-                setNotifications(notifications === "None" ? "Monthly" : "None")
-              }
-            >
-              <Image
-                className={`pe-1`}
-                src="/mail-2.svg"
-                alt="open envelope with letter, toggles update frequency options"
-                width="22"
-                height="19"
-              />
-              {notifications === "None" ? "Enable" : "Enabled"}
-            </StyledButton>
-          </Stack>
-          <Stack
-            className={`
-              pt-3 ${styles.modalFontSize} 
-              ${notifications === "None" ? "invisible" : ""} 
-            `}
+            <Col>
+              <StyledButton
+                className={`btn btn-sm d-flex justify-content-end ms-auto py-1 ${buttonSecondary}`}
+                onClick={() =>
+                  setNotifications(
+                    notifications === "None" ? "Monthly" : "None"
+                  )
+                }
+              >
+                <Image
+                  className={`pe-1`}
+                  src="/mail-2.svg"
+                  alt="open envelope with letter, toggles update frequency options"
+                  width="22"
+                  height="19"
+                />
+                {notifications === "None" ? "Enable" : "Enabled"}
+              </StyledButton>
+            </Col>
+          </StyledRow>
+          <StyledRow
+            className={`pt-3 ${notifications === "None" ? "invisible" : ""}`}
             direction={`horizontal`}
           >
             <Col className={`col-8`}>
               How often would you like to receive emails?
             </Col>
-            <Dropdown className={`d-inline-block ms-auto`}>
-              <StyledDropdownToggle
-                className={`btn-sm py-1`}
-                variant="outline-secondary"
-                id="dropdown-basic"
-              >
-                {notifications}
-              </StyledDropdownToggle>
-              <Dropdown.Menu>
-                <Dropdown.Item onClick={() => setNotifications("Daily")}>
-                  Daily
-                </Dropdown.Item>
-                <Dropdown.Item onClick={() => setNotifications("Weekly")}>
-                  Weekly
-                </Dropdown.Item>
-                <Dropdown.Item onClick={() => setNotifications("Monthly")}>
-                  Monthly
-                </Dropdown.Item>
-              </Dropdown.Menu>
-            </Dropdown>
-          </Stack>
+            <Col className={`d-flex justify-content-end`}>
+              <Dropdown className={`d-inline-block ms-auto`}>
+                <StyledDropdownToggle
+                  className={`btn-sm py-1`}
+                  variant="outline-secondary"
+                  id="dropdown-basic"
+                >
+                  {notifications}
+                </StyledDropdownToggle>
+                <Dropdown.Menu>
+                  <Dropdown.Item onClick={() => setNotifications("Daily")}>
+                    Daily
+                  </Dropdown.Item>
+                  <Dropdown.Item onClick={() => setNotifications("Weekly")}>
+                    Weekly
+                  </Dropdown.Item>
+                  <Dropdown.Item onClick={() => setNotifications("Monthly")}>
+                    Monthly
+                  </Dropdown.Item>
+                </Dropdown.Menu>
+              </Dropdown>
+            </Col>
+          </StyledRow>
           <Stack className={`pt-4`}>
             &nbsp; Profile Settings
             <hr className={`mt-0`} />
           </Stack>
-          <Stack className={`${styles.modalFontSize}`} direction={`horizontal`}>
+          <StyledRow>
             <Col className={`col-8`}>
               Don't make my profile public. (Your name will still be associated
               with your testimony.)
             </Col>
-            <StyledButton
-              className={`
-              btn btn-sm ms-auto py-1
-                ${
-                  isProfilePublic === true
-                    ? "btn-outline-secondary"
-                    : "btn-secondary"
+            <Col>
+              <StyledButton
+                className={`
+                  btn btn-sm d-flex justify-content-center ms-auto py-1
+                  ${
+                    isProfilePublic === true
+                      ? "btn-outline-secondary"
+                      : "btn-secondary"
+                  }
+                `}
+                onClick={() =>
+                  setIsProfilePublic(isProfilePublic === true ? false : true)
                 }
-              `}
-              onClick={() =>
-                setIsProfilePublic(isProfilePublic === true ? false : true)
-              }
-            >
-              {isProfilePublic === true ? "Enable" : "Enabled"}
-            </StyledButton>
-          </Stack>
+              >
+                {isProfilePublic === true ? "Enable" : "Enabled"}
+              </StyledButton>
+            </Col>
+          </StyledRow>
           <Stack
             className={`d-flex justify-content-end pt-4`}
             direction={`horizontal`}
