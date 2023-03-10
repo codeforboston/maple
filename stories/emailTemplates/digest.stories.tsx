02eddd831c9467b68cafb1416f0c1524bd4f9a0b
Merritt Baggett <mertbagt@gmail.com>
2023-03-10 09:59:31 -0500

    bill counters

--- stories/emailTemplates/digest.stories.tsx
+++ stories/emailTemplates/digest.stories.tsx
@@ -67,22 +67,27 @@ DigestEmail.args = {
     notificationFrequency: "weekly",
     startDate: "2/1/2023",
     endDate: "2/7/2023",
-    BillTitle2: "H.98",
-    BillSubtitle2: "An Act to further govt. transparency",
     Bill1: {
       Title: "H.1289",
       Subtitle:
-        "An Act to protect the civil rights and safety of all Massachusetts residents"
+        "An Act to protect the civil rights and safety of all Massachusetts residents",
+      Testimonies: "12",
+      Cosponsors: "2",
+      HearingDate: "Jan 31"
     },
     Bill2: {
       Title: "H.98",
-      Subtitle: "An Act to further govt. transparency"
+      Subtitle: "An Act to further govt. transparency",
+      Testimonies: "2",
+      Cosponsors: "0",
+      HearingDate: "-"
     },
     Org1: {
       Title: "Boston’s Teacher Union",
-      Counter: "2",
+      Counter: "3",
       Item1: "S.77",
-      Item2: "S.1022"
+      Item2: "S.1022",
+      Item3: "S.64"
     },
     Org2: {
       Title: "Moms for Liberty",
