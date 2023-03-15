d6ab0c4001b91bd2c79af1d01aac9ea8610e7bbf
Alex Ball <me@alexjball.com>
2023-03-14 22:01:08 -0400

    add an example helper. To add more export additional functions from the helpers.ts module.
    See also https://handlebarsjs.com/guide/block-helpers.html#basic-blocks

---
+++ functions/src/email/partials/index.ts
@@ -0,0 +1,17 @@
+import bill from "./bills/bill.handlebars"
+import bills from "./bills/bills.handlebars"
+import footer from "./footer.handlebars"
+import header from "./header.handlebars"
+import newsFeedLink from "./newsFeedLink.handlebars"
+import noUpdates from "./noUpdates.handlebars"
+import orgs from "./orgs/orgs.handlebars"
+
+export const partials = {
+  bill,
+  bills,
+  footer,
+  header,
+  newsFeedLink,
+  noUpdates,
+  orgs
+}
