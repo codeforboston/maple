db583c60fcd4b573bc5533613d82c3c63d1de04d
Merritt Baggett <mertbagt@gmail.com>
2023-03-15 12:37:41 -0400

    noUpdates partial and formatting

--- functions/src/email/helpers.ts
+++ functions/src/email/helpers.ts
@@ -1,3 +1,20 @@
 export function isDefined(v: unknown) {
   return v !== undefined
 }
+
+export function toLowerCase(aString: string) {
+  return aString.toLowerCase()
+}
+
+export function noUpdatesFormat(aString: string) {
+  switch (aString) {
+    case "Monthly":
+      return "this month"
+      break
+    case "Weekly":
+      return "this week"
+      break
+    default:
+      return "today"
+  }
+}
