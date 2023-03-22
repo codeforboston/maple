4b417ab75eb6d6741b607fddf497e95dfafe5222
Mark Trepanier <marktrepanier.cajigas@gmail.com>
2023-03-22 18:51:30 -0400

    fixes nomenclature bug

--- components/db/testimony/useEditTestimony.test.ts
+++ components/db/testimony/useEditTestimony.test.ts
@@ -36,7 +36,7 @@ beforeEach(() => {
     court: court,
     content: "fake testimony",
     position: "endorse",
-    authorRole: "Individual"
+    authorRole: "individual"
   }
   testimony = {
     authorUid: uid,
@@ -45,7 +45,7 @@ beforeEach(() => {
     content: draft.content,
     court: court,
     position: draft.position,
-    authorRole: "Individual",
+    authorRole: "individual",
     version: 1
   }
   updatedDraft = { ...draft, content: "update", position: "oppose" }
