14e8e199571f8a624b72aa479513f0eaafbff169
Mark Trepanier <marktrepanier.cajigas@gmail.com>
2023-03-31 16:39:17 -0400

    adds useAuth and pulls authorId from useCurrentTestimonyDetails

--- components/testimony/TestimonyDetailPage/TestimonyDetailPage.tsx
+++ components/testimony/TestimonyDetailPage/TestimonyDetailPage.tsx
@@ -3,10 +3,15 @@ import { Col, Container, Row } from "react-bootstrap"
 import { BillTitle } from "./BillTitle"
 import { PolicyActions } from "./PolicyActions"
 import { RevisionHistory } from "./RevisionHistory"
+import { useCurrentTestimonyDetails } from "./testimonyDetailSlice"
 import { TestimonyDetail } from "./TestimonyDetail"
 import { VersionBanner } from "./TestimonyVersionBanner"
+import { useAuth } from "components/auth"
 
 export const TestimonyDetailPage: FC = () => {
+  const { authorUid } = useCurrentTestimonyDetails()
+  const { user } = useAuth()
+  const isUser = user?.uid === authorUid
   return (
     <>
       <VersionBanner fluid="xl" />
