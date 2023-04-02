2d0a7d9bc6fdca301478ee8530adfe8b3aef9976
Alex Ball <me@alexjball.com>
2023-04-02 17:51:14 -0400

    simplify/fix firebase auth redirect

--- next.config.js
+++ next.config.js
@@ -33,30 +33,14 @@ module.exports = {
  * Redirect auth handler paths to the hosted firebase pages. This avoids having
  * to implement these pages ourselves in next.js but continues to depend on
  * firebase hosting a bit.
- *
- * Set MAPLE_ENV to "prod" to redirect to the production site. This is required
- * in production mode.
- *
- * Set MAPLE_ENV to "dev" to redirect to the development site. This is the
- * default in dev mode. To set up redirection, set MAPLE_DEV environment
- * variable to "dev" or "prod".
+ *b
+ * In production, FIREBASE_AUTH_DOMAIN is set to digital-testimony-prod.web.app.
  *
  * @see https://firebase.google.com/docs/auth/custom-email-handler
  */
 const redirectFirebaseAuthHandlers = () => {
-  let firebaseDomain
-  if (process.env.MAPLE_ENV === "prod") {
-    firebaseDomain = "mapletestimony.org"
-  } else if (
-    process.env.MAPLE_ENV === "dev" ||
-    process.env.NODE_ENV === "development"
-  ) {
-    firebaseDomain = "digital-testimony-dev.web.app"
-  } else {
-    throw new Error(
-      'NODE_ENV must be "development" or MAPLE_DEV environment variable must be set to "dev" or "prod"'
-    )
-  }
+  const firebaseDomain =
+    process.env.FIREBASE_AUTH_DOMAIN ?? "digital-testimony-dev.web.app"
 
   return {
     source: "/__/:path*",
