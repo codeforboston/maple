989f9c5e22880e957dae9c472121eead65d5ea53
gracezhang89 <grace.zhang@stern.nyu.edu>
2023-04-10 12:54:35 -0400

    Replaced with cosponsor table

--- components/bill/LobbyingTable.tsx
+++ components/bill/LobbyingTable.tsx
@@ -1,116 +1,107 @@
-import { billSiteURL, Wrap } from "components/links"
-import { useContext } from "react"
+import React, { useState } from "react"
+import { Button, Modal, Table } from "react-bootstrap"
 import styled from "styled-components"
-import { BillHistory } from "../db"
-import { CourtContext } from "./Status"
+import { MemberReference, useMember } from "../db"
+import { memberLink } from "../links"
+import { FC } from "../types"
+import { BillProps } from "./types"
 
-export type HistoryProps = { billHistory: BillHistory }
-
-const LinkConnectedBills = ({ action }: { action: string }): JSX.Element => {
-  /* regex must have capture group for split to work */
-  const billRegex = new RegExp(/([HS]\d+)/)
-
-  const court = useContext(CourtContext)
-  if (!billRegex.test(action)) {
-    return <>{action}</>
-  }
-  const words = action.split(billRegex)
-  const wordMap = words.map((w, i) =>
-    billRegex.test(w) ? (
-      <Wrap key={i} href={billSiteURL(w, court)}>
-        {w}
-      </Wrap>
-    ) : (
-      w
+const CoSponsorRow = ({
+  court,
+  coSponsor
+}: {
+  court: number
+  coSponsor: MemberReference
+}) => {
+  const url = coSponsor
+    ? `https://malegislature.gov/Legislators/Profile/${coSponsor.Id}`
+    : ""
+  const { member, loading } = useMember(court, coSponsor.Id)
+  if (loading) {
+    return null
+  } else if (!member) {
+    return (
+      <tr>
+        <td>{coSponsor.Name}</td>
+        <td></td>
+        <td></td>
+      </tr>
     )
-  )
-
-  return <>{wordMap}</>
+  } else {
+    return (
+      <tr>
+        <td>{memberLink(member)}</td>
+        <td>{member?.Branch}</td>
+        <td>{member?.District}</td>
+      </tr>
+    )
+  }
 }
 
-const BillHistoryActionRows = ({ billHistory }: HistoryProps) => {
+const CoSponsorRows = ({
+  court,
+  coSponsors
+}: {
+  court: number
+  coSponsors: MemberReference[]
+}) => {
   return (
     <>
-      {billHistory.map((billHistoryItem, index) => {
-        const { Date, Action, Branch } = billHistoryItem
-        return (
-          <tr key={index}>
-            <td>
-              {Date.substring(5, 10)}-{Date.substring(0, 4)}
-            </td>
-            <td>
-              <LinkConnectedBills action={Action} />
-            </td>
-            <td>{Branch}</td>
-          </tr>
-        )
+      {coSponsors.map((coSponsor, index) => {
+        return <CoSponsorRow court={court} coSponsor={coSponsor} key={index} />
       })}
     </>
   )
 }
 
-export const HistoryTable = ({ billHistory }: HistoryProps) => {
+const StyledButton = styled(Button)`
+  :focus {
+    box-shadow: none;
+  }
+  padding: 0;
+  margin: 0;
+`
+
+export const Cosponsors: FC<BillProps> = ({ bill, children }) => {
+  const billNumber = bill.id
+  const court = bill.court
+  const coSponsors = bill.content.Cosponsors
+  const numCoSponsors = coSponsors ? coSponsors.length : 0
+  const [showBillCosponsors, setShowBillCosponsors] = useState(false)
+
+  const handleShowBillCosponsors = () =>
+    numCoSponsors > 0
+      ? setShowBillCosponsors(true)
+      : setShowBillCosponsors(false)
+  const handleCloseBillCosponsors = () => setShowBillCosponsors(false)
+
   return (
-    <div className="text-center">
-      <StyledTable>
-        <thead>
-          <tr>
-            <th></th>
-            <th>Status History</th>
-            <th>Branch</th>
-          </tr>
-        </thead>
-        <tbody>
-          <BillHistoryActionRows billHistory={billHistory} />
-        </tbody>
-      </StyledTable>
-    </div>
+    <>
+      <StyledButton
+        variant="link"
+        className="m-1 text-dark"
+        onClick={handleShowBillCosponsors}
+      >
+        {children}
+      </StyledButton>
+      <Modal
+        show={showBillCosponsors}
+        onHide={handleCloseBillCosponsors}
+        size="lg"
+      >
+        <Modal.Header closeButton onClick={handleCloseBillCosponsors}>
+          <Modal.Title>{billNumber + " CoSponsors"}</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <>
+            <Table responsive striped bordered hover>
+              <tbody>
+                <CoSponsorRows court={court} coSponsors={coSponsors} />
+              </tbody>
+            </Table>
+          </>
+        </Modal.Body>
+      </Modal>
+    </>
   )
 }
-
-const StyledTable = styled.table`
-  table-layout: auto;
-  width: 100%;
-  font-family: "Nunito";
-
-  th {
-    font-size: 16px;
-    font-weight: 400;
-    line-height: 22px;
-    letter-spacing: -1.125px;
-    text-align: start;
-    color: white;
-    background-color: var(--bs-secondary);
-
-    padding: 0.5rem;
-    outline: 0.25rem solid white;
-  }
-
-  th:last-child {
-    width: 24%;
-    text-align: center;
-  }
-
-  tbody {
-    td {
-      font: {
-        family: Nunito;
-        size: 16px;
-        weight: 400;
-      }
-      line-height: 22px;
-      letter-spacing: -1.125px;
-      text-align: start;
-
-      padding: 0.5rem 0.75rem;
-      outline: 0.25rem solid white;
-    }
-    tr:nth-child(odd) {
-      background: hsla(353, 74%, 45%, 0.2);
-    }
-
-    td:last-child {
-      text-align: center;
-    }
-  }
-`
