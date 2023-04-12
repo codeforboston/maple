84866bcc25ffb5f09124ff78639e6d58d555f55c
Oliver T. Herman <oherma01@eecs.tufts.edu>
2023-04-12 09:30:11 -0400

    Hid social sign on buttons

--- components/auth/SignInModal.tsx
+++ components/auth/SignInModal.tsx
@@ -93,9 +93,16 @@ export default function SignInModal({
                 Sign In
               </LoadingButton>
 
+              {
+              /*
+              Hiding social sign on buttons for now until we can get them working #TODO
+              See #1080
+
               <Divider className="px-4">or</Divider>
 
-              <SocialSignOnButtons onComplete={onHide} />
+              <SocialSignOnButtons onComplete={onHide} /> 
+              */
+              }
             </Stack>
           </Form>
         </Col>
