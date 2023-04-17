8707324de6ad97844ad9b76ba7cf98fc6bc9b9e4
Merritt Baggett <mertbagt@gmail.com>
2023-04-17 11:07:22 -0400

    basic layout

--- components/Newsfeed/Newsfeed.tsx
+++ components/Newsfeed/Newsfeed.tsx
@@ -8,12 +8,27 @@ import {
   usePublishedTestimonyListing
 } from "../db"
 import { Banner } from "../shared/StyledSharedComponents"
+import { Header, StyledContainer } from "./StyledNewsfeedComponents"
+import { AlertCard } from "components/AlertCard/AlertCard"
 
 export function Newsfeed() {
   const { user } = useAuth()
   const { result: profile, loading } = usePublicProfile(user?.uid)
 
   const isMobile = useMediaQuery("(max-width: 768px)")
+
+  const header = {
+    header: "Green Sustainability",
+    subheader: "",
+    timestamp: "5:30PM",
+    imgSrc: ""
+  }
+
+  const body = {
+    imgSrc: "",
+    imgAltTxt: "",
+    text: ""
+  }
 
   console.log("P:", profile)
   console.log("U:", user)
@@ -28,7 +43,22 @@ export function Newsfeed() {
         <>
           {profile ? (
             <>
-              <div>Hello There</div>
+              <StyledContainer>
+                <Header>
+                  <Col>Newsfeed</Col>
+                  <Col>boxes</Col>
+                </Header>
+                <AlertCard
+                  header={`Green Sustainability`}
+                  subheader={`test`}
+                  timestamp={`5:30PM`}
+                  headerImgSrc={``}
+                  bodyImgSrc={``}
+                  bodyImgAltTxt={``}
+                  bodyText={`body test`}
+                />
+                <div>Pagination</div>
+              </StyledContainer>
             </>
           ) : (
             <ErrorPage statusCode={404} withDarkMode={false} />
