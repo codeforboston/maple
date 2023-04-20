eb7793cc1f8448854d55c56774b6256faa12b494
Merritt Baggett <mertbagt@gmail.com>
2023-04-20 10:59:44 -0400

    target blank, noopener, noreferrer

--- components/HeroHeader/HeroHeader.js
+++ components/HeroHeader/HeroHeader.js
@@ -63,6 +63,8 @@ const HeroHeader = ({ authenticated }) => {
                     <a
                       href="https://calendar.google.com/calendar/embed?src=998f62323926f0b0076e7f578d3ca72b1bc94c4efa2f24be57b11f52b1b88595%40group.calendar.google.com&ctz=America%2FNew_York"
                       style={{ color: "white" }}
+                      target="_blank"
+                      rel="noopener noreferrer"
                     >
                       our calendar
                     </a>{" "}
