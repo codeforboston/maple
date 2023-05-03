9c32f34b35c7301513ab6e166a0ff919c0a01f88
Tim Blais <tblais189@gmail.com>
2023-05-02 20:50:13 -0400

    updating translations with getstaticprops

--- pages/about/mission-and-goals.tsx
+++ pages/about/mission-and-goals.tsx
@@ -1,5 +1,6 @@
 import { createPage } from "../../components/page"
 import GoalsAndMission from "../../components/GoalsAndMission/GoalsAndMission"
+import { createGetStaticTranslationProps } from "components/translations"
 
 export default createPage({
   title: "About",
@@ -8,20 +9,8 @@ export default createPage({
   }
 })
 
-// this must only be on pages in the pages folder
-// it will throw an error if it's in the components folder
-
-import { serverSideTranslations } from "next-i18next/serverSideTranslations"
-
-export async function getStaticProps({ locale }: any) {
-  return {
-    props: {
-      ...(await serverSideTranslations(locale, [
-        "common",
-        "goalsandmission",
-        "footer"
-      ]))
-      // Will be passed to the page component as props
-    }
-  }
-}
+export const getStaticProps = createGetStaticTranslationProps([
+  "common",
+  "goalsandmission",
+  "footer"
+])
--- pages/submit-testimony.tsx
+++ pages/submit-testimony.tsx
@@ -5,6 +5,7 @@ import {
   useSyncRouterAndStore
 } from "../components/publish/hooks"
 import { SubmitTestimonyForm } from "../components/publish/SubmitTestimonyForm"
+import { createGetStaticTranslationProps } from "components/translations"
 
 export default createPage({
   title: "Submit Testimony",
@@ -20,15 +21,7 @@ export default createPage({
   })
 })
 
-// this must only be on pages in the pages folder
-// it will throw an error if it's in the components folder
-import { serverSideTranslations } from "next-i18next/serverSideTranslations"
-
-export async function getStaticProps({ locale }: any) {
-  return {
-    props: {
-      ...(await serverSideTranslations(locale, ["common", "footer"]))
-      // Will be passed to the page component as props
-    }
-  }
-}
+export const getStaticProps = createGetStaticTranslationProps([
+  "common",
+  "footer"
+])
