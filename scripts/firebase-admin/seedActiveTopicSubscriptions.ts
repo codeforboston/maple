ebc611a44e2d286981c0941962e004d6c89a7610
Oliver T. Herman <oherma01@eecs.tufts.edu>
2023-05-15 14:59:24 -0400

    Added Bill/Org notification support

--- scripts/firebase-admin/seedActiveTopicSubscriptions.ts
+++ scripts/firebase-admin/seedActiveTopicSubscriptions.ts
@@ -6,7 +6,7 @@ import { addTopicSubscription } from "functions/src/subscriptions/addTopicSubscr
 export const script: Script = async ({ db, auth }) => {
   // For each user, add a topic subscription.
   const allUsers = await listAllUsers(auth);
-  console.log(`Seeding ${allUsers.length} users with activeTopicSubscriptions`);
+  console.log(`Seeding ${allUsers.length} users with a topic subscription for bill-example-1 and org-example-1`);
 
   for (const user of allUsers) {
     const subscriptionData = {
@@ -17,6 +17,17 @@ export const script: Script = async ({ db, auth }) => {
         court: "exampleCourt",
       },
       type: "bill",
+    };
+
+    await addTopicSubscription({ user, subscriptionData, db });
+  }
+
+  for (const user of allUsers) {
+    const subscriptionData = {
+      topicName: "org-example-1",
+      uid: user.uid,
+      orgId: "example-1",
+      type: "org",
     };
 
     await addTopicSubscription({ user, subscriptionData, db });
