78fe8d31e1907b31b0a208744d7a131ebf27236c
Merritt Baggett <mertbagt@gmail.com>
2023-05-23 20:27:26 -0400

    fixed missing filters

--- components/Newsfeed/Newsfeed.tsx
+++ components/Newsfeed/Newsfeed.tsx
@@ -4,7 +4,7 @@ import {
 } from "@alexjball/react-instantsearch-hooks-web"
 import { collection, query, where, getDocs } from "firebase/firestore"
 import ErrorPage from "next/error"
-import { useEffect, useState } from "react"
+import { useCallback, useEffect, useState } from "react"
 import TypesenseInstantSearchAdapter from "typesense-instantsearch-adapter"
 import { useMediaQuery } from "usehooks-ts"
 import { useAuth } from "../auth"
@@ -22,6 +22,19 @@ import { SearchContainer } from "components/search/SearchContainer"
 import { SearchErrorBoundary } from "components/search/SearchErrorBoundary"
 import { initialSortByValue } from "components/search/SortBy"
 import { currentGeneralCourt } from "functions/src/shared"
+
+type ElementProps = {
+  bodyText: string
+  court: string
+  header: string
+  id: string
+  subheader: string
+  timestamp: string
+  topicName: string
+  type: string
+}
+
+type Props = ElementProps[]
 
 const searchClient = new TypesenseInstantSearchAdapter({
   server: getServerConfig(),
@@ -62,31 +75,63 @@ const Layout = () => {
     `/users/${uid}/userNotificationFeed/`
   )
 
-  let notificationList: {
-    bodyText: string
-    court: string
-    header: string
-    id: string
-    subheader: string
-    timestamp: string
-    topicName: string
-    type: string
-  }[] = []
+  const [orgFilter, setOrgFilter] = useState<boolean>(true)
+  const [billFilter, setBillFilter] = useState<boolean>(true)
 
-  const [notificationsDisplayed, setNotificationsDisplayed] = useState<
-    {
-      bodyText: string
-      court: string
-      header: string
-      id: string
-      subheader: string
-      timestamp: string
-      topicName: string
-      type: string
-    }[]
-  >([])
+  const [allResults, setAllResults] = useState<Props>([])
+  const [orgResults, setOrgResults] = useState<Props>([])
+  const [billResults, setBillResults] = useState<Props>([])
+
+  const [notificationsDisplay, setNotificationsDisplay] = useState<Props>([])
+  const [noDisplay, setNoDisplay] = useState<boolean>(true)
+
+  const onFilterChange = (trolley: string, boxType: string) => {
+    const Box = document.getElementById(boxType) as HTMLInputElement
+    if (Box?.checked) {
+      trolley ? setOrgFilter(true) : setBillFilter(true)
+    } else {
+      trolley ? setOrgFilter(false) : setBillFilter(false)
+    }
+  }
+
+  const checkDisplay = useCallback(() => {
+    setNoDisplay(false)
+    if (orgFilter == true && billFilter == true) {
+      return setNotificationsDisplay(allResults)
+    } else if (orgFilter == true) {
+      return setNotificationsDisplay(orgResults)
+    } else if (billFilter == true) {
+      return setNotificationsDisplay(billResults)
+    }
+    return setNoDisplay(true)
+  }, [allResults, orgFilter, orgResults, billFilter, billResults])
+
+  // let notificationList: {
+  //   bodyText: string
+  //   court: string
+  //   header: string
+  //   id: string
+  //   subheader: string
+  //   timestamp: string
+  //   topicName: string
+  //   type: string
+  // }[] = []
+
+  // const [notificationsDisplayed, setNotificationsDisplayed] = useState<
+  //   {
+  //     bodyText: string
+  //     court: string
+  //     header: string
+  //     id: string
+  //     subheader: string
+  //     timestamp: string
+  //     topicName: string
+  //     type: string
+  //   }[]
+  // >([])
 
   const notificationQuery = async () => {
+    let notificationList: Props = []
     const q = query(subscriptionRef, where("uid", "==", `${uid}`))
     const querySnapshot = await getDocs(q)
     querySnapshot.forEach(doc => {
@@ -94,14 +139,36 @@ const Layout = () => {
       notificationList.push(doc.data().notification)
     })
 
-    if (notificationsDisplayed.length === 0 && notificationList.length != 0) {
-      setNotificationsDisplayed(notificationList)
+    if (notificationsDisplay.length === 0 && notificationList.length != 0) {
+      setAllResults(notificationList)
+      setOrgResults(
+        notificationList.filter(notification => notification.type === "org")
+      )
+      setBillResults(
+        notificationList.filter(notification => notification.type === "bill")
+      )
+      setNotificationsDisplay(allResults)
     }
   }
 
   useEffect(() => {
     uid ? notificationQuery() : null
   })
+
+  useEffect(() => {
+    checkDisplay()
+  }, [checkDisplay, orgFilter, billFilter, notificationsDisplay])
+
+  function Filters() {
+    return (
+      <FilterBoxs
+        isMobile={isMobile}
+        onFilterChange={onFilterChange}
+        orgFilter={orgFilter}
+        billFilter={billFilter}
+      />
+    )
+  }
 
   return (
     <>
@@ -117,41 +184,47 @@ const Layout = () => {
                 <Header>
                   <HeaderTitle>Newsfeed</HeaderTitle>
                   {isMobile ? (
-                    <FilterBox isMobile={isMobile} />
+                    <Filters />
                   ) : (
                     <Col className="d-flex flex-column">
-                      <FilterBox isMobile={isMobile} />
+                      <Filters />
                     </Col>
                   )}
                 </Header>
-                {notificationsDisplayed.map(
-                  (
-                    element: {
-                      bodyText: string
-                      court: string
-                      header: string
-                      id: string
-                      subheader: string
-                      timestamp: string
-                      topicName: string
-                      type: string
-                    },
-                    index: number
-                  ) => (
-                    <div className="pb-4" key={index}>
-                      <AlertCard
-                        header={element.header}
-                        subheader={element.subheader}
-                        timestamp={element.timestamp}
-                        headerImgSrc={`${
-                          element.type === `org` ? `/profile-org-white.svg` : ``
-                        }`}
-                        bodyImgSrc={``}
-                        bodyImgAltTxt={``}
-                        bodyText={element.bodyText}
-                      />
-                    </div>
-                  )
+                {noDisplay ? (
+                  <div className="pb-4">
+                    <AlertCard
+                      header={`No Results`}
+                      subheader={``}
+                      timestamp={``}
+                      headerImgSrc={``}
+                      bodyImgSrc={``}
+                      bodyImgAltTxt={``}
+                      bodyText={`There are no news updates for your current followed topics`}
+                    />
+                  </div>
+                ) : (
+                  <>
+                    {notificationsDisplay.map(
+                      (element: ElementProps, index: number) => (
+                        <div className="pb-4" key={index}>
+                          <AlertCard
+                            header={element.header}
+                            subheader={element.subheader}
+                            timestamp={element.timestamp}
+                            headerImgSrc={`${
+                              element.type === `org`
+                                ? `/profile-org-white.svg`
+                                : ``
+                            }`}
+                            bodyImgSrc={``}
+                            bodyImgAltTxt={``}
+                            bodyText={element.bodyText}
+                          />
+                        </div>
+                      )
+                    )}
+                  </>
                 )}
                 <SearchContainer>
                   <Pagination className="d-flex justify-content-center mt-2 mb-3" />
@@ -167,7 +240,17 @@ const Layout = () => {
   )
 }
 
-function FilterBox({ isMobile }: { isMobile: boolean }) {
+function FilterBoxs({
+  isMobile,
+  onFilterChange,
+  orgFilter,
+  billFilter
+}: {
+  isMobile: boolean
+  onFilterChange: any
+  orgFilter: boolean
+  billFilter: boolean
+}) {
   return (
     <>
       <Row
@@ -180,9 +263,13 @@ function FilterBox({ isMobile }: { isMobile: boolean }) {
             className="form-check-input"
             type="checkbox"
             value=""
-            id="flexCheckDefault"
+            id="orgCheck"
+            onChange={e => {
+              onFilterChange("non-empty", "orgCheck")
+            }}
+            checked={orgFilter}
           />
-          <label className="form-check-label" htmlFor="flexCheckDefault">
+          <label className="form-check-label" htmlFor="orgCheck">
             Organization Updates
           </label>
         </div>
@@ -197,9 +284,13 @@ function FilterBox({ isMobile }: { isMobile: boolean }) {
             className="form-check-input"
             type="checkbox"
             value=""
-            id="flexCheckDefault"
+            id="billCheck"
+            onChange={e => {
+              onFilterChange("", "billCheck")
+            }}
+            checked={billFilter}
           />
-          <label className="form-check-label" htmlFor="flexCheckDefault">
+          <label className="form-check-label" htmlFor="billCheck">
             Bill Updates
           </label>
         </div>
