217645b6a62437908e4b666931bbfb0c4ec20d95
Merritt Baggett <mertbagt@gmail.com>
2023-07-02 11:34:56 -0400

    alternate banner wording

--- components/ProfilePage/ProfilePage.tsx
+++ components/ProfilePage/ProfilePage.tsx
@@ -60,6 +60,7 @@ export function ProfilePage(profileprops: {
         <>
           {profile ? (
             <>
+              {isUser && <Banner> {t("content.viewingProfile")} </Banner>}
               {isUser && bannerContent}
               <StyledContainer>
                 <ProfileHeader
