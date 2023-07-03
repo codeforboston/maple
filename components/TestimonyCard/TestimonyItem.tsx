927f193b2531d398cc225415432316ce499a5ab4
Madame Web <101157878+hshareef9@users.noreply.github.com>
2023-07-03 15:07:02 -0400

    ellipses for testimonies

--- components/TestimonyCard/TestimonyItem.tsx
+++ components/TestimonyCard/TestimonyItem.tsx
@@ -18,6 +18,7 @@ import { ViewAttachment } from "components/ViewAttachment"
 import styles from "./ViewTestimony.module.css"
 import { UseAsyncReturn } from "react-async-hook"
 import { useTranslation } from "next-i18next"
+import { trimContent } from "components/TestimonyCallout/TestimonyCallout"
 
 const FooterButton = styled(Button)`
   margin: 0;
@@ -106,7 +107,7 @@ export const TestimonyItem = ({
   const [showAllTestimony, setShowAllTestimony] = useState(false)
   const snippet = showAllTestimony
     ? testimonyContent
-    : testimonyContent.slice(0, snippetChars)
+    : trimContent(testimonyContent.slice(0, snippetChars), snippetChars)
   const canExpand = snippet.length !== testimonyContent.length
 
   const [showConfirm, setShowConfirm] = useState(false)
