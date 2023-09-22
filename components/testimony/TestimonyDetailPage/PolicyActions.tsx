132b4ac948c70d5a5e6ba2144c91f379e27a80cd
Mark <marktrepanier.cajigas@gmail.com>
2023-09-22 15:59:33 -0400

    adds reporting state params to policy action props

--- components/testimony/TestimonyDetailPage/PolicyActions.tsx
+++ components/testimony/TestimonyDetailPage/PolicyActions.tsx
@@ -10,6 +10,8 @@ import { useTranslation } from "next-i18next"
 interface PolicyActionsProps {
   className?: string
   isUser?: boolean
+  isReporting: boolean
+  setReporting: (boolean: boolean) => void
 }
 
 const PolicyActionItem: FC<ListItemProps> = props => (
@@ -18,7 +20,9 @@ const PolicyActionItem: FC<ListItemProps> = props => (
 
 export const PolicyActions: FC<PolicyActionsProps> = ({
   className,
-  isUser
+  isUser,
+  isReporting,
+  setReporting
 }) => {
   const { bill } = useCurrentTestimonyDetails(),
     billLabel = formatBillId(bill.id)
