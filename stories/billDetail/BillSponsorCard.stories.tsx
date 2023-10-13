b734674b129d18c13076234fb19e0b7b85be4539
sashamaryl <sashamaryl@gmail.com>
2023-10-13 07:36:08 -0400

    pulling in components on stories marked 'todo'

--- stories/billDetail/BillSponsorCard.stories.tsx
+++ stories/billDetail/BillSponsorCard.stories.tsx
@@ -1,13 +1,103 @@
 import { createMeta } from "stories/utils"
+import { Cosponsors } from "components/bill/Cosponsors"
+import { ComponentStory } from "@storybook/react"
+import { BillProps } from "components/bill/types"
+import { Timestamp } from "firebase/firestore"
+import { Bill } from "components/db"
+import type { CurrentCommittee } from "functions/src/bills/types"
 
 // TODO: move into components directory
-const BillSponsorCard = () => <div>TODO</div>
 
 export default createMeta({
-  title: "Bill Detail/BillSponsorCard",
+  title: "Bill Detail/Cosponsors",
   figmaUrl:
     "https://www.figma.com/file/3ifz37EOwDfmnEG8320KlD/CS1---MAPLE?node-id=172%3A8380",
-  component: BillSponsorCard
+  component: Cosponsors
 })
 
-export const Primary = () => <BillSponsorCard />
+const Template: ComponentStory<typeof Cosponsors> = args => (
+  <Cosponsors {...args} />
+)
+
+export const Primary = Template.bind({})
+
+const newBillHistory = [
+  {
+    Date: new Date().toISOString(),
+    Branch: "House",
+    Action: "this is the action"
+  },
+  {
+    Date: new Date().toISOString(),
+    Branch: "House",
+    Action: "this is the action"
+  },
+  {
+    Date: new Date().toISOString(),
+    Branch: "House",
+    Action: "this is the action"
+  },
+  {
+    Date: new Date().toISOString(),
+    Branch: "House",
+    Action: "this is the action"
+  }
+]
+
+const newBillContent = {
+  Title: "An Act relative to the safety of autistic and alzheimer individuals",
+  BillNumber: "H. 1728",
+  DocketNumber: "HD. 2027",
+  GeneralCourtNumber: 192,
+  PrimarySponsor: {
+    Id: "1",
+    Name: "Paul McMurtry",
+    Type: 1
+  },
+  Cosponsors: [
+    {
+      Id: "2",
+      Name: "Paul McMurtry",
+      Type: 1
+    },
+    {
+      Id: "3",
+      Name: "Paul McMurtry",
+      Type: 1
+    },
+    {
+      Id: "4",
+      Name: "Paul McMurtry",
+      Type: 1
+    }
+  ],
+  LegislationTypeName: "House",
+  Pinslip: "",
+  DocumentText: "this is the document text"
+}
+
+const bill = {
+  id: "123",
+  court: 192,
+  content: newBillContent,
+  cosponsorCount: 0,
+  testimonyCount: 0,
+  endorseCount: 0,
+  opposeCount: 0,
+  neutralCount: 0,
+  fetchedAt: Timestamp.fromDate(new Date()),
+  history: newBillHistory,
+  currentCommittee: {
+    name: "Committee on Public Safety and Homeland Security",
+    id: "J30",
+    branch: "Joint",
+    chamber: "House",
+    phone: "617-722-2230",
+    email: "committee@committee.org"
+  } as CurrentCommittee,
+  city: "Boston"
+}
+
+Primary.args = {
+  bill: bill as Bill
+}
--- stories/dashboard/missionCenter/BillCosponsorCard.stories.tsx
+++ stories/dashboard/missionCenter/BillCosponsorCard.stories.tsx
@@ -1,13 +1,69 @@
 import { createMeta } from "stories/utils"
-
+import { Cosponsors } from "components/bill/Cosponsors"
+import type {
+  MISSING_TIMESTAMP,
+  CurrentCommittee,
+  CommitteeMember
+} from "functions/src/bills/types"
+import { Timestamp } from "firebase/firestore"
+import { BillProps } from "components/bill/types"
+import { ComponentStory } from "@storybook/react"
+import type { MemberReference } from "components/db"
+import React from "react"
 // TODO: move into components directory
-const BillCosponsorCard = () => <div>TODO</div>
+// const BillCosponsorCard = () => <div>TODO</div>
 
 export default createMeta({
   title: "Dashboard/Mission Center/BillCosponsorCard",
   figmaUrl:
     "https://www.figma.com/file/3ifz37EOwDfmnEG8320KlD/CS1---MAPLE?node-id=245%3A12313",
-  component: BillCosponsorCard
+  component: Cosponsors
 })
 
-export const Primary = () => <BillCosponsorCard />
+type CosponsorProps = React.ComponentProps<typeof Cosponsors>
+
+const Template = (args: CosponsorProps) => <Cosponsors {...args} />
+
+export const Primary: ComponentStory<typeof Cosponsors> = Template.bind({})
+
+const primeSponsor: MemberReference = {
+  Id: "1",
+  Name: "Paul McMurtry",
+  Type: 1
+}
+
+const currentComm: CurrentCommittee = {
+  id: "2",
+  name: "Joint Committee on Public Health",
+  houseChair: { id: "1", name: "Paul McMurtry", email: "aaa@email.com" }
+}
+
+Primary.args = {
+  bill: {
+    id: "345",
+    court: 0,
+    content: {
+      Title: "Title",
+      BillNumber: "BillNumber",
+      DocketNumber: "DocketNumber",
+      GeneralCourtNumber: 0,
+      PrimarySponsor: primeSponsor,
+      Cosponsors: [primeSponsor],
+      LegislationTypeName: "",
+      Pinslip: "",
+      DocumentText: ""
+    },
+    cosponsorCount: 1,
+    testimonyCount: 1,
+    endorseCount: 1,
+    opposeCount: 0,
+    neutralCount: 0,
+    nextHearingAt: Timestamp.fromMillis(0),
+    latestTestimonyAt: Timestamp.fromMillis(0),
+    latestTestimonyId: "1",
+    fetchedAt: Timestamp.fromMillis(0),
+    history: [],
+    currentCommittee: currentComm,
+    city: undefined
+  }
+}
