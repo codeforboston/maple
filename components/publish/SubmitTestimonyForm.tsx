e242bb6041340bfbcefdc348b0edaaab7bea045d
hanboyu <bill_han97@hotmail.com>
2023-10-17 13:23:27 -0400

    Added mobile support for write testimony step 1

--- components/publish/SubmitTestimonyForm.tsx
+++ components/publish/SubmitTestimonyForm.tsx
@@ -1,10 +1,12 @@
 import clsx from "clsx"
 import styled from "styled-components"
-import { Col, Container, Image, Row, Spinner } from "../bootstrap"
-import { Bill } from "../db"
+import { Col, Container, Image, Row, Spinner, Button, Collapse } from "../bootstrap"
+import { Bill, Profile } from "../db"
 import * as links from "../links"
 import { ChooseStance } from "./ChooseStance"
 import { useFormInfo } from "./hooks"
+import { useState } from "react";
+import { useMediaQuery } from "usehooks-ts"
 import { ProgressBar } from "./ProgressBar"
 import { PublishInfo } from "./PublishInfo"
 import { PublishTestimony } from "./PublishTestimony"
@@ -16,29 +18,38 @@ import { WriteTestimony } from "./WriteTestimony"
 import { KeepNote } from "./KeepNote"
 
 const Background = styled.div`
-  background: linear-gradient(to right, white 50%, var(--bs-body-bg) 50%);
+  background: var(--bs-white);
 `
 
 const StyledContainer = styled(Container)`
   font-family: "Nunito";
+  background: var(--bs-white);
 `
 
 export const SubmitTestimonyForm = () => {
   const form = useFormInfo()
+  const isMobile = useMediaQuery("(max-width: 768px)")
 
   return form.ready ? (
-    <Background>
+    <Background className="p-0">
       <StyledContainer fluid="lg">
+        <Row>
+          <Col xs={12}>
+            {isMobile ? <PolicyDetails bill={form.bill} profile={form.profile} /> : null}
+          </Col>
+        </Row>
         <Row className="g-0">
-          <Col xs={9}>
+          <Col md={9} xs={12}>
             <Form step={form.step} bill={form.bill} synced={form.synced} />
           </Col>
-          <Col xs={3}>
-            {form.step == "position" ? (
-              <QuickInfo bill={form.bill} profile={form.profile} />
-            ) : (
-              <KeepNote currentStep={form.step} />
-            )}
+          <Col md={3} xs={12}>
+            { isMobile ? null :
+                form.step == "position" ? (
+                  <QuickInfo bill={form.bill} profile={form.profile} />
+                ) : (
+                  <KeepNote currentStep={form.step} />
+                )
+            }
           </Col>
         </Row>
       </StyledContainer>
@@ -59,6 +70,38 @@ const Divider = styled.div`
   height: 1px;
   background-color: var(--bs-gray-500);
 `
+
+const PolicyDetailsStyle = styled.div`
+  color: var(--bs-blue);
+  background-color: var(--bs-body-bg);
+  font-weight: bolder;
+  text-align: center;
+  box-shadow: 5px 5px 5px 0px rgba(0,0,0,0.3);
+`
+
+const PolicyDetails = ({ bill, profile }: { bill: Bill; profile: Profile }) => {
+  const [isCollapsed, setCollapsed] = useState(false);
+
+  return (
+    <PolicyDetailsStyle className="mb-3 p-3">
+      <Collapse in={isCollapsed}>
+        <div>
+          <QuickInfo bill={bill} profile={profile} />
+          <Divider />
+        </div>
+      </Collapse> 
+
+      <div onClick={() => setCollapsed(!isCollapsed)}>
+        {isCollapsed ?
+          <div>View Less Details &#9650;</div> :
+          <div>View Policy Details &#9660;</div>
+        }
+      </div>
+        
+    </PolicyDetailsStyle>
+  );
+};
+
 
 const Form = ({
   step,
@@ -94,18 +137,23 @@ const Form = ({
 
 const Overview = ({ className }: { className: string }) => (
   <div className={clsx("d-flex", className)}>
-    <div>
-      <h1>Write, Publish, and Send Your Testimony!</h1>
-      <Divider className="me-5" />
-      <div className="mt-2" style={{ fontWeight: "bolder" }}>
-        Your voice matters. And it's important that you share it. MAPLE helps
-        you 1) write testimony, 2) publish it to our community, and 3) send it
-        to the right legislators so it can be formally considered. It's easy as
-        1-2-3!
-      </div>
-    </div>
-    <div>
-      <Image className="ms-4 " alt="" src="/writing.svg" />
-    </div>
+    <Row className="align-items-center">
+      <Col md={10} xs={12}>
+        <h1>Write, Publish, and Send Your Testimony!</h1>
+        <Divider className="me-5" />
+        <div className="mt-2" style={{ fontWeight: "bolder" }}>
+          Your voice matters. And it's important that you share it. MAPLE helps
+          you 1) write testimony, 2) publish it to our community, and 3) send it
+          to the right legislators so it can be formally considered. It's easy as
+          1-2-3!
+        </div>
+      </Col>
+      <Col md={2} xs={12}>
+        <div className="mx-auto text-center">
+          <Image alt="" src="/writing.svg" /> 
+        </div>
+        
+      </Col>
+    </Row>
   </div>
 )
