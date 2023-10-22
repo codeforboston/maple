ab1250496816e0b193518ed432032d66799cbf11
Madame Web <101157878+hshareef9@users.noreply.github.com>
2023-10-22 15:20:00 -0400

    prettier

--- stories/components/GlobalSearchBar.stories.tsx
+++ stories/components/GlobalSearchBar.stories.tsx
@@ -1,18 +1,15 @@
 import { createMeta } from "stories/utils"
-import { GlobalSearchBar }from "../../components/GlobalSearchBar/GlobalSearchBar"
+import { GlobalSearchBar } from "../../components/GlobalSearchBar/GlobalSearchBar"
 import { ComponentStory } from "@storybook/react"
-
 
 export default createMeta({
   title: "Components/GlobalSearchBar",
   component: GlobalSearchBar
 })
 
-
 const Template: ComponentStory<typeof GlobalSearchBar> = () => {
   return <GlobalSearchBar></GlobalSearchBar>
 }
 
 export const Primary = Template.bind({})
-Primary.args = {
-}
+Primary.args = {}
