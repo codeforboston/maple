364c6d1f363f92c567036fd95db6c36f4bffbba0
Merritt Baggett <mertbagt@gmail.com>
2023-12-07 17:24:36 -0500

    prettier pass

--- components/TestimonyCard/ViewTestimony.tsx
+++ components/TestimonyCard/ViewTestimony.tsx
@@ -1,32 +1,34 @@
-import { useState, useEffect } from 'react';
-import { NoResults } from "components/search/NoResults";
-import { PaginationButtons } from "components/table";
-import { TFunction, useTranslation } from "next-i18next";
-import { Card as BootstrapCard, Col, Row } from "react-bootstrap";
-import styled from "styled-components";
-import { Card as MapleCard } from "../Card";
-import { useAuth } from "../auth";
-import { Testimony, UsePublishedTestimonyListing } from "../db";
-import { SortTestimonyDropDown } from "./SortTestimonyDropDown";
-import { Tab, Tabs } from "./Tabs";
-import { TestimonyItem } from "./TestimonyItem";
+import { useState, useEffect } from "react"
+import { NoResults } from "components/search/NoResults"
+import { PaginationButtons } from "components/table"
+import { TFunction, useTranslation } from "next-i18next"
+import { Card as BootstrapCard, Col, Row } from "react-bootstrap"
+import styled from "styled-components"
+import { Card as MapleCard } from "../Card"
+import { useAuth } from "../auth"
+import { Testimony, UsePublishedTestimonyListing } from "../db"
+import { SortTestimonyDropDown } from "./SortTestimonyDropDown"
+import { Tab, Tabs } from "./Tabs"
+import { TestimonyItem } from "./TestimonyItem"
 
 const Container = styled.div`
   font-family: Nunito;
-`;
+`
 
 const Head = styled(BootstrapCard.Header)`
   background-color: var(--bs-blue);
   color: white;
   font-size: 22px;
-`;
+`
 
-const ViewTestimony = (props: UsePublishedTestimonyListing & {
-  search?: boolean,
-  onProfilePage?: boolean,
-  className?: string,
-  isOrg?: boolean
-}) => {
+const ViewTestimony = (
+  props: UsePublishedTestimonyListing & {
+    search?: boolean
+    onProfilePage?: boolean
+    className?: string
+    isOrg?: boolean
+  }
+) => {
   const {
     items,
     setFilter,
@@ -34,10 +36,10 @@ const ViewTestimony = (props: UsePublishedTestimonyListing & {
     className,
     pagination,
     isOrg
-  } = props;
+  } = props
 
-  const { user } = useAuth();
-  const [totalTestimonies, setTotalTestimonies] = useState<number>(0);
+  const { user } = useAuth()
+  const [totalTestimonies, setTotalTestimonies] = useState<number>(0)
 
   // Updated useEffect
   useEffect(() => {
@@ -46,27 +48,27 @@ const ViewTestimony = (props: UsePublishedTestimonyListing & {
         .then(response => response.json())
         .then(data => {
           if (data && data.count !== undefined) {
-            setTotalTestimonies(data.count);
+            setTotalTestimonies(data.count)
           }
         })
         .catch(error => {
-          console.error("Failed to count user testimonies", error);
-        });
+          console.error("Failed to count user testimonies", error)
+        })
     }
-  }, [user?.uid]);
+  }, [user?.uid])
 
-  const testimony = items.result ?? [];
-  const [orderBy, setOrderBy] = useState<string>();
-  const [activeTab, setActiveTab] = useState(1);
+  const testimony = items.result ?? []
+  const [orderBy, setOrderBy] = useState<string>()
+  const [activeTab, setActiveTab] = useState(1)
 
   const handleTabClick = (e: Event, value: number) => {
-    setActiveTab(value);
-  };
+    setActiveTab(value)
+  }
 
   const handleFilter = (filter: string | undefined) => {
-    setFilter({ authorRole: filter ?? '' }); 
-  };
-  
+    setFilter({ authorRole: filter ?? "" })
+  }
+
   const tabs = [
     <Tab
       key="at"
@@ -89,15 +91,17 @@ const ViewTestimony = (props: UsePublishedTestimonyListing & {
       value={3}
       action={() => handleFilter("organization")}
     />
-  ];
+  ]
 
-  const { t } = useTranslation("testimony");
+  const { t } = useTranslation("testimony")
 
   return (
     <Container>
       <MapleCard
         className={className}
-        headerElement={<Head>{isOrg ? t("Our Testimonies") : t("Testimonies")}</Head>}
+        headerElement={
+          <Head>{isOrg ? t("Our Testimonies") : t("Testimonies")}</Head>
+        }
         body={
           <BootstrapCard.Body>
             {!onProfilePage && (
@@ -162,34 +166,35 @@ const ViewTestimony = (props: UsePublishedTestimonyListing & {
         }
       />
     </Container>
-  );
-};
+  )
+}
 
-export default ViewTestimony;
+export default ViewTestimony
 
 function ShowPaginationSummary({
   totalItems,
   pagination,
   t
 }: {
-  totalItems: number;
-  pagination: { currentPage: number; itemsPerPage: number };
-  t: TFunction;
+  totalItems: number
+  pagination: { currentPage: number; itemsPerPage: number }
+  t: TFunction
 }) {
   if (totalItems < 1) {
-    return <div>{t("viewTestimony.noTestimonies")}</div>;
+    return <div>{t("viewTestimony.noTestimonies")}</div>
   }
-  
-  const { currentPage, itemsPerPage } = pagination;
-  const currentPageStart = (currentPage - 1) * itemsPerPage;
-  let currentPageEnd = currentPageStart + itemsPerPage;
+
+  const { currentPage, itemsPerPage } = pagination
+  const currentPageStart = (currentPage - 1) * itemsPerPage
+  let currentPageEnd = currentPageStart + itemsPerPage
   if (currentPageEnd > totalItems) {
-    currentPageEnd = totalItems;
+    currentPageEnd = totalItems
   }
 
   return (
     <div>
-      {t("viewTestimony.showing")} {currentPageStart + 1}–{currentPageEnd} {t("viewTestimony.outOf")} {totalItems}
+      {t("viewTestimony.showing")} {currentPageStart + 1}–{currentPageEnd}{" "}
+      {t("viewTestimony.outOf")} {totalItems}
     </div>
-  );
+  )
 }
