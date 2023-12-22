3dd24ac88ff7c659a0fe0379f9add8125608950f
sashamaryl <sashamaryl@gmail.com>
2023-12-22 13:17:57 -0500

    refactor profile icons to fix styled comp error

--- stories/molecules/Icons.stories.tsx
+++ stories/molecules/Icons.stories.tsx
@@ -1,14 +1,15 @@
 import { Meta } from "@storybook/react"
-import {
-  OrgIconLarge,
-  OrgIconSmall,
-  UserIconLarge,
-  UserIconSmall
-} from "components/ProfilePage/StyledProfileComponents"
+import { StyledProfileIcon } from "components/ProfilePage/StyledUserIcons"
 import React from "react"
 
-const IconStories = ({ children }: { children: React.ReactNode }) => {
-  return <div>{children}</div>
+const IconStories = ({
+  children,
+  className
+}: {
+  children: React.ReactNode
+  className?: string
+}) => {
+  return <div className={className}>{children}</div>
 }
 
 const meta: Meta = {
@@ -19,12 +20,13 @@ const meta: Meta = {
 export const Primary = {
   args: {
     children: (
-      <IconStories>
-        {/* <UserIconLarge />
-    <UserIconSmall />
-    <OrgIconLarge />
-    <OrgIconSmall /> */}
-        <div>TODO</div>
+      <IconStories className={`d-flex flex-row gap-5`}>
+        <StyledProfileIcon large />
+        <StyledProfileIcon />
+        <StyledProfileIcon isOrg large />
+        <StyledProfileIcon isOrg />
+        <StyledProfileIcon profileImage="/codeforbostonicon.png" />
+        <StyledProfileIcon large profileImage="/codeforbostonicon.png" />
       </IconStories>
     )
   },
