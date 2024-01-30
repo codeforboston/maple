0d19207a3a16b3c6a2f06b68ee97277ee906e8d8
sashamaryl <sashamaryl@gmail.com>
2024-01-30 07:31:43 -0500

    add section dropdowns back to mobile view

--- components/Footer/Footer.tsx
+++ components/Footer/Footer.tsx
@@ -1,11 +1,16 @@
+/* use client */
+
 import { authStepChanged } from "components/auth/redux"
 import { useAppDispatch } from "components/hooks"
 import { User } from "firebase/auth"
 import { useTranslation } from "next-i18next"
-import Image from "react-bootstrap/Image"
 import styled from "styled-components"
 import { ExternalNavLink, NavLink } from "../Navlink"
-import { Button, Col, Container, Row } from "../bootstrap"
+import { Button, Col, Image, Container, Row, Nav, Navbar } from "../bootstrap"
+import CustomDropdown, {
+  CustomDropdownProps
+} from "components/Footer/CustomFooterDropdown"
+import { FooterContainer } from "./FooterContainer"
 
 export type PageFooterProps = {
   children?: any
@@ -62,15 +67,12 @@ function MapleContainer({ className }: { className?: string }) {
             target="_blank"
             rel="noopener noreferrer"
           >
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
+            <Image
+              src="/images/instagram.svg"
+              alt="Instagram Icon"
               width="24"
               height="24"
-              fill="currentColor"
-              viewBox="0 0 16 16"
-            >
-              <path d="M8 0C5.829 0 5.556.01 4.703.048 3.85.088 3.269.222 2.76.42a3.917 3.917 0 0 0-1.417.923A3.927 3.927 0 0 0 .42 2.76C.222 3.268.087 3.85.048 4.7.01 5.555 0 5.827 0 8.001c0 2.172.01 2.444.048 3.297.04.852.174 1.433.372 1.942.205.526.478.972.923 1.417.444.445.89.719 1.416.923.51.198 1.09.333 1.942.372C5.555 15.99 5.827 16 8 16s2.444-.01 3.298-.048c.851-.04 1.434-.174 1.943-.372a3.916 3.916 0 0 0 1.416-.923c.445-.445.718-.891.923-1.417.197-.509.332-1.09.372-1.942C15.99 10.445 16 10.173 16 8s-.01-2.445-.048-3.299c-.04-.851-.175-1.433-.372-1.941a3.926 3.926 0 0 0-.923-1.417A3.911 3.911 0 0 0 13.24.42c-.51-.198-1.092-.333-1.943-.372C10.443.01 10.172 0 7.998 0h.003zm-.717 1.442h.718c2.136 0 2.389.007 3.232.046.78.035 1.204.166 1.486.275.373.145.64.319.92.599.28.28.453.546.598.92.11.281.24.705.275 1.485.039.843.047 1.096.047 3.231s-.008 2.389-.047 3.232c-.035.78-.166 1.203-.275 1.485a2.47 2.47 0 0 1-.599.919c-.28.28-.546.453-.92.598-.28.11-.704.24-1.485.276-.843.038-1.096.047-3.232.047s-2.39-.009-3.233-.047c-.78-.036-1.203-.166-1.485-.276a2.478 2.478 0 0 1-.92-.598 2.48 2.48 0 0 1-.6-.92c-.109-.281-.24-.705-.275-1.485-.038-.843-.046-1.096-.046-3.233 0-2.136.008-2.388.046-3.231.036-.78.166-1.204.276-1.486.145-.373.319-.64.599-.92.28-.28.546-.453.92-.598.282-.11.705-.24 1.485-.276.738-.034 1.024-.044 2.515-.045v.002zm4.988 1.328a.96.96 0 1 0 0 1.92.96.96 0 0 0 0-1.92zm-4.27 1.122a4.109 4.109 0 1 0 0 8.217 4.109 4.109 0 0 0 0-8.217zm0 1.441a2.667 2.667 0 1 1 0 5.334 2.667 2.667 0 0 1 0-5.334z" />
-            </svg>
+            ></Image>
           </Button>
           <Button
             variant="light"
@@ -79,15 +81,12 @@ function MapleContainer({ className }: { className?: string }) {
             target="_blank"
             rel="noopener noreferrer"
           >
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
+            <Image
+              src="/images/twitter.svg"
+              alt="Twitter Icon"
               width="24"
               height="24"
-              fill="currentColor"
-              viewBox="0 0 16 16"
-            >
-              <path d="M5.026 15c6.038 0 9.341-5.003 9.341-9.334 0-.14 0-.282-.006-.422A6.685 6.685 0 0 0 16 3.542a6.658 6.658 0 0 1-1.889.518 3.301 3.301 0 0 0 1.447-1.817 6.533 6.533 0 0 1-2.087.793A3.286 3.286 0 0 0 7.875 6.03a9.325 9.325 0 0 1-6.767-3.429 3.289 3.289 0 0 0 1.018 4.382A3.323 3.323 0 0 1 .64 6.575v.045a3.288 3.288 0 0 0 2.632 3.218 3.203 3.203 0 0 1-.865.115 3.23 3.23 0 0 1-.614-.057 3.283 3.283 0 0 0 3.067 2.277A6.588 6.588 0 0 1 .78 13.58a6.32 6.32 0 0 1-.78-.045A9.344 9.344 0 0 0 5.026 15z" />
-            </svg>
+            ></Image>
           </Button>
           <Button
             variant="light"
@@ -96,12 +95,12 @@ function MapleContainer({ className }: { className?: string }) {
             target="_blank"
             rel="noopener noreferrer"
           >
-            <img
+            <Image
               src="/Linked In.svg"
               alt="LinkedIn Icon"
               width="24"
               height="24"
-            ></img>
+            ></Image>
           </Button>
         </Col>
       </Row>
@@ -121,7 +120,6 @@ const ResourcesLinks = () => {
   const { t } = useTranslation("footer")
   return (
     <>
-      <TextHeader>{t("headers.resources")}</TextHeader>
       <StyledExternalLink href="https://malegislature.gov/Search/FindMyLegislator">
         {t("links.resourcesLegislators")}
       </StyledExternalLink>
@@ -139,7 +137,6 @@ const BrowseLinks = () => {
   const { t } = useTranslation("footer")
   return (
     <>
-      <TextHeader>{t("headers.browse")}</TextHeader>
       <StyledInternalLink href="/bills">
         {t("links.browseBills")}
       </StyledInternalLink>
@@ -154,7 +151,6 @@ const OurTeamLinks = () => {
   const { t } = useTranslation("footer")
   return (
     <>
-      <TextHeader>{t("headers.team")}</TextHeader>
       <StyledExternalLink href="https://www.nulawlab.org">
         {t("links.teamNEU")}
       </StyledExternalLink>
@@ -170,7 +166,6 @@ const AccountLinks = ({ authenticated, user, signOut }: PageFooterProps) => {
   const { t } = useTranslation("footer")
   return (
     <>
-      <TextHeader>{t("headers.account")}</TextHeader>
       {authenticated ? (
         <>
           <StyledInternalLink
@@ -197,7 +192,6 @@ const LearnLinks = () => {
   const { t } = useTranslation("footer")
   return (
     <>
-      <TextHeader>{t("headers.learn")}</TextHeader>
       <StyledInternalLink href="/learn/writing-effective-testimony">
         {t("links.learnWriting")}
       </StyledInternalLink>
@@ -215,7 +209,6 @@ const AboutLinks = () => {
   const { t } = useTranslation("footer")
   return (
     <>
-      <TextHeader>{t("headers.about")}</TextHeader>
       <StyledInternalLink href="/about/mission-and-goals">
         {t("links.aboutMission")}
       </StyledInternalLink>
@@ -229,21 +222,58 @@ const AboutLinks = () => {
 const PageFooter = (props: PageFooterProps) => {
   const { t } = useTranslation("footer")
   return (
-    <Container
+    <FooterContainer
       fluid
       className="bg-black d-flex flex-wrap flex-column-reverse flex-md-row align-items-center align-items-md-stretch p-2 p-md-5"
     >
+      <Navbar
+        variant="dark"
+        expand="lg"
+        className="d-md-none w-100 order-1 p-2 mb-2"
+      >
+        <Nav className={`d-flex w-100`}>
+          <CustomDropdown title={t("headers.browse")}>
+            <BrowseLinks />
+          </CustomDropdown>
+          <CustomDropdown title={t("headers.account")}>
+            <AccountLinks {...props} />
+          </CustomDropdown>
+
+          <CustomDropdown title={t("headers.learn")}>
+            <LearnLinks />
+          </CustomDropdown>
+
+          <CustomDropdown title={t("headers.about")}>
+            <AboutLinks />
+          </CustomDropdown>
+
+          <CustomDropdown title={t("headers.resources")}>
+            <ResourcesLinks />
+          </CustomDropdown>
+
+          <CustomDropdown title={t("headers.team")}>
+            <OurTeamLinks />
+          </CustomDropdown>
+        </Nav>
+      </Navbar>
       <div className={`d-none d-md-flex order-1 flex-grow-1`}>
         <Col>
+          <TextHeader>{t("headers.browse")}</TextHeader>
           <BrowseLinks />
+          <TextHeader>{t("headers.account")}</TextHeader>
+
           <AccountLinks {...props} />
         </Col>
         <Col>
+          <TextHeader>{t("headers.learn")}</TextHeader>
           <LearnLinks />
+          <TextHeader>{t("headers.about")}</TextHeader>
           <AboutLinks />
         </Col>
         <Col>
+          <TextHeader>{t("headers.resources")}</TextHeader>
           <ResourcesLinks />
+          <TextHeader>{t("headers.team")}</TextHeader>
           <OurTeamLinks />
         </Col>
       </div>
@@ -269,7 +299,7 @@ const PageFooter = (props: PageFooterProps) => {
           </StyledInternalLink>
         </Col>
       </div>
-    </Container>
+    </FooterContainer>
   )
 }
 
