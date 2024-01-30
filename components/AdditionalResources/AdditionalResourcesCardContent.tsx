0b176476f29b3147f24e6a6c2b3b236ad043920d
sashamaryl <sashamaryl@gmail.com>
2024-01-30 07:55:44 -0500

    run prettier

--- components/AdditionalResources/AdditionalResourcesCardContent.tsx
+++ components/AdditionalResources/AdditionalResourcesCardContent.tsx
@@ -4,9 +4,7 @@ const AdditionalResourcesCardContent: FC<PropsWithChildren<{}>> = ({
   children
 }) => {
   return (
-    <div
-      className={`d-flex flex-0 justify-content-xs-center p-4 `}
-    >
+    <div className={`d-flex flex-0 justify-content-xs-center p-4 `}>
       <p className={`fs-4`}>{children}</p>
     </div>
   )
