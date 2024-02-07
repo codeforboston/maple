5eafb6e5ef19d5891cc1b2129460c708ef3336c9
sashamaryl <sashamaryl@gmail.com>
2024-02-06 19:35:44 -0500

    clean up commented code

--- components/ProfilePage/StyledProfileComponents.tsx
+++ components/ProfilePage/StyledProfileComponents.tsx
@@ -8,22 +8,8 @@ export const StyledImage = styled(Image)`
 `
 
 export const Header = styled("div").attrs(props => ({
-  className: `fs-1 d-flex flex-column flex-md-row align-items-center my-5 px-0 h-auto gx-0 ${props.className}`
-}))`
-  /* font-size: 39px; */
-  /* font-family: Nunito; */
-  /* font-weight: 500; */
-  /* flex-direction: column !important; */
-  /* align-items: center; */
-  /* margin: 2.5rem 0; */
-  /* padding-right: 0; */
-  /* padding-left: 0; */
-  /* height: auto; */
-
-  /* @media (min-width: 768px) {
-    flex-direction: row !important;
-  } */
-`
+  className: `h1 d-flex flex-column flex-md-row align-items-center my-5 px-0 h-auto gx-0 ${props.className}`
+}))``
 
 export const ContactInfoRow = styled(Row)`
   font-size: 1.375rem;
@@ -37,99 +23,8 @@ export const ProfileDisplayName = styled.div.attrs<{ large: boolean }>(
       props.large ? "h1" : "h2"
     } text-left text-black tracking-narrow ${props.className}`
   })
-)`
-  /* margin: 0; */
-  max-height: 108px;
-  /* font-family: Nunito; */
-  /* font-weight: 500; */
-  /* font-size: 39px; */
-  /* letter-spacing: -0.015em; */
-  /* text-align: left; */
-  /* color: #000; */
-
-  /* these classes not used */
-  /* .firstName {
-    font-size: 1.5rem;
-  }
-
-  .lastName {
-    font-size: 2.75rem;
-  } */
-`
+)` max-height: 108px; `
 
 export const ProfileDisplayNameSmall = styled(Col).attrs(props => ({
   className: `m-0 h2 text-left text-black tracking-narrow${props.className}`
-}))`
-  /* margin: 0;
-  font-family: Nunito;
-  font-weight: 500;
-  font-size: 29px;
-  letter-spacing: -0.015em;
-  text-align: left;
-  color: #000; */
-`
-
-// export const StyledContainer = styled(Container)`
-//   .about-me-checkbox input {
-//     height: 25px;
-//     width: 25px;
-//     margin-top: 0;
-//     border-color: #12266f;
-//   }
-
-//   .input-social-media::placeholder {
-//     font-size: 12px;
-//   }
-
-//   .save-profile-button > button {
-//     width: 100%;
-//   }
-
-//   .edit-profile-header {
-//     flex-direction: column !important;
-//     height: auto;
-//   }
-
-//   .your-legislators-width {
-//     width: 100%;
-//   }
-
-//   .view-edit-profile,
-//   .follow-button {
-//     width: 100%;
-//     text-decoration: none;
-//   }
-
-//   .view-edit-profile > button,
-//   .follow-button > button {
-//     width: 100%;
-//   }
-
-//   @media (min-width: 768px) {
-//     .edit-profile-header {
-//       flex-direction: row !important;
-//     }
-
-//     .your-legislators-width {
-//       width: 50%;
-//     }
-
-//     .save-profile-button {
-//       align-self: flex-end;
-//       width: auto;
-//     }
-
-//     .view-edit-profile {
-//       display: flex;
-//       justify-content: flex-end;
-//     }
-
-//     .view-edit-profile > button {
-//       width: auto;
-//     }
-
-//     .follow-button > button {
-//       width: unset;
-//     }
-//   }
-// `
+}))``
