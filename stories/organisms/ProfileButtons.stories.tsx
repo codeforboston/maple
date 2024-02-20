21ac8c714cdec79db8e790ddfa29da188f07958f
sashamaryl <sashamaryl@gmail.com>
2024-02-20 11:44:32 -0500

    fix not found import in storybook profile buttons

--- stories/organisms/ProfileButtons.stories.tsx
+++ stories/organisms/ProfileButtons.stories.tsx
@@ -1,13 +1,9 @@
-import { StoryObj, Meta } from "@storybook/react"
-import {
-  ToggleProfilePulicity,
-  EditProfileButton
-} from "components/ProfilePage/ProfileButtons"
+import { Meta, StoryObj } from "@storybook/react"
 import { ProfileButtonsUser } from "components/ProfilePage/ProfileHeader"
-import { wrapper } from "components/store"
-import { Provider as Redux } from "react-redux"
 import { Providers } from "components/providers"
-import { Button, Col, Row, Stack } from "react-bootstrap"
+import { wrapper } from "components/store"
+import { Col, Row } from "react-bootstrap"
+import { Provider as Redux } from "react-redux"
 
 const meta: Meta = {
   title: "Organisms/Profile/ProfileButtons",
@@ -44,48 +40,3 @@ export const Primary: Story = {
   },
   name: "ProfileButtons"
 }
-
-// @TODO: [next] clean up display of these buttons
-// @TODO: [later] refactor this component to use the shared project buttons [refactor phase]
-
-// export default createMeta({
-//   title: "Organisms/Profile/ProfileButtons",
-//   component: MakePublicButton,
-//   decorators: [
-//     (Story, ...rest) => {
-//       const { store, props } = wrapper.useWrappedStore(...rest)
-
-//       return (
-//         <Redux store={store}>
-//           <Providers>
-//             <Row>
-//               <Col className="col-4">
-//                 <Story />
-//               </Col>
-//             </Row>
-//           </Providers>
-//         </Redux>
-//       )
-//     }
-//   ]
-// })
-
-// export const Primary = Template.bind({})
-
-// Primary.args = {
-//   isProfilePublic: true,
-//   onProfilePublicityChanged: () => {
-//     console.log("clicked")
-//   }
-// }
-// Primary.storyName = "ProfileButtons"
-
-// export const Buttons = () => (
-//   <Stack className="gap-2">
-//     <EditProfileButton />
-//     <StyledButton>StyledButton1</StyledButton>
-//     <StyledButton2>StyledButton2</StyledButton2>
-//     <Button variant="primary">Primary Button</Button>
-//     <Button variant="secondary">Secondary Button</Button>
-//   </Stack>
-// )
