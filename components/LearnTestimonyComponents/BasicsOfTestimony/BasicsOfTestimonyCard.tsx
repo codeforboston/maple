cf3981eb0b4e6b6b002c05f6d10b98f1b7fe4a88
Mephistic <deathbyfiresermon@gmail.com>
2024-02-20 22:49:38 -0500

    Moving more styles out of modules for BasicsOfTestimony

--- components/LearnTestimonyComponents/BasicsOfTestimony/BasicsOfTestimonyCard.tsx
+++ components/LearnTestimonyComponents/BasicsOfTestimony/BasicsOfTestimonyCard.tsx
@@ -1,3 +1,4 @@
+import styled from "styled-components"
 import { Col, Image } from "../../bootstrap"
 import styles from "./BasicsOfTestimonyCard.module.css"
 
@@ -9,11 +10,71 @@ export type BasicsOfTestimonyCardProps = {
   src: string
 }
 
+// TODO: Consider moving outside this into a CommonComponents class
+// TODO: Check for overlap between component styles between the seconds
+//       of Learn Testimony. They're all just a bit different, but we can 
+//       likely reduce boilerplate here.
+const StyledCard = styled.div`
+  width: 100%;
+  height: auto;
+  padding: 2rem 3rem;
+  flex-direction: row;
+  flex-wrap: wrap;
+  display: flex;
+  margin: 3rem 0;
+  @media (max-width: 48em) {
+    margin: 0;
+    padding: 0;
+  }
+`
+
+const StyledImageCol = styled(Col)`
+  width: 20rem;
+  display: flex;
+  align-items: center;
+  @media (max-width: 48em) {
+    width: 50%;
+    margin: 0 25%;
+    transform: translate(0, 2rem);
+  }
+`
+
+const StyledTitle = styled.div`
+  font-family: "Nunito";
+  font-size: 1.5rem;
+  font-weight: bold;
+  font-stretch: normal;
+  font-style: normal;
+  line-height: normal;
+  letter-spacing: -0.63px;
+  text-align: left;
+  color: #000;
+  margin-bottom: 1rem;
+  @media (max-width: 36em) {
+    font-size: 1.25rem;
+  }
+`
+
+const StyledContent = styled.div`
+  flex-grow: 0;
+  font-family: "Nunito";
+  font-size: 1.5rem;
+  font-weight: normal;
+  font-stretch: normal;
+  font-style: normal;
+  line-height: normal;
+  letter-spacing: -0.63px;
+  text-align: left;
+  color: #000;
+  @media (max-width: 36em) {
+    font-size: 1.125rem;
+  }
+`
+
 const BasicsOfTestimonyCard = ({ title, index, alt, paragraph, src }: BasicsOfTestimonyCardProps) => {
   return (
-    <div className={styles.card}>
-      <Col
-        className={styles.image}
+    <StyledCard>
+      <StyledImageCol
         md={6}
         lg={{ order: index % 2 == 0 ? 0 : 5 }}
       >
@@ -23,15 +84,15 @@ const BasicsOfTestimonyCard = ({ title, index, alt, paragraph, src }: BasicsOfTe
           src={src}
           alt={alt}
         />
-      </Col>
+      </StyledImageCol>
       <Col
         className={index % 2 == 0 ? styles.textRight : styles.textLeft}
         lg={{ order: 3 }}
       >
-        <div className={styles.title}>{title}</div>
-        <div className={styles.content}>{paragraph}</div>
+        <StyledTitle>{title}</StyledTitle>
+        <StyledContent>{paragraph}</StyledContent>
       </Col>
-    </div>
+    </StyledCard>
   )
 }
 
