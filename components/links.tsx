c6c06656192e1d7777cf8a978c24d18bb0fb99f5
Celena T <chibiynm@gmail.com>
2024-02-25 19:19:23 -0500

    Replace text with i18n translations

--- components/links.tsx
+++ components/links.tsx
@@ -1,3 +1,4 @@
+import { useTranslation } from "next-i18next"
 import Link from "next/link"
 import { forwardRef, PropsWithChildren } from "react"
 import { CurrentCommittee } from "../functions/src/bills/types"
