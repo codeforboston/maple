296ce3e6b8b3ff2f38b9266fbf65b89fafe8761d
Mephistic <deathbyfiresermon@gmail.com>
2024-03-05 16:25:43 -0500

    Using bootstrap utility classes for TestimonyCardComponents

--- components/LearnTestimonyComponents/RoleOfTestimony/RoleOfTestimonyCard.tsx
+++ components/LearnTestimonyComponents/RoleOfTestimony/RoleOfTestimonyCard.tsx
@@ -29,7 +29,7 @@ const RoleOfTestimonyCard = ({
         <Image fluid src={src} alt={alt} style={{ height: "10rem" }} />
       </Col>
       <Col
-        className={clsx("d-flex flex-column justify-content-center pt-3 pt-md-0", index % 2 === 0 && "pl-3 pl-md-5")}
+        className={clsx("d-flex flex-column justify-content-center pt-3 pt-md-0", index % 2 === 0 && "ps-3 ps-md-5")}
         sm={{ span: 12, order: 1 }}
         md={{ span: 6, order: 3 }}
       >
--- components/LearnTestimonyComponents/TestimonyCardComponents.tsx
+++ components/LearnTestimonyComponents/TestimonyCardComponents.tsx
@@ -31,43 +31,22 @@ export type TestimonyCardProps = {
 
 const StyledCard = styled(Card)`
   margin: 8rem 5rem;
-  padding-top: 0;
-  padding-bottom: 0;
-  border-radius: 12px;
-  background: #ffffff;
+
   @media (max-width: 48em) {
     margin: 8rem 3rem;
   }
 `
 const StyledHeader = styled(Card.Header)`
-  background-color: var(--bs-blue);
   width: max-content;
-  padding-top: 0;
-  padding-bottom: 0;
-  padding-right: 4rem;
-  padding-left: 2rem;
-  height: 5rem;
   transform: translate(-3rem, -40%);
-  overflow: hidden;
-  line-height: 5rem;
-  font-weight: 900;
+
   &:first-child {
     border-radius: 0 5rem 5rem 0;
   }
   @media (max-width: 48em) {
-    font-size: 1.5rem;
-    padding-right: 3rem;
-    padding-left: 1.5rem;
-    height: 4rem;
-    line-height: 4rem;
     transform: translate(-2rem, -40%);
   }
   @media (max-width: 36em) {
-    font-size: 1.125rem;
-    padding-right: 3rem;
-    padding-left: 1.5rem;
-    height: 2.5rem;
-    line-height: 2.5rem;
     transform: translate(-1.5rem, -40%);
   }
 `
@@ -76,8 +55,11 @@ const TestimonyCard = ({
   shouldAlternateImage
 }: TestimonyCardProps) => {
   return (
-    <StyledCard>
-      <StyledHeader as="h1" className={`text-center text-white`}>
+    <StyledCard className="py-0 rounded-3 bg-white">
+      <StyledHeader 
+        as="h1" 
+        className="d-flex align-items-center py-3 ps-4 pe-5 bg-secondary text-center text-white fw-bolder overflow-hidden"
+      >
         {content.title}
       </StyledHeader>
       <TestimonyCardContent
@@ -88,39 +70,12 @@ const TestimonyCard = ({
   )
 }
 
-const StyledCol = styled(Col)`
-  padding-right: 4rem;
-  padding-left: 2rem;
-  display: flex;
-  flex-direction: row;
-  justify-content: center;
-  @media (max-width: 29em) {
-    padding-right: 2rem;
-    padding-left: 1rem;
-  }
-`
 const StyledImage = styled(Image)`
   @media (max-width: 36em) {
     width: 12rem;
   }
   @media (max-width: 29em) {
     width: 10rem;
-  }
-`
-const StyledTextWrapper = styled.div`
-  flex-grow: 0;
-  font-family: "Nunito";
-  font-weight: normal;
-  font-stretch: normal;
-  font-style: normal;
-  letter-spacing: -0.63px;
-  text-align: left;
-  color: #000;
-  @media (max-width: 62em) {
-    padding-top: 1.5rem;
-  }
-  @media (max-width: 36em) {
-    padding-top: 1rem;
   }
 `
 
@@ -132,26 +87,24 @@ const TestimonyCardContent = ({
     <Card.Body>
       <Container fluid>
         <Row className="my-auto align-items-center flex-row">
-          <StyledCol
-            className="text-center align-self-center justify-content-xs-center"
+          <Col
+            className="d-flex flex-row pl-3 pr-4 pl-s-4 pr-s-5 text-center align-self-center justify-content-center"
             md={12}
             lg={{ span: 4, order: shouldAlternateImage ? 4 : 1 }}
           >
             <div>
               <StyledImage fluid src={`/${content.src}`} alt={content.alt} />
             </div>
-          </StyledCol>
-          <StyledCol
-            className="text-center align-self-center justify-content-xs-center"
+          </Col>
+          <Col
+            className="d-flex flex-column pl-3 pr-4 pl-sm-4 pr-sm-5 pt-3 pt-sm-4 pt-md-0 text-center align-self-center justify-content-center tracking-tighter"
             md={12}
             lg={{ span: 8, order: 2 }}
           >
-            <StyledTextWrapper>
-              {content.paragraphs.map((paragraph, index) => (
-                <p key={index}>{paragraph}</p>
-              ))}
-            </StyledTextWrapper>
-          </StyledCol>
+            {content.paragraphs.map((paragraph, index) => (
+              <p className="text-start" key={index}>{paragraph}</p>
+            ))}
+          </Col>
         </Row>
       </Container>
     </Card.Body>
