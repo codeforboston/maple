0fe03929228a6c431e050c244de4957dee3c2812
Mephistic <deathbyfiresermon@gmail.com>
2024-03-13 00:13:04 -0400

    Migrating LobbyingTable to bootstrap

--- components/bill/LobbyingTable.tsx
+++ components/bill/LobbyingTable.tsx
@@ -1,15 +1,8 @@
-import { format, fromUnixTime } from "date-fns"
-import styled from "styled-components"
-import { Row } from "../bootstrap"
+import { Card, Container } from "../bootstrap"
 import { FC } from "../types"
-import { LabeledContainer } from "./LabeledContainer"
-import styles from "./SponsorsAndCommittees.module.css"
 import { BillProps } from "./types"
-import { billSiteURL, Wrap } from "components/links"
-import { useContext } from "react"
-import { BillHistory } from "../db"
-import { CourtContext } from "./Status"
-import { Button, Modal, Table } from "react-bootstrap"
+import { Table } from "react-bootstrap"
+import { Card as MapleCard } from "../Card"
 
 export const LobbyingTable: FC<React.PropsWithChildren<BillProps>> = ({
   bill,
@@ -18,31 +11,40 @@ export const LobbyingTable: FC<React.PropsWithChildren<BillProps>> = ({
   const current = bill.currentCommittee
   if (!current) return null
   return (
-    <LabeledContainer className={className}>
-      <Row className={`bg-secondary text-light ${styles.subHeader}`}>
-        Lobbying Parties
-      </Row>
-      <Table>
-        <thead>
-          <tr>
-            <th>Client Name</th>
-            <th>Position</th>
-            <th>Disclosure Date</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr>
-            <td>Example Name</td>
-            <td>Pro</td>
-            <td>April 10, 2023</td>
-          </tr>
-          <tr>
-            <td>Example Name</td>
-            <td>Neutral</td>
-            <td>March 29, 2023</td>
-          </tr>
-        </tbody>
-      </Table>
-    </LabeledContainer>
+    <Container className="p-0">
+      <MapleCard
+        className={`${className} bg-white`}
+        headerElement={
+          <Card.Header className="h4 bg-secondary text-light">
+            Lobbying Parties
+          </Card.Header>
+        }
+        body={
+          <Card.Body>
+            <Table>
+              <thead>
+                <tr>
+                  <th>Client Name</th>
+                  <th>Position</th>
+                  <th>Disclosure Date</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <td>Example Name</td>
+                  <td>Pro</td>
+                  <td>April 10, 2023</td>
+                </tr>
+                <tr>
+                  <td>Example Name</td>
+                  <td>Neutral</td>
+                  <td>March 29, 2023</td>
+                </tr>
+              </tbody>
+            </Table>
+          </Card.Body>
+        }
+      />
+    </Container>
   )
 }
