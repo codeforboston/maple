9ac7cdcb3e2722d4a1a8b9178d658d6db6b5bf1d
Mephistic <deathbyfiresermon@gmail.com>
2024-03-23 23:58:19 -0400

    Migrating AboutPagesCard to bootstrap/styled components

--- components/AboutPagesCard/AboutPagesCard.js
+++ components/AboutPagesCard/AboutPagesCard.js
@@ -1,12 +1,32 @@
+import styled from "styled-components"
 import { Card } from "../bootstrap"
-import styles from "./AboutPagesCard.module.css"
+
+const StyledHeader = styled(Card.Header)`
+  transform: translate(0);
+
+  &:first-child {
+    border-radius: 10px 10px 0 0;
+  }
+
+  @media (min-width: 48em) {
+    width: max-content;
+    transform: translate(-3rem, -40%);
+
+    &:first-child {
+      border-radius: 0 5rem 5rem 0;
+    }
+  }
+`
 
 const AboutPagesCard = ({ title, children }) => {
   return (
-    <Card className={"m-5 " + styles.card}>
-      <Card.Header as="h3" className={`${styles.header}`}>
+    <Card className="m-5 rounded-3 bg-white">
+      <StyledHeader
+        as="h1"
+        className="bg-secondary text-white overflow-hidden fs-sm-1 pt-2 pt-sm-3 pb-1 pb-sm-3 ps-4 ps-sm-5 pe-5"
+      >
         {title}
-      </Card.Header>
+      </StyledHeader>
       <Card.Body>{children}</Card.Body>
     </Card>
   )
