9a05bbfb5f9a914c6ecddaf69f0810e83f264c39
mertbagt <73559781+mertbagt@users.noreply.github.com>
2024-04-25 08:46:42 -0400

    Update legislative-process.tsx - prettier

--- pages/learn/legislative-process.tsx
+++ pages/learn/legislative-process.tsx
@@ -11,7 +11,7 @@ export default createPage({
     return (
       <Container>
         <Legislative />
-        <AdditionalResources/>
+        <AdditionalResources />
       </Container>
     )
   }
