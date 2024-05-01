b52ce6b4607d62905a653aaf3b76c7bbb79d0e86
Merritt Baggett <mertbagt@gmail.com>
2024-05-01 15:49:47 -0400

    refactor user dropdown

--- components/DesktopNav.tsx
+++ components/DesktopNav.tsx
@@ -5,7 +5,7 @@ import Image from "react-bootstrap/Image"
 import { useMediaQuery } from "usehooks-ts"
 import { SignInWithButton, signOutAndRedirectToHome, useAuth } from "./auth"
 import AuthModal from "./auth/AuthModal"
-import { Container, Nav, NavDropdown, Navbar } from "./bootstrap"
+import { Container, Dropdown, Nav, NavDropdown, Navbar } from "./bootstrap"
 import { useProfile } from "./db"
 import PageFooter from "./Footer/Footer"
 import { NavBarBox, NavBarBoxContainer } from "./layout"
@@ -13,13 +13,15 @@ import { NavLink } from "./Navlink"
 import ProfileLink from "./ProfileLink"
 
 export const DesktopNav: React.FC<React.PropsWithChildren<unknown>> = () => {
-  const { authenticated, claims } = useAuth()
+  const { authenticated, claims, user } = useAuth()
   const { profile } = useProfile()
   const [isExpanded, setIsExpanded] = useState(false)
   const { t } = useTranslation(["common", "auth"])
 
   const toggleNav = () => setIsExpanded(!isExpanded)
   const closeNav = () => setIsExpanded(false)
+
+  const userLink = "/profile?id=" + user?.uid
 
   return (
     <>
@@ -71,95 +73,145 @@ export const DesktopNav: React.FC<React.PropsWithChildren<unknown>> = () => {
 
           <NavBarBox className="align-self-center">
             <div className="mx-auto">
-              <NavDropdown
-                className={"navLink-primary text-white-50"}
-                title={t("about")}
-              >
-                <NavDropdown.Item>
-                  <NavLink href="/about/faq-page" handleClick={closeNav}>
-                    {t("navigation.faq")}
-                  </NavLink>
-                </NavDropdown.Item>
-                <NavDropdown.Item>
-                  <NavLink
-                    href="/about/mission-and-goals"
-                    handleClick={closeNav}
-                  >
-                    {t("navigation.missionAndGoals")}
-                  </NavLink>
-                </NavDropdown.Item>
-                <NavDropdown.Item>
-                  <NavLink href="/about/our-team" handleClick={closeNav}>
-                    {t("team")}
-                  </NavLink>
-                </NavDropdown.Item>
-                <NavDropdown.Item>
-                  <NavLink href="/about/support-maple" handleClick={closeNav}>
-                    {t("navigation.supportMaple")}
-                  </NavLink>
-                </NavDropdown.Item>
-                <NavDropdown.Item>
-                  <NavLink href="/policies" handleClick={closeNav}>
-                    {t("navigation.privacyAndConduct")}
-                  </NavLink>
-                </NavDropdown.Item>
-              </NavDropdown>
+              <Dropdown>
+                <Dropdown.Toggle className="btn-secondary text-white-50">
+                  {t("about")}
+                </Dropdown.Toggle>
+                <Dropdown.Menu>
+                  <NavDropdown.Item>
+                    <NavLink href="/about/faq-page" handleClick={closeNav}>
+                      {t("navigation.faq")}
+                    </NavLink>
+                  </NavDropdown.Item>
+                  <NavDropdown.Item>
+                    <NavLink
+                      href="/about/mission-and-goals"
+                      handleClick={closeNav}
+                    >
+                      {t("navigation.missionAndGoals")}
+                    </NavLink>
+                  </NavDropdown.Item>
+                  <NavDropdown.Item>
+                    <NavLink href="/about/our-team" handleClick={closeNav}>
+                      {t("team")}
+                    </NavLink>
+                  </NavDropdown.Item>
+                  <NavDropdown.Item>
+                    <NavLink href="/about/support-maple" handleClick={closeNav}>
+                      {t("navigation.supportMaple")}
+                    </NavLink>
+                  </NavDropdown.Item>
+                  <NavDropdown.Item>
+                    <NavLink href="/policies" handleClick={closeNav}>
+                      {t("navigation.privacyAndConduct")}
+                    </NavLink>
+                  </NavDropdown.Item>
+                </Dropdown.Menu>
+              </Dropdown>
             </div>
           </NavBarBox>
 
           <NavBarBox className="align-self-center">
             <div className="mx-auto">
-              <NavDropdown
-                className={"navLink-primary text-white-50"}
-                title={t("learn")}
-              >
-                <NavDropdown.Item>
-                  <NavLink
-                    href="/learn/to-write-effective-testimony"
-                    handleClick={closeNav}
-                  >
-                    {t("navigation.toWriteEffectiveTestimony")}
-                  </NavLink>
-                </NavDropdown.Item>
-                <NavDropdown.Item>
-                  <NavLink
-                    href="/learn/legislative-process"
-                    handleClick={closeNav}
-                  >
-                    {t("navigation.legislativeProcess")}
-                  </NavLink>
-                </NavDropdown.Item>
-                <NavDropdown.Item>
-                  <NavLink
-                    href="/why-use-maple/for-individuals"
-                    handleClick={closeNav}
-                  >
-                    {t("navigation.forIndividuals")}
-                  </NavLink>
-                </NavDropdown.Item>
-                <NavDropdown.Item>
-                  <NavLink
-                    href="/why-use-maple/for-orgs"
-                    handleClick={closeNav}
-                  >
-                    {t("navigation.forOrganizations")}
-                  </NavLink>
-                </NavDropdown.Item>
-                <NavDropdown.Item>
-                  <NavLink
-                    href="/why-use-maple/for-legislators"
-                    handleClick={closeNav}
-                  >
-                    {t("navigation.forLegislators")}
-                  </NavLink>
-                </NavDropdown.Item>
-              </NavDropdown>
+              <Dropdown>
+                <Dropdown.Toggle className="btn-secondary text-white-50">
+                  {t("learn")}
+                </Dropdown.Toggle>
+                <Dropdown.Menu>
+                  <NavDropdown.Item>
+                    <NavLink
+                      href="/learn/to-write-effective-testimony"
+                      handleClick={closeNav}
+                    >
+                      {t("navigation.toWriteEffectiveTestimony")}
+                    </NavLink>
+                  </NavDropdown.Item>
+                  <NavDropdown.Item>
+                    <NavLink
+                      href="/learn/legislative-process"
+                      handleClick={closeNav}
+                    >
+                      {t("navigation.legislativeProcess")}
+                    </NavLink>
+                  </NavDropdown.Item>
+                  <NavDropdown.Item>
+                    <NavLink
+                      href="/why-use-maple/for-individuals"
+                      handleClick={closeNav}
+                    >
+                      {t("navigation.forIndividuals")}
+                    </NavLink>
+                  </NavDropdown.Item>
+                  <NavDropdown.Item>
+                    <NavLink
+                      href="/why-use-maple/for-orgs"
+                      handleClick={closeNav}
+                    >
+                      {t("navigation.forOrganizations")}
+                    </NavLink>
+                  </NavDropdown.Item>
+                  <NavDropdown.Item>
+                    <NavLink
+                      href="/why-use-maple/for-legislators"
+                      handleClick={closeNav}
+                    >
+                      {t("navigation.forLegislators")}
+                    </NavLink>
+                  </NavDropdown.Item>
+                </Dropdown.Menu>
+              </Dropdown>
             </div>
           </NavBarBox>
 
-          <NavBarBox className={`align-self-center justify-content-end`}>
-            <ProfileLink role={claims?.role} fullName={profile?.fullName} />
+          <NavBarBox className="align-self-center">
+            <div className="mx-auto">
+              <Dropdown>
+                <Dropdown.Toggle className="btn-secondary">
+                  <Image
+                    className="mx-2"
+                    src="/profile-icon.svg"
+                    alt="profile icon"
+                  />
+                </Dropdown.Toggle>
+                <Dropdown.Menu>
+                  <NavDropdown.Item>
+                    <NavLink
+                      className={"navLink-primary"}
+                      handleClick={() => {
+                        location.assign(userLink)
+                      }}
+                    >
+                      View Profile
+                    </NavLink>
+                  </NavDropdown.Item>
+                  <NavDropdown.Item>
+                    <NavLink
+                      className={"navLink-primary"}
+                      href="/editprofile"
+                      handleClick={closeNav}
+                    >
+                      Edit Profile
+                    </NavLink>
+                  </NavDropdown.Item>
+                  <NavDropdown.Item>
+                    <NavLink
+                      className={"navLink-primary"}
+                      handleClick={() => {
+                        closeNav()
+                        void signOutAndRedirectToHome()
+                      }}
+                    >
+                      Sign Out
+                    </NavLink>
+                  </NavDropdown.Item>
+                </Dropdown.Menu>
+              </Dropdown>
+            </div>
           </NavBarBox>
+
+          {/* <NavBarBox className={`align-self-center justify-content-end`}>
+            <ProfileLink role={claims?.role} fullName={profile?.fullName} />
+          </NavBarBox> */}
         </NavBarBoxContainer>
       </Container>
       {/* </Navbar> */}
@@ -167,5 +219,5 @@ export const DesktopNav: React.FC<React.PropsWithChildren<unknown>> = () => {
   )
 }
 
-// refactor ProfileLink for dropdown to properly use z:index
 // rework spacing amoung elements in dropbox (primarily right to left spacing)
+// replace profile icon with white border/blue person?
