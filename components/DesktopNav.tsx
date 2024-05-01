94c2a4974f8a992e01ca050baabe7450f99e5270
Merritt Baggett <mertbagt@gmail.com>
2024-05-01 13:57:27 -0400

    basic layout for desktop nav

--- components/DesktopNav.tsx
+++ components/DesktopNav.tsx
@@ -32,146 +32,125 @@ export const DesktopNav: React.FC<React.PropsWithChildren<unknown>> = () => {
     >
       <Container fluid>
         <NavBarBoxContainer>
-          <div
-            className={`col d-flex justify-content-start align-items-center`}
-          >
+          <div className={`col justify-content-start align-items-center`}>
             <Nav.Link href="/" className="py-0 px-2 w-100">
               <Image src="/Logo2024.png" alt="logo" width="80" height="80" />
             </Nav.Link>
           </div>
 
-          <button className={`bg-light col`}>
-            <div className={` text-dark`}>Placeholder Search Widget</div>
-          </button>
+          <div className={`align-self-center col d-flex `}>
+            <button className={`bg-light col mx-2 my-2`}>
+              <div className={`text-dark`}>Placeholder Search Widget</div>
+            </button>
+          </div>
 
-          <NavBarBox>
-            <Navbar expand={false} expanded={isExpanded}>
-              <Navbar.Brand>
-                <Navbar.Toggle aria-controls="topnav" onClick={toggleNav} />
-              </Navbar.Brand>
-              <Navbar.Collapse id="topnav">
-                <Nav className="me-auto">
-                  <NavLink
-                    className={"navLink-primary"}
-                    href="/"
-                    handleClick={closeNav}
-                  >
-                    {t("navigation.home")}
-                  </NavLink>
-                  <NavLink
-                    className={"navLink-primary"}
-                    href="/bills"
-                    handleClick={closeNav}
-                  >
-                    {t("navigation.browseBills")}
-                  </NavLink>
-                  <NavLink
-                    className={"navLink-primary"}
-                    href="/testimony"
-                    handleClick={closeNav}
-                  >
-                    {t("navigation.browseTestimony")}
-                  </NavLink>
-
-                  <NavDropdown className={"navLink-primary"} title={t("learn")}>
-                    <NavDropdown.Item>
-                      <NavLink
-                        href="/learn/to-write-effective-testimony"
-                        handleClick={closeNav}
-                      >
-                        {t("navigation.toWriteEffectiveTestimony")}
-                      </NavLink>
-                    </NavDropdown.Item>
-                    <NavDropdown.Item>
-                      <NavLink
-                        href="/learn/legislative-process"
-                        handleClick={closeNav}
-                      >
-                        {t("navigation.legislativeProcess")}
-                      </NavLink>
-                    </NavDropdown.Item>
-                  </NavDropdown>
-
-                  <NavDropdown className={"navLink-primary"} title={t("about")}>
-                    <NavDropdown.Item>
-                      <NavLink href="/about/faq-page" handleClick={closeNav}>
-                        {t("navigation.faq")}
-                      </NavLink>
-                    </NavDropdown.Item>
-                    <NavDropdown.Item>
-                      <NavLink
-                        href="/about/mission-and-goals"
-                        handleClick={closeNav}
-                      >
-                        {t("navigation.missionAndGoals")}
-                      </NavLink>
-                    </NavDropdown.Item>
-                    <NavDropdown.Item>
-                      <NavLink href="/about/our-team" handleClick={closeNav}>
-                        {t("team")}
-                      </NavLink>
-                    </NavDropdown.Item>
-                    <NavDropdown.Item>
-                      <NavLink
-                        href="/about/support-maple"
-                        handleClick={closeNav}
-                      >
-                        {t("navigation.supportMaple")}
-                      </NavLink>
-                    </NavDropdown.Item>
-                    <NavDropdown.Item>
-                      <NavLink href="/policies" handleClick={closeNav}>
-                        {t("navigation.privacyAndConduct")}
-                      </NavLink>
-                    </NavDropdown.Item>
-                  </NavDropdown>
-
-                  <NavDropdown
-                    className={"navLink-primary"}
-                    title={t("whyUseMaple")}
-                  >
-                    <NavDropdown.Item>
-                      <NavLink
-                        href="/why-use-maple/for-individuals"
-                        handleClick={closeNav}
-                      >
-                        {t("navigation.forIndividuals")}
-                      </NavLink>
-                    </NavDropdown.Item>
-                    <NavDropdown.Item>
-                      <NavLink
-                        href="/why-use-maple/for-orgs"
-                        handleClick={closeNav}
-                      >
-                        {t("navigation.forOrganizations")}
-                      </NavLink>
-                    </NavDropdown.Item>
-                    <NavDropdown.Item>
-                      <NavLink
-                        href="/why-use-maple/for-legislators"
-                        handleClick={closeNav}
-                      >
-                        {t("navigation.forLegislators")}
-                      </NavLink>
-                    </NavDropdown.Item>
-                  </NavDropdown>
-
-                  {authenticated && (
-                    <NavLink
-                      className={"navLink-primary"}
-                      handleClick={() => {
-                        closeNav()
-                        void signOutAndRedirectToHome()
-                      }}
-                    >
-                      {t("signOut", { ns: "auth" })}
-                    </NavLink>
-                  )}
-                </Nav>
-              </Navbar.Collapse>
-            </Navbar>
+          <NavBarBox className="align-self-center">
+            <Nav className="mx-auto">
+              <NavLink
+                className={"navLink-primary"}
+                href="/bills"
+                handleClick={closeNav}
+              >
+                {t("navigation.browseBills")}
+              </NavLink>
+            </Nav>
           </NavBarBox>
-          <NavBarBox className={`justify-content-end`}>
+
+          <NavBarBox className="align-self-center">
+            <Nav className="mx-auto">
+              <NavLink
+                className={"navLink-primary"}
+                href="/testimony"
+                handleClick={closeNav}
+              >
+                {t("navigation.browseTestimony")}
+              </NavLink>
+            </Nav>
+          </NavBarBox>
+
+          <NavBarBox className="align-self-center">
+            <Nav className="mx-auto">
+              <NavDropdown className={"navLink-primary"} title={t("about")}>
+                <NavDropdown.Item>
+                  <NavLink href="/about/faq-page" handleClick={closeNav}>
+                    {t("navigation.faq")}
+                  </NavLink>
+                </NavDropdown.Item>
+                <NavDropdown.Item>
+                  <NavLink
+                    href="/about/mission-and-goals"
+                    handleClick={closeNav}
+                  >
+                    {t("navigation.missionAndGoals")}
+                  </NavLink>
+                </NavDropdown.Item>
+                <NavDropdown.Item>
+                  <NavLink href="/about/our-team" handleClick={closeNav}>
+                    {t("team")}
+                  </NavLink>
+                </NavDropdown.Item>
+                <NavDropdown.Item>
+                  <NavLink href="/about/support-maple" handleClick={closeNav}>
+                    {t("navigation.supportMaple")}
+                  </NavLink>
+                </NavDropdown.Item>
+                <NavDropdown.Item>
+                  <NavLink href="/policies" handleClick={closeNav}>
+                    {t("navigation.privacyAndConduct")}
+                  </NavLink>
+                </NavDropdown.Item>
+              </NavDropdown>
+            </Nav>
+          </NavBarBox>
+
+          <NavBarBox className="align-self-center">
+            <Nav className="mx-auto">
+              <NavDropdown className={"navLink-primary"} title={t("learn")}>
+                <NavDropdown.Item>
+                  <NavLink
+                    href="/learn/to-write-effective-testimony"
+                    handleClick={closeNav}
+                  >
+                    {t("navigation.toWriteEffectiveTestimony")}
+                  </NavLink>
+                </NavDropdown.Item>
+                <NavDropdown.Item>
+                  <NavLink
+                    href="/learn/legislative-process"
+                    handleClick={closeNav}
+                  >
+                    {t("navigation.legislativeProcess")}
+                  </NavLink>
+                </NavDropdown.Item>
+                <NavDropdown.Item>
+                  <NavLink
+                    href="/why-use-maple/for-individuals"
+                    handleClick={closeNav}
+                  >
+                    {t("navigation.forIndividuals")}
+                  </NavLink>
+                </NavDropdown.Item>
+                <NavDropdown.Item>
+                  <NavLink
+                    href="/why-use-maple/for-orgs"
+                    handleClick={closeNav}
+                  >
+                    {t("navigation.forOrganizations")}
+                  </NavLink>
+                </NavDropdown.Item>
+                <NavDropdown.Item>
+                  <NavLink
+                    href="/why-use-maple/for-legislators"
+                    handleClick={closeNav}
+                  >
+                    {t("navigation.forLegislators")}
+                  </NavLink>
+                </NavDropdown.Item>
+              </NavDropdown>
+            </Nav>
+          </NavBarBox>
+
+          <NavBarBox className={`align-self-center justify-content-end`}>
             <ProfileLink role={claims?.role} fullName={profile?.fullName} />
           </NavBarBox>
         </NavBarBoxContainer>
