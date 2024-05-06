49d343e4591cc974b3742213f5102c70dad2236a
Celena T <chibiynm@gmail.com>
2024-05-06 14:04:39 -0400

    Add playwright and e2e tests for homepage

---
+++ tests/e2e/homepage.spec.ts
@@ -0,0 +1,13 @@
+import { test, expect, type Page } from "@playwright/test"
+
+test.beforeEach(async ({ page }) => {
+  await page.goto("http://localhost:3000")
+})
+
+test.describe("Maple Homepage", () => {
+  test("should display logo and text", async ({ page }) => {
+    const logo = page.getByAltText("logo").first()
+    expect(logo).toBeVisible()
+    expect(page.getByText("Let your voice be heard!")).toBeVisible()
+  })
+})
