0304a4ebfdbd423b0dce7f2fe2cb598990dac8a2
Merritt Baggett <mertbagt@gmail.com>
2024-05-24 13:16:39 -0400

    adjust KeepNote

--- components/publish/KeepNote.tsx
+++ components/publish/KeepNote.tsx
@@ -1,5 +1,6 @@
 import styled from "styled-components"
 import { useState } from "react"
+import { Link } from "react-admin"
 import { Image, Button, Modal, Col, Row } from "../bootstrap"
 import { Step } from "./redux"
 
@@ -97,7 +98,7 @@ export const YourTestimony = () => {
         </NoteItem>
         <NoteItem>
           When you send this email, you are submitting formal public testimony!
-          As fellow constituents, we thank you for your sharing your voice.
+          As fellow constituents, we thank you for sharing your voice.
         </NoteItem>
       </ul>
     </NoteContent>
@@ -117,8 +118,9 @@ export const PublishingToMAPLE = () => {
           will remain available.
         </NoteItem>
         <NoteItem>
-          Since MAPLE is an archive you cannot remove your testimony from the
-          site.
+          Since MAPLE is an archive, you cannot remove your testimony from the
+          site. You may request a deletion in certain circumstances (see our{" "}
+          <Link to="/about/faq-page">FAQ page</Link>)
         </NoteItem>
         <NoteItem>Don't forget to send the email to your legislator.</NoteItem>
       </ul>
