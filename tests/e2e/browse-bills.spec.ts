9db1a046b804f046079b667938f86cd361d06110
huanfeng <yeh.hua@northeastern.edu>
2024-06-03 16:10:54 +0800

    Implement clicking sort options for sort bills test

--- tests/e2e/browse-bills.spec.ts
+++ tests/e2e/browse-bills.spec.ts
@@ -2,21 +2,17 @@ import { test, expect } from "@playwright/test"
 
 test.beforeEach(async ({ page }) => {
   await page.goto("http://localhost:3000/bills")
+  // Wait for the list of bills to be visible
+  await page.waitForSelector("ol.ais-Hits-list")
+
+  // Check that there are list items within the list
+  const bills = await page.$$("li.ais-Hits-item")
+  expect(bills.length).toBeGreaterThan(0)
 })
 
 test.describe("Browse Bills Page", () => {
-  test("should display a list of bills", async ({ page }) => {
-    // Wait for the list of bills to be visible
-    await page.waitForSelector("ol.ais-Hits-list")
-
-    // Check that there are list items within the list
-    const bills = await page.$$("li.ais-Hits-item")
-    expect(bills.length).toBeGreaterThan(0)
-  })
-
   test("should find bills via text search", async ({ page }) => {
-    // Wait for the search input to be available
-    await page.waitForSelector('input[placeholder="Search For Bills"]')
+    // Setup serach term
     const searchTerm = "act"
 
     // Perform the search
@@ -63,6 +59,60 @@ test.describe("Browse Bills Page", () => {
     //   }
     // } while (hasNextPage)
   })
+})
 
-  test.describe("Sorting Bills", () => {})
+test.describe("Browse Bills Page", () => {
+  const sortOptions = [
+    "Sort by Most Recent Testimony",
+    "Sort by Relevance",
+    "Sort by Testimony Count",
+    "Sort by Cosponsor Count",
+    "Sort by Next Hearing Date"
+  ]
+})
+test.describe("Browse Bills Page", () => {
+  const sortingTests = [
+    {
+      option: "Sort by Most Recent Testimony",
+      attribute: "data-testimony-date",
+      order: "desc",
+      type: "number"
+    },
+    {
+      option: "Sort by Relevance",
+      attribute: "data-relevance-score",
+      order: "desc",
+      type: "number"
+    }, // Assuming relevance score is a number attribute
+    {
+      option: "Sort by Testimony Count",
+      attribute: "data-testimony-count",
+      order: "desc",
+      type: "number"
+    },
+    {
+      option: "Sort by Cosponsor Count",
+      attribute: "data-cosponsor-count",
+      order: "desc",
+      type: "number"
+    },
+    {
+      option: "Sort by Next Hearing Date",
+      attribute: "data-hearing-date",
+      order: "asc",
+      type: "date"
+    }
+  ]
+
+  for (const { option, attribute, order, type } of sortingTests) {
+    test(`should sort bills by ${option}`, async ({ page }) => {
+      // Interact with the sorting dropdown
+      const sortDropdown = page.locator(".s__control")
+      await sortDropdown.click()
+
+      // Select the sorting option
+      const sortByOption = page.locator(`div.s__option:has-text("${option}")`)
+      await sortByOption.click()
+    })
+  }
 })
