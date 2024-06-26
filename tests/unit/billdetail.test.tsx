6ec432b63ace514f285cd034dddc3137adf9dbab
Simran Kaur <simran00kk@gmail.com>
2024-06-26 00:03:48 -0400

    Tests are running but failing. Installed redux-mock-store.

--- tests/unit/billdetail.test.tsx
+++ tests/unit/billdetail.test.tsx
@@ -1,29 +1,13 @@
 import "@testing-library/jest-dom"
-import { render, screen, fireEvent, waitFor } from "@testing-library/react"
-import { Bill, draftAttachment } from "components/db"
+import { render, screen } from "@testing-library/react"
+import { Bill } from "components/db"
 import { BillDetails } from "components/bill/BillDetails"
 import { Timestamp } from "firebase/firestore"
 import { Provider } from "react-redux"
-import { thunk } from "redux-thunk"
 import configureStore from "redux-mock-store"
 
-// mock window match media
-Object.defineProperty(window, "matchMedia", {
-  writable: true,
-  value: jest.fn().mockImplementation(query => ({
-    matches: false,
-    media: query,
-    onchange: null,
-    addListener: jest.fn(),
-    removeListener: jest.fn(),
-    addEventListener: jest.fn(),
-    removeEventListener: jest.fn(),
-    dispatchEvent: jest.fn()
-  }))
-})
-
-// mocking feature flags to match dev environment
-jest.mock("components/featureFlags", () => ({
+// mocking dependencies:
+jest.mock('components/featureFlags', () => ({
   useFlags: () => ({
     testimonyDiffing: false,
     notifications: true,
@@ -31,80 +15,67 @@ jest.mock("components/featureFlags", () => ({
     followOrg: true,
     lobbyingTable: false
   })
-}))
+}));
 
-// translation dependency -mocking so that we aren't actually translating anything in this test
 jest.mock("next-i18next", () => ({
   useTranslation: () => ({ t: (key: string) => key })
 }))
 
-// mock bill (later used in redux store)
 const mockBill: Bill = {
-  id: "A1234",
+  id: "S1653",
   court: 193,
   content: {
-    BillNumber: "A1234",
+    BillNumber: "S1653",
     PrimarySponsor: {
-      Name: "Primary Sponsor",
-      Id: "ABC1",
+      Name: "Ryan C. Fattman",
+      Id: "RCF0",
       Type: 1
     },
-    DocketNumber: "AB1234",
-    Title: "Content Title",
-    DocumentText: "Document Text",
+    DocketNumber: "SD1568",
+    Title:
+      "An Act authorizing the appointment of special police officers in the town of Charlton",
+    DocumentText:
+      "\tSECTION 1. The chief of police of the town of Charlton may appoint, if the chief deems necessary, individuals with a law enforcement background as special police officers for the purpose of performing police details or any other police duties arising from or during the course of police detail work, whether or not related to the detail work. Prior to appointment under this act, a special police officer shall pass a medical examination conducted by a physician or other certified professional chosen by the town to determine that the special police officer is capable of performing the essential duties of a special police officer, the cost of which shall be borne by the special police officer.\r\n\tSECTION 2. Notwithstanding section 1 of chapter 32 of the General Laws or any other general or special law to the contrary, a special police officer appointed pursuant to this act shall not be subject to the maximum age restrictions applied to regular officers under chapter 32 of the General Laws; provided, however, that a special police officer appointed pursuant to this act shall not be eligible to serve as a special police officer upon reaching the age of 70.\r\n\tSECTION 3. Special police officers appointed under this act shall be subject to the limitation on hours worked and other restrictions on earnings as provided in paragraph (b) of section 91 of chapter 32 of the General Laws.\r\n\tSECTION 4. Special police officers shall be subject to the rules and regulations, policies, procedures and requirements of the chief of police of the town of Charlton including, but not limited to, restrictions on the type of detail assignments, requirements regarding medical examinations to determine continuing capability to perform the duties of a special police officer, requirements for training, requirements for firearms licensing and qualifications and requirements regarding uniforms and equipment. Such special police officers shall comply with all requirements of chapter 6E of the General Laws, including: (i) maintaining certification and good standing with the Massachusetts Peace Officer Standards and Training Commission; and (ii) complying with all annual in-service and other training requirements mandated by the municipal police training committee.\r\n\tSECTION 5. This act shall take effect upon passage.\r\n",
     LegislationTypeName: "Bill",
-    Pinslip: "Pinslip",
+    Pinslip:
+      "By Mr. Fattman, a petition (accompanied by bill, Senate, No. 1653) of Ryan C. Fattman (by vote of the town) for legislation to authorize the appointment of special police officers in the town of Charlton.  Public Service.  [Local Approval Received.]",
     Cosponsors: [
       {
         Type: 1,
-        Id: "ABC1",
-        Name: "Primary Sponsor"
-      },
-      {
-        Type: 1,
-        Id: "AAAA",
-        Name: "Second Sponsor"
-      },
-      {
-        Type: 1,
-        Id: "BBBB",
-        Name: "Third Sponsor"
-      },
-      {
-        Type: 1,
-        Id: "CCCC",
-        Name: "Fourth Sponsor"
+        Id: "RCF0",
+        Name: "Ryan C. Fattman"
       }
     ],
     GeneralCourtNumber: 193
   },
-  cosponsorCount: 4,
+  cosponsorCount: 1,
   testimonyCount: 2,
   endorseCount: 2,
   opposeCount: 0,
   neutralCount: 0,
   nextHearingAt: new Timestamp(1680616800, 0),
   latestTestimonyAt: new Timestamp(1718147489, 987000000),
-  latestTestimonyId: "Xy7dkT90nmLVac56pq_ZW",
+  latestTestimonyId: "F2euSWhugXXPzb38jk_EJ",
   fetchedAt: new Timestamp(1718811579, 935000000),
   history: [
     {
-      Action: "History Action 1",
+      Action: "Referred to the committee on Public Service",
       Branch: "Senate",
       Date: "2023-02-16T11:17:15.563"
     },
     {
       Branch: "House",
       Date: "2023-02-16T11:17:15.563",
-      Action: "History Action 2"
+      Action: "House concurred"
     },
     {
       Date: "2023-03-29T16:13:01.983",
-      Action: "History Action 3",
+      Action:
+        "Hearing scheduled for 04/04/2023 from 10:00 AM-01:00 PM in A-1    ",
       Branch: "Joint"
     },
     {
-      Action: "Last History Action",
+      Action: "Accompanied a new draft, see S2416",
       Date: "2023-07-13T00:00:00",
       Branch: "Senate"
     }
@@ -112,187 +83,75 @@ const mockBill: Bill = {
   city: "Sample City"
 }
 
-// set up Redux mock store with thunk middleware bc resolveBill is thunk
-const mockStore = configureStore([thunk])
+// set up Redux mock store
+const mockStore = configureStore([])
 
 describe("BillDetails", () => {
-  let store: ReturnType<typeof mockStore>
-  const { id, content, history, cosponsorCount } = mockBill
-  const { Title, DocumentText, PrimarySponsor, Cosponsors } = content
+  let store
+
+  // before each Bill Details test, initialize a stor with an auth slice
   beforeEach(() => {
     store = mockStore({
       auth: {
         authenticated: false,
         user: null,
         claims: null
-      },
-      publish: {
-        service: {},
-        showThankYou: false,
-        bill: mockBill
       }
     })
+  })
 
+  it("renders bill title", () => {
     render(
       <Provider store={store}>
         <BillDetails bill={mockBill} />
       </Provider>
     )
+    const titleElement = screen.getByText(mockBill.id)
+    expect(titleElement).toBeInTheDocument()
   })
 
-  it("renders a link with the bill number as the link text and links to the appropriate bill", () => {
-    const title = screen.getByRole("link", {
-      name: id[0] + "." + id.slice(1)
-    })
-    expect(title).toHaveAttribute(
-      "href",
-      `https://malegislature.gov/Bills/193/${id}`
-    )
-  })
+  it("renders bill status", () => {})
 
-  it("renders the Bill Status button with the last Action when mockBill has history", () => {
-    const statusButton = screen.getByRole("button", {
-      name: history[history.length - 1].Action
-    })
-    expect(statusButton).toBeInTheDocument()
-  })
+  it("renders bill description", () => {})
 
-  it("renders the summary and full text", () => {
-    expect(screen.getByText(Title)).toBeInTheDocument
-    const readMoreButton = screen.getByRole("button", { name: "Read more.." })
-    expect(readMoreButton).toBeInTheDocument
-    fireEvent.click(readMoreButton)
-    expect(screen.getByText(DocumentText)).toBeInTheDocument
-  })
+  it("renders bill number", () => {})
 
-  // below test assumes mockBill contains a primary sponsor
-  it("renders Sponsors", () => {
-    const cosponsors = Cosponsors.filter(s => s.Id !== PrimarySponsor?.Id)
-
-    expect(screen.getByText("Sponsors")).toBeInTheDocument
-    expect(screen.getByRole("img", { name: "Lead Sponsor image" }))
-      .toBeInTheDocument
-    const leadSponsorLink = screen.getByRole("link", {
-      name: PrimarySponsor?.Name
-    })
-    expect(leadSponsorLink).toBeInTheDocument
-
-    // if the mockBill has cosponsors (excluding primary), then atleast one cosponsor image and link should show
-    if (cosponsors.length > 0) {
-      const cosponsorImgs = screen.getAllByRole("img", {
-        name: "Sponsor image"
-      })
-      const cosponsorLinks = screen.getAllByRole("link", {
-        name: cosponsors[0].Name
-      })
-      expect(cosponsorImgs[0]).toBeInTheDocument
-      expect(cosponsorLinks[0]).toBeInTheDocument
-    }
-
-    // expect the 'See X Sponsors' button if there are more than two cosponsors (excluding primary)
-    if (cosponsors.length > 2) {
-      expect(
-        screen.getByRole("button", {
-          name: `See ${cosponsorCount} Sponsors`
-        })
-      ).toBeInTheDocument
-    }
-  })
-
-  it("renders testimony section headings and subheadings", () => {
-    const headings = [
-      "Testimonies",
-      "All Testimonies",
-      "Individuals",
-      "Organizations"
-    ]
-    headings.forEach(heading => {
-      expect(screen.getByText(heading)).toBeInTheDocument()
-    })
-  })
-})
-
-describe("Bill Details Testimony States", () => {
-  const firebaseUser = {
-    uid: "user123",
-    email: "user@example.com",
-    emailVerified: true
-  }
-
-  const unverifiedUser = {
-    uid: "user123"
-  }
-
-  // Mock Testimony object
-  const mockTestimonyPublication = {
-    id: "testimony123",
-    billId: "bill123",
-    court: 1,
-    position: "endorse",
-    content:
-      "This is a sample testimony content that is less than 10,000 characters.",
-    attachmentId: null,
-    editReason: null,
-    authorUid: "user123",
-    authorDisplayName: "John Doe",
-    authorRole: "user",
-    billTitle: "Sample Bill Title",
-    version: 1,
-    publishedAt: Timestamp.fromMillis(Date.now()),
-    representativeId: null,
-    senatorId: null,
-    senatorDistrict: null,
-    representativeDistrict: null,
-    draftAttachmentId: null,
-    fullName: "John Doe"
-  }
-
-  const mockTestimonyDraft = {
-    position: "endorse",
-    content:
-      "This is a sample testimony content that is less than 10,000 characters.",
-    draftAttachmentId: null
-  }
-
-  let store: ReturnType<typeof mockStore>
-
-  it("renders appropriate testimony panel state when user is not logged in", () => {
-    store = mockStore({
-      auth: {
-        authenticated: false,
-        user: null,
-        claims: null
-      },
-      publish: {
-        service: {},
-        showThankYou: false,
-        bill: mockBill,
-        draft: {},
-        sync: "synced"
-      }
-    })
+  it("renders sponsors", () => {
     render(
       <Provider store={store}>
         <BillDetails bill={mockBill} />
       </Provider>
     )
-    expect(screen.getByText("Sign In to Add Testimony")).toBeInTheDocument
-    expect(screen.getByRole("button", { name: "logInSignUp" }))
-      .toBeInTheDocument
+    mockBill.sponsors.forEach(sponsor => {
+      expect(screen.getByText(sponsor)).toBeInTheDocument()
+    })
   })
 
-  it("renders appropriate testimony panel state when user has unverified email", () => {
+  it("renders testimonies section", () => {
+    render(
+      <Provider store={store}>
+        <BillDetails bill={mockBill} />
+      </Provider>
+    )
+    expect(screen.getByTestId("bill-testimonies")).toBeInTheDocument()
+  })
+
+  // Test different user states
+  it("renders not logged in state", () => {
+    render(
+      <Provider store={store}>
+        <BillDetails bill={mockBill} />
+      </Provider>
+    )
+    expect(screen.getByText("Not Logged In")).toBeInTheDocument()
+  })
+
+  it("renders unsubmitted state for authenticated user", () => {
     store = mockStore({
       auth: {
         authenticated: true,
-        user: unverifiedUser,
-        claims: null
-      },
-      publish: {
-        service: {},
-        showThankYou: false,
-        bill: mockBill,
-        sync: "synced"
+        user: { uid: "test-uid" },
+        claims: { role: "user" }
       }
     })
     render(
@@ -300,104 +159,8 @@ describe("Bill Details Testimony States", () => {
         <BillDetails bill={mockBill} />
       </Provider>
     )
-
-    expect(
-      screen.getByRole("button", {
-        name: "Verify Your Email"
-      })
-    ).toBeInTheDocument
+    expect(screen.getByText("Unsubmitted")).toBeInTheDocument()
   })
 
-  it("renders appropriate testimony panel state when user is logged in and does NOT have a testimony draft", () => {
-    store = mockStore({
-      auth: {
-        authenticated: true,
-        user: firebaseUser,
-        claims: null
-      },
-      publish: {
-        service: {},
-        showThankYou: false,
-        bill: mockBill,
-        sync: "synced"
-      }
-    })
-    render(
-      <Provider store={store}>
-        <BillDetails bill={mockBill} />
-      </Provider>
-    )
-
-    expect(screen.getByText("You Haven't Submitted Testimony"))
-      .toBeInTheDocument
-    expect(screen.getByRole("button", { name: "Create Testimony" }))
-      .toBeInTheDocument
-  })
-
-  it("renders appropriate testimony panel state when user is logged in and has a testimony draft", () => {
-    store = mockStore({
-      auth: {
-        authenticated: true,
-        user: firebaseUser,
-        claims: null
-      },
-      publish: {
-        service: {},
-        showThankYou: false,
-        bill: mockBill,
-        draft: mockTestimonyDraft,
-        sync: "synced"
-      }
-    })
-    render(
-      <Provider store={store}>
-        <BillDetails bill={mockBill} />
-      </Provider>
-    )
-
-    expect(screen.getByText("You Have Draft Testimony")).toBeInTheDocument
-    expect(screen.getByRole("button", { name: "Complete Testimony" }))
-      .toBeInTheDocument
-  })
-
-  // made below test async bc of async updating that is likely related to the testimony preview subcomponent
-  it("renders appropriate testimony panel state when user has a submitted testimony", async () => {
-    store = mockStore({
-      auth: {
-        authenticated: true,
-        user: firebaseUser,
-        claims: null
-      },
-      publish: {
-        service: {},
-        showThankYou: false,
-        bill: mockBill,
-        draft: mockTestimonyDraft,
-        sync: "synced",
-        publication: mockTestimonyPublication
-      }
-    })
-    render(
-      <Provider store={store}>
-        <BillDetails bill={mockBill} />
-      </Provider>
-    )
-
-    await waitFor(() => {
-      expect(screen.getByText("Your Testimony")).toBeInTheDocument()
-    })
-    const editTestimonyButton = screen.getByRole("button", {
-      name: "edit testimony"
-    })
-    const emailButton = screen.getByRole("button", {
-      name: "Email Your Published Testimony"
-    })
-    const twitterShareLinkButton = screen.getByRole("button", {
-      name: "link.twitter"
-    })
-
-    expect(editTestimonyButton).toBeInTheDocument()
-    expect(twitterShareLinkButton).toBeInTheDocument()
-    expect(emailButton).toBeInTheDocument()
-  })
+  // Add more tests for Draft and Submitted states
 })
