7529a3b229499f0158e61161930c743c98105771
huanfeng <yeh.hua@northeastern.edu>
2024-09-10 09:00:15 -0400

    Add sorting, user reports and upgrade requests page test

--- tests/e2e/adminPage.spec.ts
+++ tests/e2e/adminPage.spec.ts
@@ -21,39 +21,45 @@ test.describe("Admin Page", () => {
     await page.fill('input[name="password"]', "password")
     await page.click('button[type="submit"]')
     await expect(page.getByAltText("profile icon")).toBeVisible()
-    // await authenticate(page)
-    await page.goto("http://localhost:3000/admin")
   })
   test.afterAll(async () => {
     await browser.close() // Close the browser instance after all tests
   })
 
-  test("should display menu icon", async () => {
-    const menuIcon = page.getByTestId("MenuIcon")
-    await expect(menuIcon).toBeVisible()
+  test.beforeEach(async ({ page }) => {
+    await page.goto("http://localhost:3000/admin")
   })
 
-  test("should display refresh icon", async () => {
-    const refreshIcon = page.getByTestId("RefreshIcon")
-    await expect(refreshIcon).toBeVisible()
+  test("should display the User Reports page", async () => {
+    const adminPage = new AdminPage(page)
+    await expect(adminPage.menuIcon).toBeVisible()
+    await expect( adminPage.refreshIcon).toBeVisible()
+    await expect(adminPage.userReports).toBeVisible()
+    await expect(adminPage.upgradeRequests).toBeVisible()
+    await expect(adminPage.getAppIcon).toBeVisible()
   })
 
-  test("should display viewList icon", async () => {
-    const userReports = page.getByRole('menuitem', { name: 'User Reports' })
-    const upgradeRequests = page.getByRole('menuitem', { name: 'Upgrade Requests' })
-    await expect(userReports).toBeVisible()
-    await expect(upgradeRequests).toBeVisible()
-  })
+  test("should display the Upgrade Requests page", async () => {
+    const adminPage = new AdminPage(page)
+    await adminPage.upgradeRequests.click()
 
-  test("should display getApp icon", async () => {
-    const getAppIcon = page.getByTestId("GetAppIcon")
-    await expect(getAppIcon).toBeVisible()
+    await expect(adminPage.menuIcon).toBeVisible()
+    await expect(adminPage.refreshIcon).toBeVisible()
+    await expect(adminPage.userReports).toBeVisible()
+    await expect(adminPage.upgradeRequests).toBeVisible()
+    await expect(adminPage.arrowDropDownIcon).toBeVisible()
+    await expect(adminPage.navigateBeforeIcon).toBeVisible()
+    await expect(adminPage.navigateNextIcon).toBeVisible()
   })
 
 
   test("should display the table in acending order", async () => {
-    const reportIdHeader = await page.getByText("report id")
+    const adminPage = new AdminPage(page)
+    adminPage.gotoUserReportPage()
+
+    const reportIdHeader = adminPage.reportIdHeader
     await reportIdHeader.click()
+
     const currentUrl = await page.url()
     if(!currentUrl.includes("order=ASC")) {
       await reportIdHeader.click()
@@ -67,14 +73,17 @@ test.describe("Admin Page", () => {
   })
 
   test("should display the table in descending order", async () => {
-    const reportIdHeader = await page.getByText("Testimony", { exact: true })
-    await reportIdHeader.click()
+    const adminPage = new AdminPage(page)
+    adminPage.gotoUserReportPage()
+
+    const testimonyHeader = adminPage.testimonyHeader
+    await testimonyHeader.click()
     const currentUrl = await page.url()
     if(!currentUrl.includes("order=DESC")) {
-      await reportIdHeader.click()
+      await testimonyHeader.click()
     }
     const newUrl = await page.url()
-    const classList = await reportIdHeader.locator("..").getAttribute('class')
+    const classList = await testimonyHeader.locator("..").getAttribute('class')
 
     expect(newUrl).toContain("order=DESC") 
     expect(newUrl).toContain("sort=testimonyId") 
@@ -82,22 +91,36 @@ test.describe("Admin Page", () => {
   })
 
   test("should allow adding a report", async () => {
-    // Click the "SEED WITH A FAKE REPORT" button
-    await page.getByRole("button", { name: "SEED WITH A FAKE REPORT" }).click()
+    // Create a report
+    const adminPage = new AdminPage(page)
+    adminPage.gotoUserReportPage()
+
+    await adminPage.fakeReportbtn.click()
     await expect(page.getByText("reportTestimony")).toBeVisible()
     await page.getByRole("radio", {name: "Violent"}).click()
-    await await page.getByRole('button', { name: '' }).click()
+    await page.click('button[type="submit"]')
   })
 
-  test("should allow resolve a report", async () => {
-    const resolvedCases = await page.getByText("resolved").count()
-    await page.getByLabel("RESOLVE REPORT").first().click()
-    await expect(page.getByText("User Report Content")).toBeVisible()
-    await page.getByRole("radio", {name: "Remove"}).click()
-    await page.locator('form').getByText('Reason:').fill('This is the reason text.')
-    await page.click('button[type="submit"]')
-    const currentResolvedCases = await page.getByText("resolved").count()
-    expect(currentResolvedCases == resolvedCases + 1).toBeTruthy()
+  test("should reslove a report", async () => {
+    // Resolve the report
+    const adminPage = new AdminPage(page)
+    adminPage.gotoUserReportPage()
+
+    const pendingCases = await page.getByText("pending").count()
+    const filledReason = "This is the reason text."
+
+    if(pendingCases > 0 ){
+      const resolvedCases = await page.getByText("resloves").count()
+      await page.getByLabel("RESOLVE REPORT").first().click()
+      await expect(page.getByText("User Report Content")).toBeVisible()
+      await page.getByRole("radio", {name: "Remove"}).click()
+      await page.locator('form').getByText('Reason:').fill(filledReason)
+      await page.click('input[type="submit"]')
+      await expect(adminPage.fakeReportbtn).toBeVisible()
+      const currentResolvedCases = await page.getByText("resolved").count()
+      expect(currentResolvedCases).toBeGreaterThan(resolvedCases)
+    }
+    
   })
 
   test("should display a table with differet categories", async () => {
@@ -111,7 +134,21 @@ test.describe("Admin Page", () => {
     await expect(page.getByText("Resolve Report", { exact: true })).toBeVisible()
   })
 
+  test("should filter the profile with pending upgrade", async () => {
+    const adminPage = new AdminPage(page)
+    adminPage.gotoUpgradeRequests()
 
+    await page.getByRole("button", {name:"pendingUpgrades"}).click()
+    await expect(page).toHaveURL(/.*pendingUpgrade/)
+  })
 
+  test("should filter the profile with organization", async () => {
+    const adminPage = new AdminPage(page)
+    adminPage.gotoUpgradeRequests()
+
+    await page.getByRole("button", {name:"organization"}).click()
+    await expect(page).toHaveURL(/.*organization/)
+  })
+  
 
 })
