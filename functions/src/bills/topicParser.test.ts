fb502ad173341e3d772da025fce654621c852670
Mephistic <deathbyfiresermon@gmail.com>
2024-11-17 13:20:36 -0500

    Switching bill topic format per dev feedback, extracting topic parser for later use, adding unit tests for topic parser

---
+++ functions/src/bills/topicParser.test.ts
@@ -0,0 +1,26 @@
+import { assignCategoriesToTopics } from "./topicParser"
+
+describe("assignCategoriesToTopics", () => {
+  it("assigns categories to topics", () => {
+    const topics = [
+      "Consumer protection",
+      "Correctional facilities",
+      "Property crimes"
+    ]
+    const categories = assignCategoriesToTopics(topics)
+    expect(categories).toEqual([
+      { category: "Commerce", topic: "Consumer protection" },
+      {
+        category: "Crime and Law Enforcement",
+        topic: "Correctional facilities"
+      },
+      { category: "Crime and Law Enforcement", topic: "Property crimes" }
+    ])
+  })
+
+  it("ignores topics with missing categories", () => {
+    const topics = ["Consumer protection", "Unknown topic"]
+    const categories = assignCategoriesToTopics(topics)
+    expect(categories).toEqual([{ category: "Commerce", topic: "Consumer protection" }])
+  })
+})
