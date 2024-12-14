844d4be6e820064d1321028c4fe28503d57ddfc8
kiminkim724 <kim.ki@husky.neu.edu>
2024-12-14 14:22:58 -0500

    Revert ignoreUndefinedProperties

--- functions/src/firebase.ts
+++ functions/src/firebase.ts
@@ -2,7 +2,6 @@ import * as admin from "firebase-admin"
 
 admin.initializeApp()
 export const db = admin.firestore()
-db.settings({ ignoreUndefinedProperties: true })
 export const storage = admin.storage()
 export const auth = admin.auth()
 export { admin }
