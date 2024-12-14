cc0b69b344b3f831f8b20e9cd7c3f040fcdf37c1
kiminkim724 <kim.ki@husky.neu.edu>
2024-12-14 13:43:28 -0500

    Make the notification return type more explicit

--- components/Newsfeed/NotificationProps.tsx
+++ components/Newsfeed/NotificationProps.tsx
@@ -1,22 +1,39 @@
 import { Timestamp } from "firebase/firestore"
 
-export type NotificationProps = {
+export type NotificationProps = BillHistoryNotification | TestimonyNotification
+
+type BillHistoryNotification = {
+  type: "bill"
   bodyText: string
   court: string
   delivered: boolean
   header: string // Bill Title
   id: number
   subheader: string
-  type: string
   topicName: string
   timestamp: Timestamp
   createdAt: Timestamp
-  position?: string
   isBillMatch: boolean // Is subscribed to Bill
   isUserMatch: boolean // is subscribed to User/Org
-  authorUid?: string // Testimony author
-  testimonyId?: string
-  userRole?: string
+}
+
+interface TestimonyNotification {
+  type: "testimony"
+  bodyText: string
+  court: string
+  delivered: boolean
+  header: string // Bill Title
+  id: number
+  subheader: string
+  topicName: string
+  timestamp: Timestamp
+  createdAt: Timestamp
+  position: string
+  isBillMatch: boolean // Is subscribed to Bill
+  isUserMatch: boolean // is subscribed to User/Org
+  authorUid: string // Testimony author
+  testimonyId: string
+  userRole: string
 }
 
 export type Notifications = NotificationProps[]
--- functions/src/firebase.ts
+++ functions/src/firebase.ts
@@ -2,6 +2,7 @@ import * as admin from "firebase-admin"
 
 admin.initializeApp()
 export const db = admin.firestore()
+db.settings({ ignoreUndefinedProperties: true })
 export const storage = admin.storage()
 export const auth = admin.auth()
 export { admin }
