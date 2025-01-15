07850db8bd8b166f851694c6b86667612db0fa6a
ananyasinghz <143516466+ananyasinghz@users.noreply.github.com>
2025-01-15 13:53:56 +0530

    Forgot to commit changes before push

--- components/search/testimony/TestimonySearch.tsx
+++ components/search/testimony/TestimonySearch.tsx
@@ -101,23 +101,23 @@ const Layout = () => {
 
   const onTabClick = (t: Tab) => {
     setKey(t)
-    setIndexUiState((prevState) => {
-      const validRoles = ["user", "organization", "admin"] 
+    setIndexUiState(prevState => {
+      const validRoles = ["user", "organization", "admin"]
       const role =
         t === "Individuals"
           ? ["user"]
           : t === "Organizations"
           ? ["organization"]
-          : validRoles 
+          : validRoles
       return {
         ...prevState,
         refinementList: {
           ...prevState.refinementList,
-          authorRole: role,
-        },
+          authorRole: role
+        }
       }
     })
-  }  
+  }
 
   const [followStatus, setFollowStatus] = useState<OrgFollowStatus>({})
 
