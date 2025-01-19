b92f1b3a194dd9a53a32cde87feffe844f3c587c
aerhartic <aerhartic@gmail.com>
2025-01-19 12:15:30 -0500

    feat: fixed hyperlink and summary

--- components/bill/SmartDisclaimer.tsx
+++ components/bill/SmartDisclaimer.tsx
@@ -1,9 +1,10 @@
-import { useTranslation } from "next-i18next"
+import { Trans, useTranslation } from "next-i18next"
 import Image from "react-bootstrap/Image"
 import styled from "styled-components"
 import { useMediaQuery } from "usehooks-ts"
 import { Col, Row } from "../bootstrap"
 import { NavLink } from "components/Navlink"
+import * as links from "../links"
 
 const SmartTagDesc = styled(Row)`
   font-size: 12px;
@@ -17,8 +18,46 @@ export const SmartDisclaimer = () => {
     <>
       {isMobile ? (
         <div className={`m-3`}>
-          <Row className={`fs-5 fw-bold`}>{t("bill.smart_summary")}</Row>
+          <Row className={`fs-5 fw-bold`} fontSize={"12px"}>
+            {t("bill.smart_summary")}
+          </Row>
           <SmartTagDesc>
+            <Trans
+              t={t}
+              i18nKey="bill.smart_disclaimer"
+              components={[
+                // eslint-disable-next-line react/jsx-key
+                <links.Internal href="/about/how-maple-uses-ai" />
+              ]}
+            />
+          </SmartTagDesc>
+        </div>
+      ) : (
+        <Row className={`d-flex my-3`} fontSize={"12px"} xs="auto">
+          <Col>
+            <Image src="/images/smart-summary.svg" alt={t("bill.smart_tag")} />
+          </Col>
+          <Col className={`mt-1`} xs="10">
+            <Row className={`fs-5 fw-bold`}>{t("bill.smart_summary")}</Row>
+            <SmartTagDesc>
+              <Trans
+                t={t}
+                i18nKey="bill.smart_disclaimer"
+                components={[
+                  // eslint-disable-next-line react/jsx-key
+                  <links.Internal href="/about/how-maple-uses-ai" />
+                ]}
+              />
+            </SmartTagDesc>
+          </Col>
+        </Row>
+      )}
+    </>
+  )
+}
+
+/* 
+
             {
               "This content has been generated using artificial intelligence and may not accurately reflect the details of the legislation."
             }
@@ -29,30 +68,6 @@ export const SmartDisclaimer = () => {
             {
               "To report an inaccuracy or to suggest an improvement, please email admin@mapletestimony.org"
             }
-          </SmartTagDesc>
-        </div>
-      ) : (
-        <Row className={`d-flex my-3`} xs="auto">
-          <Col>
-            <Image src="/images/smart-summary.svg" alt={t("bill.smart_tag")} />
-          </Col>
-          <Col className={`mt-1`} xs="10">
-            <Row className={`fs-5 fw-bold`}>{t("bill.smart_summary")}</Row>
-            <SmartTagDesc>
-              {
-                "This content has been generated using artificial intelligence and may not accurately reflect the details of the legislation."
-              }
-              <NavLink
-                className={"navLink-primary"}
-                href="/about/how-maple-uses-ai"
-              >{`Learn more about How MAPLE Uses AI. `}</NavLink>
-              {
-                "To report an inaccuracy or to suggest an improvement, please email admin@mapletestimony.org"
-              }
-            </SmartTagDesc>
-          </Col>
-        </Row>
-      )}
-    </>
-  )
-}
+
+
+*/
