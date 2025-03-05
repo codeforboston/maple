927539327d69de39d1b80a203b5e7fdd13c1887f
Barry Moore <chiroptical@gmail.com>
2025-03-04 21:32:40 -0500

    Add both the /summary and /tags functions
    - Add basic incomplete post body testing.
      Running them for real would require OpenAI calls

--- llm/api.py
+++ llm/api.py
@@ -1,23 +1,42 @@
-from flask import Flask, jsonify, abort
-from llm_functions import get_summaries_and_tags_api_function
+from flask import Flask, jsonify, abort, request
+from llm_functions import get_summary_api_function, get_tags_api_function
+import json
 
 app = Flask(__name__)
 
-@app.route('/summary', methods=['POST'])
+
+def is_intersection(keys, required_keys):
+    return (keys & required_keys) == required_keys
+
+
+@app.route("/summary", methods=["POST"])
 def summary():
     body = json.loads(request.data)
     # We require bill_id, bill_title, bill_text to exist as keys in the POST
-    # Note: & is essentially set intersection
-    if not (body.keys() & {"bill_id", "bill_title", "bill_text"}):
+    if not is_intersection(body.keys(), {"bill_id", "bill_title", "bill_text"}):
         abort(404, description="requires bill_id, bill_title, and bill_text")
 
-    summary = get_summaries_and_tags_api_function(
-        body["bill_id"],
-        body["bill_title"],
-        body["bill_text"]
+    summary = get_summary_api_function(
+        body["bill_id"], body["bill_title"], body["bill_text"]
     )
 
     if summary["status"] in [-1, -2]:
-        abort(404, description="unable to generate summary or tags")
-    
-    return jsonify(summary)
+        abort(500, description="Unable to generate summary")
+
+    return jsonify(summary["summary"])
+
+
+@app.route("/tags", methods=["POST"])
+def tags():
+    body = json.loads(request.data)
+    # We require bill_id, bill_title, bill_text to exist as keys in the POST
+    # Note: & is essentially set intersection
+    if not is_intersection(body.keys(), {"bill_id", "bill_title", "bill_text"}):
+        abort(404, description="requires bill_id, bill_title, and bill_text")
+
+    tags = get_tags_api_function(body["bill_id"], body["bill_title"], body["bill_text"])
+
+    if tags["status"] in [-1, -2]:
+        abort(500, description="Unable to generate tags")
+
+    return jsonify(tags["tags"])
