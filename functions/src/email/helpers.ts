eda87c17689417ab9a41c78482ccef01ce3f1e2d
Merritt Baggett <mertbagt@gmail.com>
2025-04-25 15:07:25 -0400

    refactored date formatting

--- functions/src/email/helpers.ts
+++ functions/src/email/helpers.ts
@@ -24,11 +24,8 @@ export function isDefined(v: unknown) {
 
 export function formatDate(timestamp: Date) {
   const date = new Date(timestamp)
-  const month = String(date.getMonth() + 1).padStart(2, "0")
-  const day = String(date.getDate()).padStart(2, "0")
-  const year = date.getFullYear()
 
-  return `${month}/${day}/${year}`
+  return date.toLocaleDateString()
 }
 
 export function minusFour(value: number) {
