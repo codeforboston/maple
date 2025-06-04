099373dc08517a714f889c193b7de421fddd8756
Barry Moore <chiroptical@gmail.com>
2025-06-03 22:28:29 -0400

    Add Path.mkdir to make the databases directory;
    Add a GET /ready endpoint for testing
    
    Add some command examples to readme.md

--- llm/main.py
+++ llm/main.py
@@ -2,7 +2,7 @@ from flask import Flask, jsonify, abort, request
 from llm_functions import get_summary_api_function, get_tags_api_function
 import json
 from firebase_admin import initialize_app
-from firebase_functions import https_fn
+from firebase_functions import https_fn, options
 
 initialize_app()
 app = Flask(__name__)
@@ -28,7 +28,6 @@ def summary():
 
     return jsonify(summary["summary"])
 
-
 @app.route("/tags", methods=["POST"])
 def tags():
     body = json.loads(request.data)
@@ -44,8 +43,11 @@ def tags():
 
     return jsonify(tags["tags"])
 
+@app.route("/ready", methods=["GET"])
+def ready():
+    return ""
 
-@https_fn.on_request(secrets=["OPENAI_DEV"])
+@https_fn.on_request(secrets=["OPENAI_DEV"], timeout_sec=300, memory=options.MemoryOption.GB_1)
 def httpsflaskexample(req: https_fn.Request) -> https_fn.Response:
     with app.request_context(req.environ):
         return app.full_dispatch_request()
