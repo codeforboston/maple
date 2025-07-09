88baabb34811ac0ee59376298fdd3ad7d23c9aaf
kiminkim724 <kim.ki@husky.neu.edu>
2025-07-08 20:51:52 -0400

    Fix Lint

--- functions/jest.config.js
+++ functions/jest.config.js
@@ -4,8 +4,10 @@ module.exports = {
   testEnvironment: "node",
   moduleNameMapper: {
     // This maps firebase-admin/auth to the correct location within node_modules
-    "^firebase-admin/auth$": "<rootDir>/node_modules/firebase-admin/lib/auth/index.js",
-    "^firebase-admin/app$": "<rootDir>/node_modules/firebase-admin/lib/app/index.js",
-    "^firebase-admin/(.*)$": "<rootDir>/node_modules/firebase-admin/lib/$1",
-  },
-};
+    "^firebase-admin/auth$":
+      "<rootDir>/node_modules/firebase-admin/lib/auth/index.js",
+    "^firebase-admin/app$":
+      "<rootDir>/node_modules/firebase-admin/lib/app/index.js",
+    "^firebase-admin/(.*)$": "<rootDir>/node_modules/firebase-admin/lib/$1"
+  }
+}
