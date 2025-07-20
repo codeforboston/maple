0373117bb68a02b50726a6f2b6584fe47c3ab4ad
Merritt Baggett <mertbagt@gmail.com>
2025-07-20 15:40:36 -0400

    basic links

--- components/Login/login.tsx
+++ components/Login/login.tsx
@@ -1,3 +1,4 @@
+import { Alert, Button, Col, Form, Modal, Stack } from "../bootstrap"
 import { useAppDispatch } from "../hooks"
 import {
   SignInWithButton,
@@ -7,22 +8,45 @@ import {
 import { AuthFlowStep, authStepChanged } from "components/auth/redux"
 import { useTranslation } from "next-i18next"
 import { useContext, useEffect, useState } from "react"
+import { useNavigate } from "react-router-dom"
+import { useLocation } from "react-router-dom"
+import { useRouter } from "next/router"
 
 export default function LoginPage() {
   const { t } = useTranslation("auth")
-  const dispatch = useAppDispatch()
-  const setCurrentModal = (step: AuthFlowStep) =>
-    dispatch(authStepChanged(step))
+  // const dispatch = useAppDispatch()
+  // const setCurrentModal = (step: AuthFlowStep) =>
+  //   dispatch(authStepChanged(step))
 
-  useEffect(() => {
-    setCurrentModal("start")
-  })
+  // useEffect(() => {
+  //   setCurrentModal("start")
+  // })
+
+  // const navigate = useNavigate()
+  // const handleGoBack = () => {
+  //   navigate(-1) // Navigates back one entry in the history stack
+  // }
+
+  const router = useRouter()
+
+  // const location = useLocation()
 
   return (
     <div>
       <p>Hello Login World</p>
       <br />
-      <p>handle if arriving on page but already logged in</p>
+      <p>
+        You were possibly signed out while trying to go to a page that needs you
+        to be signed in
+      </p>
+      <br />
+      <div>Please consider logging in first:</div>
+      <div className="" style={{ width: "100px" }}>
+        <SignInWithButton />
+      </div>
+      <br />
+      <p>If logged in, try and provide user with link to prior navpoint</p>
+      <p>Current Pathname: {router.pathname}</p>
     </div>
   )
 }
