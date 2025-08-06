036dad2cb54ca6094a019a2a543264bd6943b991
jicruz96 <accounts@jicruz.com>
2025-08-06 02:34:45 -0400

    fix/refactor useRefinements

--- components/search/testimony/useTestimonyRefinements.tsx
+++ components/search/testimony/useTestimonyRefinements.tsx
@@ -1,32 +1,31 @@
 import { RefinementListItem } from "instantsearch.js/es/connectors/refinement-list/connectRefinementList"
 import { useRefinements } from "../useRefinements"
-import { useCallback, useMemo } from "react"
+import { useMemo } from "react"
 import { useTranslation } from "next-i18next"
 
 export const useTestimonyRefinements = () => {
   const { t } = useTranslation("search")
-  const refinementProps = useMemo(
-    () =>
-      [
-        {
-          transformItems: useCallback(
-            (i: RefinementListItem[]) => i.filter(i => i.label !== "private"),
-            []
-          ),
-          attribute: "authorDisplayName"
-        },
-        { attribute: "court" },
-        { attribute: "position" },
-        { attribute: "billId" },
-        { attribute: "authorRole", searchable: false, hidden: true }
-      ].map(props => ({
-        limit: 500,
-        searchable: props.searchable ?? true,
-        searchablePlaceholder: t(`refinements.testimony.${props.attribute}`),
-        ...props
-      })),
-    [t]
-  )
 
-  return useRefinements({ refinementProps })
+  return useRefinements({
+    refinementProps: useMemo(
+      () =>
+        [
+          {
+            transformItems: (i: RefinementListItem[]) =>
+              i.filter(i => i.label !== "private"),
+            attribute: "authorDisplayName"
+          },
+          { attribute: "court" },
+          { attribute: "position" },
+          { attribute: "billId" },
+          { attribute: "authorRole", searchable: false, hidden: true }
+        ].map(props => ({
+          limit: 500,
+          searchable: true,
+          searchablePlaceholder: t(`refinements.testimony.${props.attribute}`),
+          ...props
+        })),
+      [t]
+    )
+  })
 }
