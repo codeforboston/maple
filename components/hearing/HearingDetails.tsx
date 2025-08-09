1eedc9fe0916bcc47cd97a750a2875a5ccfd46b5
Merritt Baggett <mertbagt@gmail.com>
2025-08-09 14:43:39 -0400

    retrieve data

--- components/hearing/HearingDetails.tsx
+++ components/hearing/HearingDetails.tsx
@@ -1,5 +1,31 @@
+import {
+  collection,
+  doc,
+  getDoc,
+  getDocs,
+  orderBy,
+  query
+} from "firebase/firestore"
 import { useTranslation } from "next-i18next"
+import { firestore } from "components/firebase"
+
+async function HearingData() {
+  const hearingId = "hearing-5180"
+  const hearing = await getDoc(doc(firestore, `events/${hearingId}`))
+
+  // ignore errors related to: Property X does not exist on type 'DocumentSnapshot<DocumentData>'.
+
+  console.log("hearing", hearing._document.data.value.mapValue.fields)
+
+  const { videoTranscriptionId, videoURL } = hearing.data()
+
+  console.log("url", videoURL)
+
+  return hearing
+}
 
 export const HearingDetails = () => {
-  return <>Hello Hearings World</>
+  HearingData()
+
+  return <>Hello Hearing World</>
 }
