ae13ee567f42e509cb8ee296e2caaeee63b8e101
Merritt Baggett <mertbagt@gmail.com>
2025-08-10 18:52:16 -0400

    refactor async function

--- components/hearing/HearingDetails.tsx
+++ components/hearing/HearingDetails.tsx
@@ -7,39 +7,41 @@ import {
   query
 } from "firebase/firestore"
 import { useTranslation } from "next-i18next"
+import { useCallback, useEffect, useRef, useState } from "react"
 import styled from "styled-components"
 import { Col, Container, Image, Row } from "../bootstrap"
 import { firestore } from "components/firebase"
+import { DatagridCell } from "react-admin"
 
 const StyledContainer = styled(Container)`
   font-family: "Nunito";
 `
 
-async function HearingData() {
-  const hearingId = "hearing-5180"
-  const hearing = await getDoc(doc(firestore, `events/${hearingId}`))
-
-  // ignore errors related to: Property X does not exist on type 'DocumentSnapshot<DocumentData>'.
-
-  console.log("data:", hearing.data())
-
-  const docData = hearing.data()
-  const content = docData?.content ?? "Default Content"
-  const videoTranscriptionId =
-    docData?.content ?? "Default Video Transcripton Id"
-  const videoURL = docData?.content ?? "Default URL"
-
-  return { content, videoTranscriptionId, videoURL }
-}
-
 export const HearingDetails = () => {
   const { t } = useTranslation("common")
-  HearingData()
+  const hearingId = "hearing-5180"
+  const [videoURL, setVideoURL] = useState("")
+
+  const hearingData = useCallback(async () => {
+    const hearing = await getDoc(doc(firestore, `events/${hearingId}`))
+    const docData = hearing.data()
+    const content = docData?.content ?? "Default Content"
+    const videoTranscriptionId =
+      docData?.videoTranscriptionId ?? "Default Video Transcripton Id"
+    setVideoURL(docData?.videoURL ?? "Default URL")
+
+    console.log("data: ", docData)
+    console.log("content Name: ", content.Name)
+  }, [])
+
+  hearingData()
+
+  console.log("url: ", videoURL)
 
   return (
     <StyledContainer className="mt-3 mb-3">
       <h1>{t("hearing_transcription")}</h1>
-      <>Hello Hearing World</>
+      <>Hello Hearing World, {videoURL}</>
     </StyledContainer>
   )
 }
