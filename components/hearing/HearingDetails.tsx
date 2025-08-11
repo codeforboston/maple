9256787313118159b389ca8bedd6592dc4e6cbc3
Merritt Baggett <mertbagt@gmail.com>
2025-08-11 12:22:43 -0400

    committee tag

--- components/hearing/HearingDetails.tsx
+++ components/hearing/HearingDetails.tsx
@@ -12,15 +12,26 @@ import styled from "styled-components"
 import { Col, Container, Image, Row } from "../bootstrap"
 import { firestore } from "components/firebase"
 
+import * as links from "components/links"
+
 const StyledContainer = styled(Container)`
   font-family: "Nunito";
+`
+
+// same as SmartTagButton from bill/summary.tsx
+// consolidate as a shared component when there's time in future?
+const CommitteeButton = styled.button`
+  border-radius: 12px;
+  font-size: 12px;
 `
 
 export const HearingDetails = () => {
   const { t } = useTranslation("common")
   const hearingId = "hearing-5180"
 
+  const [committeeCode, setCommitteeCode] = useState("")
   const [committeeName, setCommitteeName] = useState("")
+  const [generalCourtNumber, setGeneralCourtNumber] = useState("")
   const [videoTranscriptionId, setVideoTranscriptionId] = useState("")
   const [videoURL, setVideoURL] = useState("")
 
@@ -29,7 +40,14 @@ export const HearingDetails = () => {
     const docData = hearing.data()
     const content = docData?.content ?? "Default Content"
 
-    setCommitteeName(content.Name)
+    setCommitteeCode(
+      docData?.content.HearingHost.CommitteeCode ?? "Default Committee Code"
+    )
+    setCommitteeName(docData?.content.Name ?? "Default Name")
+    setGeneralCourtNumber(
+      docData?.content.HearingHost.GeneralCourtNumber ??
+        "Default General Court Number"
+    )
     setVideoTranscriptionId(
       docData?.videoTranscriptionId ?? "Default Video Transcripton Id"
     )
@@ -44,12 +62,25 @@ export const HearingDetails = () => {
   }, [hearingData])
 
   console.log("Committee: ", committeeName)
+  console.log("Committee Code: ", committeeCode)
+  console.log("General Court Number", generalCourtNumber)
   console.log("Id: ", videoTranscriptionId)
   console.log("url: ", videoURL)
 
   return (
     <StyledContainer className="mt-3 mb-3">
       <h1>{t("hearing_transcription")}</h1>
+
+      <links.External
+        href={`https://malegislature.gov/Committees/Detail/${committeeCode}/${generalCourtNumber}`}
+      >
+        <CommitteeButton
+          className={`btn btn-secondary d-flex text-nowrap mt-1 mx-1 p-1`}
+        >
+          &nbsp; {committeeName} &nbsp;
+        </CommitteeButton>
+      </links.External>
+
       <>Hello Hearing World, {videoURL}</>
     </StyledContainer>
   )
