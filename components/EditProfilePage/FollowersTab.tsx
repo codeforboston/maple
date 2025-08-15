0ec76fcbd45662d3cce55806e1a415846c5033d5
jicruz96 <accounts@jicruz.com>
2025-08-15 06:31:03 -0400

    PR review fixes

--- components/EditProfilePage/FollowersTab.tsx
+++ components/EditProfilePage/FollowersTab.tsx
@@ -1,19 +1,17 @@
 import { functions } from "components/firebase"
 import { httpsCallable } from "firebase/functions"
-import type {
-  GetFollowersRequest,
-  GetFollowersResponse
-} from "functions/src/subscriptions/getFollowers"
 import { useTranslation } from "next-i18next"
 import { Dispatch, ReactNode, SetStateAction, useEffect, useState } from "react"
 import { useAuth } from "../auth"
-import { usePublicProfile } from "components/db"
+import { Profile, usePublicProfile } from "components/db"
 import { Internal } from "components/links"
 import { FollowUserButton } from "components/shared/FollowButton"
 import React from "react"
-import { Col, Row, Spinner, Stack } from "../bootstrap"
+import { Col, Row, Spinner, Stack, Alert } from "../bootstrap"
 import { TitledSectionCard } from "../shared"
 import { OrgIconSmall } from "./StyledEditProfileComponents"
+
+type ProfileWithId = Profile & { profileId: string }
 
 export const FollowersTab = ({
   className,
@@ -23,40 +21,48 @@ export const FollowersTab = ({
   setFollowerCount: Dispatch<SetStateAction<number | null>>
 }) => {
   const uid = useAuth().user?.uid
-  const [followerIds, setFollowerIds] = useState<string[]>([])
+  const [followers, setFollowers] = useState<ProfileWithId[]>([])
+  const [loading, setLoading] = useState(true)
+  const [error, setError] = useState<string | null>(null)
   const { t } = useTranslation("editProfile")
 
   useEffect(() => {
-    const fetchFollowers = async (uid: string) => {
+    const fetchFollowers = async () => {
       try {
         const { data: followerIds } = await httpsCallable<
-          GetFollowersRequest,
-          GetFollowersResponse
-        >(
-          functions,
-          "getFollowers"
-        )({ uid })
-        setFollowerIds(followerIds)
+          void,
+          ProfileWithId[]
+        >(functions, "getFollowers")()
+        setFollowers(followerIds)
         setFollowerCount(followerIds.length)
+        setLoading(false)
       } catch (err) {
         console.error("Error fetching followerIds", err)
+        setError("Error fetching followers.")
+        setLoading(false)
         return
       }
     }
-    if (uid) fetchFollowers(uid)
+    if (uid) fetchFollowers()
   }, [uid])
   return (
     <TitledSectionCard className={className}>
-      <div className={`mx-4 mt-3 d-flex flex-column gap-3`}>
+      <div className="mx-4 mt-3 d-flex flex-column gap-3">
         <Stack>
           <h2>{t("follow.your_followers")}</h2>
           <p className="mt-0 text-muted">
             {t("follow.follower_info_disclaimer")}
           </p>
           <div className="mt-3">
-            {followerIds.map((profileId, i) => (
-              <FollowerCard key={i} profileId={profileId} />
-            ))}
+            {error ? (
+              <Alert variant="danger">{error}</Alert>
+            ) : loading ? (
+              <Spinner animation="border" className="mx-auto" />
+            ) : (
+              followers.map((profile, i) => (
+                <FollowerCard key={i} {...profile} />
+              ))
+            )}
           </div>
         </Stack>
       </div>
@@ -64,50 +70,40 @@ export const FollowersTab = ({
   )
 }
 
-const FollowerCard = ({ profileId }: { profileId: string }) => {
-  const { result: profile, loading } = usePublicProfile(profileId)
+const FollowerCard = ({
+  profileId,
+  fullName,
+  profileImage,
+  public: isPublic
+}: ProfileWithId) => {
   const { t } = useTranslation("profile")
-  if (loading) {
-    return (
-      <FollowerCardWrapper>
-        <Spinner animation="border" className="mx-auto" />
-      </FollowerCardWrapper>
-    )
-  }
-  const { fullName, profileImage, public: isPublic } = profile || {}
   const displayName = isPublic && fullName ? fullName : t("anonymousUser")
   return (
-    <FollowerCardWrapper>
-      <Col className="d-flex align-items-center flex-grow-1 p-0 text-start">
-        <OrgIconSmall
-          className="mr-4 mt-0 mb-0 ms-0"
-          profileImage={profileImage}
-        />
-        {isPublic ? (
-          <Internal href={`/profile?id=${profileId}`}>{displayName}</Internal>
-        ) : (
-          <span>{displayName}</span>
-        )}
-      </Col>
-      {isPublic ? (
-        <Col
-          xs="auto"
-          className="d-flex justify-content-end ms-auto text-end p-0"
-        >
-          <FollowUserButton profileId={profileId} />
+    <div className={`fs-3 lh-lg`}>
+      <Row className="align-items-center justify-content-between g-0 w-100">
+        <Col className="d-flex align-items-center flex-grow-1 p-0 text-start">
+          <OrgIconSmall
+            className="mr-4 mt-0 mb-0 ms-0"
+            profileImage={profileImage}
+          />
+          {isPublic ? (
+            <Internal href={`/profile?id=${profileId}`}>{displayName}</Internal>
+          ) : (
+            <span>{displayName}</span>
+          )}
         </Col>
-      ) : (
-        <></>
-      )}
-    </FollowerCardWrapper>
+        {isPublic ? (
+          <Col
+            xs="auto"
+            className="d-flex justify-content-end ms-auto text-end p-0"
+          >
+            <FollowUserButton profileId={profileId} />
+          </Col>
+        ) : (
+          <></>
+        )}
+      </Row>
+      <hr className={`mt-3`} />
+    </div>
   )
 }
-
-const FollowerCardWrapper = ({ children }: { children: ReactNode }) => (
-  <div className={`fs-3 lh-lg`}>
-    <Row className="align-items-center justify-content-between g-0 w-100">
-      {children}
-    </Row>
-    <hr className={`mt-3`} />
-  </div>
-)
--- functions/src/subscriptions/getFollowers.ts
+++ functions/src/subscriptions/getFollowers.ts
@@ -1,46 +1,46 @@
-import { getFirestore } from "firebase-admin/firestore"
+import { FieldPath, getFirestore } from "firebase-admin/firestore"
 import * as functions from "firebase-functions"
+import { checkAuth } from "../common"
 
-export interface GetFollowersRequest {
-  uid: string
-}
-export type GetFollowersResponse = string[]
+export const getFollowers = functions.https.onCall(async (_, context) => {
+  const uid = checkAuth(context, false)
 
-export const getFollowers = functions.https.onCall(
-  async ({ uid }: GetFollowersRequest, context) => {
-    if (!context.auth) {
-      throw new functions.https.HttpsError(
-        "failed-precondition",
-        "The function must be called while authenticated."
+  functions.logger.log(`[getFollowers] Finding followers for user UID: ${uid}`)
+
+  const firestore = getFirestore()
+
+  return await firestore
+    .collectionGroup("activeTopicSubscriptions")
+    .where("userLookup.profileId", "==", uid)
+    .get()
+    .then(snapshot => {
+      const followerProfileIds = Array.from(
+        // use a set to deduplicate
+        new Set(
+          snapshot.docs
+            .map(doc => doc.ref.parent.parent!.id)
+            .filter(id => id != uid)
+        )
       )
-    }
 
-    functions.logger.log(
-      `[getFollowers] Finding followers for user UID: ${uid}`
-    )
+      functions.logger.log(
+        `[getFollowers] Found ${followerProfileIds.length} followers for user UID: ${uid}`
+      )
 
-    return await getFirestore()
-      .collectionGroup("activeTopicSubscriptions")
-      .where("userLookup.profileId", "==", uid)
-      .get()
-      .then(snapshot => {
-        const followers = new Set<string>()
-        snapshot.forEach(doc => {
-          const followerUid = doc.ref.parent.parent?.id
-          if (followerUid && followerUid !== uid) followers.add(followerUid)
-        })
-        functions.logger.log(
-          `[getFollowers] Found ${followers.size} followers for user UID: ${uid}`
+      return firestore
+        .collection("profiles")
+        .where(FieldPath.documentId(), "in", followerProfileIds)
+        .get()
+        .then(snapshot =>
+          snapshot.docs.map(doc => ({ profileId: doc.id, ...doc.data() }))
         )
-        return Array.from(followers)
-      })
-      .catch(error => {
-        functions.logger.error("[getFollowers] Caught error:", error)
-        throw new functions.https.HttpsError(
-          "internal",
-          "Failed to retrieve followers and reciprocal status.",
-          error
-        )
-      })
-  }
-)
+    })
+    .catch(error => {
+      functions.logger.error("[getFollowers] Caught error:", error)
+      throw new functions.https.HttpsError(
+        "internal",
+        "Failed to retrieve followers.",
+        error
+      )
+    })
+})
