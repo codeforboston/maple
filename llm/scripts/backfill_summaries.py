897e30f8b1c9c93e54b43894d126b99ed9d5146a
Barry Moore <chiroptical@gmail.com>
2025-09-23 20:16:11 -0400

    Fill out TODOs

--- llm/scripts/backfill_summaries.py
+++ llm/scripts/backfill_summaries.py
@@ -10,9 +10,28 @@ bills = bills_ref.get()
 count = 0
 for bill in bills:
     document = bill.to_dict()
-    if document.get("summary") is None or document.get("topics") is None:
-        # Notes: DocumentText _can_ be None
-        print(document.get("content", {}).get("DocumentText"))
-        print(document.get("content", {}).get("Title"))
-        print(document.get("content", {}).get("BillNumber"))
-        exit()
+    document_text = document.get("content", {}).get("DocumentText")
+    document_title = document.get("content", {}).get("Title")
+    summary = document.get("summary")
+
+    # No document text, skip it because we can't summarize it
+    if document_text is None:
+        print(f"{document['id']},skipped")
+        continue
+
+    # If the summary is already populated move on
+    if summary is not None:
+        print(f"{document['id']},previous_summary")
+        continue
+
+    # TODO: Generate the summary
+    print(f"{document['id']},generate_summary")
+
+    # If the summary is already populated move on
+    topics = document.get("topics")
+    if topics is not None:
+        print(f"{document['id']},previous_topics")
+        continue
+
+    # TODO: Populate the topics
+    print(f"{document['id']},generate_topics")
