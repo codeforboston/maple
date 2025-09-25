cd7c3947ee6cc7c25526c39bb474e6a1e0c43c32
Mephistic <deathbyfiresermon@gmail.com>
2025-09-24 23:35:40 -0400

    fix(auth): Fix the auth redirect provider to no longer obliterate user claims on load. Our login redirect work added a new Provider for auth that overwrite the existing provider, but the new Provider didn't handle the extra cases around user claims that the old provider did (so we could not implement role-based auth. The new provider is unnecessary, so this change switches back the old fully-functioned Provider. (#1952)

--- pages/admin.tsx
+++ pages/admin.tsx
@@ -17,6 +17,11 @@ function requireAdmin(Component: React.FC<{}>) {
     const { claims } = useAuth()
     const router = useRouter()
 
+    // TODO: This will cause a blank screen to show for logged out users, but
+    //       a redirect to the home page for logged-in non-admin users.
+    //       This should ideally be a consistent experience for both sets of users,
+    //       but not worth the effort to implement right now.
+    //       Worth revisiting if we either expand our admins or add more admin-only pages.
     useEffect(() => {
       if (claims && claims?.role !== "admin") {
         router.push({ pathname: "/" })
