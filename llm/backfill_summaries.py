e7b721afc512921683d92316068ac984ed6586a4
Barry Moore <chiroptical@gmail.com>
2025-10-07 20:56:01 -0400

    Remove temporary exit

--- llm/backfill_summaries.py
+++ llm/backfill_summaries.py
@@ -16,7 +16,7 @@ db = firestore.client()
 # Conceptually, we want to return a very consistent format when generated status reports.
 # It would allow us to skip LLM regeneration when moving from dev to production.
 def make_bill_summary(bill_id, status, summary, topics):
-    return f"{bill_id},{status},{summary},{topics}"
+    return f"{bill_id},{status},{summary},{topics}\n"
 
 
 bills_ref = db.collection("generalCourts/194/bills")
@@ -33,35 +33,29 @@ with open("./summaries-and-topics.csv", "w") as f:
         # No document text, skip it because we can't summarize it
         if document_text is None:
             f.write(make_bill_summary(bill_id, "skipped", None, None))
-            f.write("\n")
             continue
 
         # If the summary is already populated move on
         if summary is not None:
             f.write(make_bill_summary(bill_id, "previous_summary", None, None))
-            f.write("\n")
             continue
 
         summary = get_summary_api_function(bill_id, document_title, document_text)
         if summary["status"] in [-1, -2]:
             f.write(make_bill_summary(bill_id, "failed_summary", None, None))
-            f.write("\n")
             continue
         summary = summary["summary"]
         bill.reference.update({"summary": summary})
         f.write(make_bill_summary(bill_id, "generated_summary", summary, None))
-        f.write("\n")
 
         # If the summary is already populated move on
         topics = document.get("topics")
         if topics is not None:
             f.write(make_bill_summary(bill_id, "previous_topics", None, None))
-            f.write("\n")
             continue
         tags = get_tags_api_function_v2(bill_id, document_title, summary)
         if tags["status"] != 1:
             f.write(make_bill_summary(bill_id, "failed_topics", None, None))
-            f.write("\n")
             continue
         topics_and_categories = get_categories_from_topics(
             tags["tags"], CATEGORY_BY_TOPIC
@@ -70,4 +64,3 @@ with open("./summaries-and-topics.csv", "w") as f:
         f.write(
             make_bill_summary(bill_id, "generated_topics", None, topics_and_categories)
         )
-        exit()
