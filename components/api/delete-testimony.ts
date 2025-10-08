5fe719a6c68a534302c384731a9b3aba4f50f604
Jasmine Rose <72074190+jasrose@users.noreply.github.com.>
2025-10-07 23:50:39 -0400

    update to firebase helper and call function

--- components/api/delete-testimony.ts
+++ components/api/delete-testimony.ts
@@ -1,14 +1,29 @@
-import { mapleClient } from "./maple-client"
+import { httpsCallable } from "firebase/functions"
+import { auth, functions } from "../firebase"
 
 /**
  * Admin-only route for deleting testimony.
  *
- * Moves testimony from published to archived.
+ * Moves testimony from published to archived and updates counts.
  *
  * @param uid user id, author of the testimony
  * @param tid testimony id
  * @returns 204 response (no body) if successful, or 4XX if not.
  */
+
 export async function deleteTestimony(uid: string, tid: string) {
-  return mapleClient.delete(`/api/users/${uid}/testimony/${tid}`)
+  const user = auth.currentUser
+  if (!user) throw new Error("Not logged in.")
+
+  try {
+    const callable = httpsCallable(functions, "deleteTestimony")
+    const res = await callable({ uid, publicationId: tid })
+    return res.data
+  } catch (err: any) {
+    const msg =
+      err?.message ||
+      err?.code ||
+      "Delete failed"
+    throw new Error(msg)
+  }
 }
