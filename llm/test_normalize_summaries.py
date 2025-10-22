be10fb56433660d5b9065723bc870a950507275c
Barry Moore <chiroptical@gmail.com>
2025-10-21 20:44:50 -0400

    Name the tests

--- llm/test_normalize_summaries.py
+++ llm/test_normalize_summaries.py
@@ -1,32 +1,40 @@
 import normalize_summaries
 
 
-def test_normalize_summary_one():
+def test_normalize_summary_handles_summary_prefix_and_bullets():
     summary = """Summary:  
 - The bill allows Joe, the chief of police in Gravity, to continue working.    
 - The city can require annual health examinations   
     """
-    assert normalize_summaries.normalize_summary(
-        summary
-    ) == "The bill allows Joe, the chief of police in Gravity, to continue working. The city can require annual health examinations"
+    assert (
+        normalize_summaries.normalize_summary(summary)
+        == "The bill allows Joe, the chief of police in Gravity, to continue working. The city can require annual health examinations"
+    )
 
 
-def test_normalize_summary_two():
+def test_normalize_summary_handles_summary_prefix_and_no_bullets():
     summary = """Summary:  
 The bill allows Joe, the chief of police in Gravity, to continue working.    
     """
-    assert normalize_summaries.normalize_summary(
-        summary
-    ) == "The bill allows Joe, the chief of police in Gravity, to continue working."
+    assert (
+        normalize_summaries.normalize_summary(summary)
+        == "The bill allows Joe, the chief of police in Gravity, to continue working."
+    )
 
-def test_normalize_summary_three():
+
+def test_normalize_summary_handles_summary_prefix_with_no_linebreak():
     summary = "Summary: The bill allows Joe, the chief of police in Gravity, to continue working."
-    assert normalize_summaries.normalize_summary(
-        summary
-    ) == "The bill allows Joe, the chief of police in Gravity, to continue working."
+    assert (
+        normalize_summaries.normalize_summary(summary)
+        == "The bill allows Joe, the chief of police in Gravity, to continue working."
+    )
 
-def test_normalize_summary_four():
-    summary = "The bill allows Joe, the chief of police in Gravity, to continue working."
-    assert normalize_summaries.normalize_summary(
-        summary
-    ) == "The bill allows Joe, the chief of police in Gravity, to continue working."
+
+def test_normalize_summary_handles_bare_summary():
+    summary = (
+        "The bill allows Joe, the chief of police in Gravity, to continue working."
+    )
+    assert (
+        normalize_summaries.normalize_summary(summary)
+        == "The bill allows Joe, the chief of police in Gravity, to continue working."
+    )
