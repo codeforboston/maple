21a668079bd78dfa8bf754dc731be84afc6c509a
Del <sam.dem2002@gmail.com>
2025-11-25 18:18:27 -0500

    Spread out Download component to sidebar, removed spare space from hearing details.

--- components/hearing/HearingSidebar.tsx
+++ components/hearing/HearingSidebar.tsx
@@ -67,53 +67,6 @@ function MemberItem({
   )
 }
 
-function TranscriptDownload({
-  hearingId,
-  transcriptData
-}: {
-  hearingId: string | string[] | undefined
-  transcriptData: Paragraph[]
-}) {
-  const [downloadName, setDownloadName] = useState<string>("hearing.csv")
-  const [downloadURL, setDownloadURL] = useState<string>("")
-
-  const { t } = useTranslation(["common", "hearing"])
-
-  useEffect(() => {
-    if (!hearingId) {
-      setDownloadName("hearing.csv")
-    } else {
-      setDownloadName(`hearing-${hearingId}.csv`)
-    }
-  }, [hearingId])
-
-  useEffect(() => {
-    if (!transcriptData) return
-    const csv_objects = transcriptData.map(doc => ({
-      start: formatMilliseconds(doc.start),
-      text: doc.text
-    }))
-    const csv = Papa.unparse(csv_objects)
-    const blob = new Blob([csv], { type: "text/csv" })
-    const url = URL.createObjectURL(blob)
-    setDownloadURL(url)
-
-    return () => {
-      URL.revokeObjectURL(url)
-    }
-  }, [transcriptData])
-
-  return downloadURL !== "" && hearingId !== undefined ? (
-    <a
-      href={downloadURL}
-      download={downloadName}
-      className="text-blue-600 underline"
-    >
-      {t("download_transcript", { ns: "hearing" })}
-    </a>
-  ) : null
-}
-
 const BillsBody = styled.div`
   background-color: white;
   max-height: 672px;
@@ -189,6 +142,8 @@ export const HearingSidebar = ({
     dateCheck = true
   }
 
+  const [downloadName, setDownloadName] = useState<string>("hearing.csv")
+  const [downloadURL, setDownloadURL] = useState<string>("")
   const [houseChairName, setHouseChairName] = useState<string>("")
   const [houseChairperson, setHouseChairperson] = useState<Legislator>()
   const [members, setMembers] = useState<Members[]>()
@@ -230,6 +185,30 @@ export const HearingSidebar = ({
   }, [committeeCode, generalCourtNumber])
 
   useEffect(() => {
+    if (!hearingId) {
+      setDownloadName("hearing.csv")
+    } else {
+      setDownloadName(`hearing-${hearingId}.csv`)
+    }
+  }, [hearingId])
+
+  useEffect(() => {
+    if (!transcriptData) return
+    const csv_objects = transcriptData.map(doc => ({
+      start: formatMilliseconds(doc.start),
+      text: doc.text
+    }))
+    const csv = Papa.unparse(csv_objects)
+    const blob = new Blob([csv], { type: "text/csv" })
+    const url = URL.createObjectURL(blob)
+    setDownloadURL(url)
+
+    return () => {
+      URL.revokeObjectURL(url)
+    }
+  }, [transcriptData])
+
+  useEffect(() => {
     committeeCode && generalCourtNumber ? committeeData() : null
   }, [committeeCode, committeeData, generalCourtNumber])
 
@@ -246,24 +225,35 @@ export const HearingSidebar = ({
         {t("hearing_details", { ns: "hearing" })}
       </SidebarHeader>
 
-      <SidebarBody className={`border-bottom fs-6 fw-bold px-3 py-3`}>
-        {dateCheck ? (
-          <>
-            <SidebarSubbody className={`mb-1`}>
-              {t("recording_date", { ns: "hearing" })}
-            </SidebarSubbody>
-            <div className={`fw-normal`}>{formattedDate}</div>
-          </>
-        ) : (
-          <></>
-        )}
-        <div className={`fw-normal`}>
-          <TranscriptDownload
-            hearingId={hearingId}
-            transcriptData={transcriptData}
-          />
-        </div>
-      </SidebarBody>
+      {dateCheck || (downloadURL !== "" && hearingId !== undefined) ? (
+        <SidebarBody className={`border-bottom fs-6 px-3 py-3`}>
+          {dateCheck ? (
+            <>
+              <SidebarSubbody className={`mb-1 fw-bold`}>
+                {t("recording_date", { ns: "hearing" })}
+              </SidebarSubbody>
+              {formattedDate}
+            </>
+          ) : (
+            <></>
+          )}
+          {downloadURL !== "" && hearingId !== undefined ? (
+            <div>
+              <a
+                href={downloadURL}
+                download={downloadName}
+                className="text-blue-600 underline"
+              >
+                {t("download_transcript", { ns: "hearing" })}
+              </a>
+            </div>
+          ) : (
+            <></>
+          )}
+        </SidebarBody>
+      ) : (
+        <></>
+      )}
 
       {committeeCode && (
         <SidebarBody
