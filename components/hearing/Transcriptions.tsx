eafbf0a59806ff00c4c9d54f19a4cbacfd3f4a99
Del <sam.dem2002@gmail.com>
2025-12-16 18:46:51 -0500

    Prettify

--- components/hearing/Transcriptions.tsx
+++ components/hearing/Transcriptions.tsx
@@ -289,11 +289,17 @@ const TranscriptItem = forwardRef(function TranscriptItem(
   }
   const highlightText = (text: string, term: string) => {
     if (!term) return text
-    const escaped = term.replace(/[.*+?^${}()|[\]\\]/g, "\\$&");
+    const escaped = term.replace(/[.*+?^${}()|[\]\\]/g, "\\$&")
     const regex = new RegExp(`(${escaped})`, "gi")
-    return text
-      .split(regex)
-      .map((part, i) => (regex.test(part) ? <mark key={i} className={`p-0`}>{part}</mark> : part))
+    return text.split(regex).map((part, i) =>
+      regex.test(part) ? (
+        <mark key={i} className={`p-0`}>
+          {part}
+        </mark>
+      ) : (
+        part
+      )
+    )
   }
 
   return (
