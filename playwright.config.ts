b3b1925a96ea8ec208d960ab77f711bdddf7adba
nssensalo <nssensalo@gmail.com>
2025-12-16 23:12:43 -0800

    edit profile page playright tests

--- playwright.config.ts
+++ playwright.config.ts
@@ -19,7 +19,7 @@ export default defineConfig({
   /* Retry on CI only */
   retries: process.env.CI ? 2 : 0,
   /* Opt out of parallel tests on CI. */
-  workers: process.env.CI ? 1 : undefined,
+  workers: process.env.CI ? 1 : 1,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: "html",
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
@@ -46,14 +46,14 @@ export default defineConfig({
       use: {
         ...devices["Desktop Firefox"]
       }
-    },
-
-    {
-      name: "webkit",
-      use: {
-        ...devices["Desktop Safari"]
-      }
     }
+
+    // {
+    //   name: "webkit",
+    //   use: {
+    //     ...devices["Desktop Safari"]
+    //   }
+    // }
 
     /* Test against mobile viewports. */
     // {
