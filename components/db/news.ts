f63eaec36354824682cd55c7d4ebc01a6ace36ec
Andre Coullard <andrecoullard@gmail.com>
2026-04-07 22:07:52 -0400

    add sorting to news items

--- components/db/news.ts
+++ components/db/news.ts
@@ -1,4 +1,4 @@
-import { collection, getDocs, orderBy, Timestamp } from "firebase/firestore"
+import { collection, getDocs, orderBy, query, Timestamp } from "firebase/firestore"
 import { useAsync } from "react-async-hook"
 import { firestore } from "../firebase"
 
@@ -17,7 +17,8 @@ export type NewsItem = {
 
 export async function listNews(): Promise<NewsItem[]> {
   const newsRef = collection(firestore, "news")
-  const result = await getDocs(newsRef)
+  const q = query(newsRef, orderBy("publishDate", "desc"))
+  const result = await getDocs(q)
   return result.docs.map(d => ({ id: d.id, ...d.data() } as NewsItem))
 }
 
